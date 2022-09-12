682de5aa18ae0a2822433e6216e16b55d30e7404
Lazar Miuchin <lazar.miuchin@dqo.ai>
2022-09-12 11:43:04 +0200

    Create a top tab container for "open files" - to show opened databases/tables/columns

--- dqoai/src/main/frontend/src/components/Tabs/tab.tsx
+++ dqoai/src/main/frontend/src/components/Tabs/tab.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import SvgIcon from '../SvgIcon';
 
 export interface TabOption {
   label: string;
@@ -9,19 +10,33 @@ export interface TabProps {
   tab: TabOption;
   active?: boolean;
   onChange?: (tab: TabOption) => void;
+  closable?: boolean;
+  onClose?: () => void;
 }
 
 const Tab = ({
   tab,
   active,
-  onChange
+  onChange,
+  closable,
+  onClose,
 }: TabProps) => {
+  const handleClose = (e: any) => {
+    e.stopPropagation();
+    if (onClose) {
+      onClose();
+    }
+  };
+
   return (
     <div
-      className={`text-sm leading-20 text-black px-8 py-2 cursor-pointer ${active ? 'font-semibold relative bg-white border-b-2 border-blue-600' : ''}`}
+      className={`relative text-sm leading-20 text-black px-8 py-2 cursor-pointer ${active ? 'font-semibold relative bg-white border-b-2 border-blue-600' : ''}`}
       onClick={() => onChange && onChange(tab)}
     >
-      {tab.label}
+      <div className="truncate max-w-40">{tab.label}</div>
+      {
+        (closable && active) && <SvgIcon name="close" onClick={handleClose} className="absolute right-1.5 top-1/2 -translate-y-1/2 transform" />
+      }
     </div>
   );
 };
