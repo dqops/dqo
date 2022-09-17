fac937b466aa1c5d9af8986765ddeca03c1994f5
Piotr Czarnas <piotr.czarnas@documati.com>
2022-09-17 21:19:10 +0200

    Typescript axios client is generated in maven, enum values with non standard characters like slashes, hyphens are converted to a hex name to compile correctly as a valid typescript identifier.

--- dqoai/src/main/frontend/src/services/apiClient.ts
+++ dqoai/src/main/frontend/src/services/apiClient.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
-import { ConnectionsApiFactory } from '../api';
+import { ConnectionsApiFactory, Configuration } from '../api';
 
 export const ConnectionApiClient = ConnectionsApiFactory(
-  {},
+  new Configuration(),
   "http://localhost:8888",
   axios
 );
