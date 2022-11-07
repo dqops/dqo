5ce8abac6bcd103e50d63e1058b60975c8bebcb8
Hoang Viet Do <hoang.viet.do@imagetemplates.com>
2022-11-07 18:22:20 +0000

    Merged PR 803: feature/3741_new_rule_max_pct
    Related work items: #3741

---
+++ dqoai/src/test/java/ai/dqo/rules/comparison/MaxPercentRuleParametersSpecTests.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright © 2021 DQO.ai (support@dqo.ai)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ai.dqo.rules.comparison;
+
+import ai.dqo.BaseTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MaxPercentRuleParametersSpecTests extends BaseTest {
+    private MaxPercentRuleParametersSpec sut;
+
+    @Override
+    @BeforeEach
+    protected void setUp() throws Throwable {
+        super.setUp();
+        this.sut = new MaxPercentRuleParametersSpec();
+    }
+
+    @Test
+    void isDirty_whenMaxValueSet_thenIsDirtyIsTrue() {
+        this.sut.setMaxPercent(1);
+        Assertions.assertEquals(1, this.sut.getMaxPercent());
+        Assertions.assertTrue(this.sut.isDirty());
+        this.sut.clearDirty(true);
+        Assertions.assertFalse(this.sut.isDirty());
+    }
+
+    @Test
+    void isDirty_whenMaxValueNumberSameAsCurrentSet_thenIsDirtyIsFalse() {
+        this.sut.setMaxPercent(1);
+        Assertions.assertTrue(this.sut.isDirty());
+        this.sut.clearDirty(true);
+        Assertions.assertFalse(this.sut.isDirty());
+        this.sut.setMaxPercent(1);
+        Assertions.assertFalse(this.sut.isDirty());
+    }
+}
