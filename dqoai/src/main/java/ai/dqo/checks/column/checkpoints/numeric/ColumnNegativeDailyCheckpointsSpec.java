e98c855c8a774c6f84d0358686afd959a5b81a59
Hoang Viet Do <hoang_viet_do@epam.com>
2022-11-17 11:51:28 +0100

    Renamed a file

---
+++ dqoai/src/main/java/ai/dqo/checks/column/checkpoints/numeric/ColumnNegativeDailyCheckpointsSpec.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright © 2021 DQO.ai (support@dqo.ai)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ai.dqo.checks.column.checkpoints.numeric;
+
+import ai.dqo.checks.AbstractCheckCategorySpec;
+import ai.dqo.checks.column.numeric.ColumnMaxNegativeCountCheckSpec;
+import ai.dqo.metadata.id.ChildHierarchyNodeFieldMap;
+import ai.dqo.metadata.id.ChildHierarchyNodeFieldMapImpl;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.EqualsAndHashCode;
+import java.util.Objects;
+
+/**
+ * Container of built-in preconfigured data quality check points on a column level that are checking at a daily level.
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@EqualsAndHashCode(callSuper = true)
+public class ColumnNegativeDailyCheckpointsSpec extends AbstractCheckCategorySpec {
+    public static final ChildHierarchyNodeFieldMapImpl<ColumnNegativeDailyCheckpointsSpec> FIELDS = new ChildHierarchyNodeFieldMapImpl<>(AbstractCheckCategorySpec.FIELDS) {
+        {
+            put("daily_checkpoint_max_negatives_count", o -> o.dailyCheckpointMaxNegativeCount);
+        }
+    };
+
+    @JsonPropertyDescription("Verifies that the number of negative values in a column does not exceed the maximum accepted count. Stores the most recent row count for each day when the data quality check was evaluated.")
+    private ColumnMaxNegativeCountCheckSpec dailyCheckpointMaxNegativeCount;
+
+    /**
+     * Returns a maximum numerics count check.
+     * @return Maximum numerics count check.
+     */
+    public ColumnMaxNegativeCountCheckSpec getDailyCheckpointMaxNegativeCount() {
+        return dailyCheckpointMaxNegativeCount;
+    }
+
+    /**
+     * Sets a new definition of a maximum numerics count check.
+     * @param dailyCheckpointMaxNegativeCount Maximum numerics count check.
+     */
+    public void setDailyCheckpointMaxNegativeCount(ColumnMaxNegativeCountCheckSpec dailyCheckpointMaxNegativeCount) {
+        this.setDirtyIf(!Objects.equals(this.dailyCheckpointMaxNegativeCount, dailyCheckpointMaxNegativeCount));
+        this.dailyCheckpointMaxNegativeCount = dailyCheckpointMaxNegativeCount;
+        propagateHierarchyIdToField(dailyCheckpointMaxNegativeCount, "daily_checkpoint_max_negatives_count");
+    }
+
+    /**
+     * Returns the child map on the spec class with all fields.
+     *
+     * @return Return the field map.
+     */
+    @Override
+    protected ChildHierarchyNodeFieldMap getChildMap() {
+        return FIELDS;
+    }
+}
