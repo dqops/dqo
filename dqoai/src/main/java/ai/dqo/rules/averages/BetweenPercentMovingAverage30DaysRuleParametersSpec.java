752b8ffa1f9c1268ff5a86e89b032aad1e7a0c06
Hoang Viet Do <hoang_viet_do@epam.com>
2022-11-21 15:03:17 +0100

    Changed "double" to "Double"

--- dqoai/src/main/java/ai/dqo/rules/averages/BetweenPercentMovingAverage30DaysRuleParametersSpec.java
+++ dqoai/src/main/java/ai/dqo/rules/averages/BetweenPercentMovingAverage30DaysRuleParametersSpec.java
@@ -56,7 +56,7 @@ public class BetweenPercentMovingAverage30DaysRuleParametersSpec extends Abstrac
      * Minimum percent value for a data quality check readout, for example a minimum row count.
      * @return A percent that is used to calculate lower limit.
      */
-    public double getMaxPercentBelow() {
+    public Double getMaxPercentBelow() {
         return maxPercentBelow;
     }
 
@@ -64,7 +64,7 @@ public class BetweenPercentMovingAverage30DaysRuleParametersSpec extends Abstrac
      * Changes the minimum value (threshold) for a data quality readout.
      * @param maxPercentBelow
      */
-    public void setMaxPercentBelow(double maxPercentBelow) {
+    public void setMaxPercentBelow(Double maxPercentBelow) {
         this.setDirtyIf(!Objects.equals(this.maxPercentBelow, maxPercentBelow));
         this.maxPercentBelow = maxPercentBelow;
     }
@@ -73,7 +73,7 @@ public class BetweenPercentMovingAverage30DaysRuleParametersSpec extends Abstrac
      * Maximum percent value for a data quality check readout, for example a minimum row count.
      * @return Maximum value for a data quality check readout.
      */
-    public double getMaxPercentAbove() {
+    public Double getMaxPercentAbove() {
         return maxPercentAbove;
     }
 
@@ -81,7 +81,7 @@ public class BetweenPercentMovingAverage30DaysRuleParametersSpec extends Abstrac
      * Changes the maximum value (threshold) for a data quality readout.
      * @param maxPercentAbove Maximum value.
      */
-    public void setMaxPercentAbove(double maxPercentAbove) {
+    public void setMaxPercentAbove(Double maxPercentAbove) {
         this.setDirtyIf(!Objects.equals(this.maxPercentAbove, maxPercentAbove));
         this.maxPercentAbove = maxPercentAbove;
     }
