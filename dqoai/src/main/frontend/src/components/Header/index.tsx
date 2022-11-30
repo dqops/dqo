446b2ee0f0f3de7528cb3a88a739a089650cb3ff
Lazar Miuchin <lazar.miuchin@dqo.ai>
2022-11-30 15:45:11 +0100

    feat: add DQO.ai logo at top bar

--- dqoai/src/main/frontend/src/components/Header/index.tsx
+++ dqoai/src/main/frontend/src/components/Header/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { useTree } from '../../contexts/treeContext';
 import NotificationMenu from '../NotificationMenu';
+import Logo from '../Logo';
 
 const Header = () => {
   const { sidebarWidth } = useTree();
   return (
     <div
-      className="fixed top-0 right-0 min-h-16 bg-white shadow-header flex items-center justify-end z-10 px-4"
+      className="fixed top-0 right-0 min-h-16 bg-white shadow-header flex items-center justify-between z-10 px-4"
       style={{ left: sidebarWidth }}
     >
+      <div>
+        <Logo className="w-30" />
+      </div>
       <NotificationMenu />
     </div>
   );
