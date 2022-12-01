2a907fd7308579b9353ae5fb55ab851baddf663c
Rafal Klimek <rafal.klimek@dqo.ai>
2022-12-01 16:34:26 +0100

    Rest list schemas from remote source database

---
+++ dqoai/src/main/java/ai/dqo/rest/models/remote/SchemaRemoteModel.java
@@ -0,0 +1,26 @@
+package ai.dqo.rest.models.remote;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+
+/**
+ * Schema model returned from REST API. Describes a schema on the source database with established connection.
+ */
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@ApiModel(value = "SchemaRemoteModel", description = "Schema remote model")
+public class SchemaRemoteModel {
+    @JsonPropertyDescription("Connection name.")
+    private String connectionName;
+
+    @JsonPropertyDescription("Schema name.")
+    private String schemaName;
+
+    @JsonPropertyDescription("Is the schema imported.")
+    private boolean isImported;
+}
