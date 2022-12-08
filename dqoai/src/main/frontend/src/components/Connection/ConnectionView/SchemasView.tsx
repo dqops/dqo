7b4f2a44e5bf684a4b7b7f5f06ba4262b79d94e5
Lazar Miuchin <lazar.miuchin@dqo.ai>
2022-12-08 11:00:09 +0100

    feat: Table with a list of source (remote) schemas should be opened only after clicking "Import Metadata"

--- dqoai/src/main/frontend/src/components/Connection/ConnectionView/SchemasView.tsx
+++ dqoai/src/main/frontend/src/components/Connection/ConnectionView/SchemasView.tsx
@@ -1,51 +1,40 @@
 import React, { useEffect, useState } from 'react';
-import { JobApiClient, SourceSchemasApi } from '../../../services/apiClient';
-import {
-  DqoJobHistoryEntryModelStatusEnum,
-  SchemaRemoteModel
-} from '../../../api';
-import SvgIcon from '../../SvgIcon';
+import { JobApiClient, SchemaApiClient } from '../../../services/apiClient';
+import { DqoJobHistoryEntryModelStatusEnum, SchemaModel } from '../../../api';
 import Button from '../../Button';
-import Loader from '../../Loader';
-import ConnectionActionGroup from './ConnectionActionGroup';
-import { useActionDispatch } from '../../../hooks/useActionDispatch';
 import { toggleMenu } from '../../../redux/actions/job.actions';
+import { isEqual } from 'lodash';
 import { useSelector } from 'react-redux';
 import { IRootState } from '../../../redux/reducers';
-import { isEqual } from 'lodash';
+import { useActionDispatch } from '../../../hooks/useActionDispatch';
+import ConnectionActionGroup from './ConnectionActionGroup';
 
 interface ISchemasViewProps {
   connectionName: string;
 }
 
 const SchemasView = ({ connectionName }: ISchemasViewProps) => {
-  const [loading, setLoading] = useState(false);
-  const [schemas, setSchemas] = useState<SchemaRemoteModel[]>([]);
+  const [schemas, setSchemas] = useState<SchemaModel[]>([]);
   const { jobs } = useSelector((state: IRootState) => state.job);
 
   const dispatch = useActionDispatch();
 
   useEffect(() => {
-    setLoading(true);
-    SourceSchemasApi.getRemoteSchemas(connectionName)
-      .then((res) => {
-        setSchemas(res.data);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+    SchemaApiClient.getSchemas(connectionName).then((res) => {
+      setSchemas(res.data);
+    });
   }, [connectionName]);
 
-  const onImportTables = (schema: SchemaRemoteModel) => {
-    JobApiClient.importTables(schema.importTableJobParameters);
+  const onImportTables = (schema: SchemaModel) => {
+    JobApiClient.importTables(schema.import_table_job_parameters);
     dispatch(toggleMenu(true));
   };
 
-  const isExist = (schema: SchemaRemoteModel) => {
+  const isExist = (schema: SchemaModel) => {
     const job = jobs?.jobs?.find((item) =>
       isEqual(
         item.parameters?.importTableParameters,
-        schema.importTableJobParameters
+        schema.import_table_job_parameters
       )
     );
     return (
@@ -59,47 +48,30 @@ const SchemasView = ({ connectionName }: ISchemasViewProps) => {
   return (
     <div className="py-4 px-8">
       <ConnectionActionGroup />
-      {loading ? (
-        <div className="flex justify-center h-100">
-          <Loader isFull={false} className="w-8 h-8 fill-green-700" />
-        </div>
-      ) : (
-        <table className="w-full">
-          <thead>
-            <tr>
-              <th className="py-2 px-4 text-left">Source Schema Name</th>
-              <th className="py-2 px-4 text-left">Is already imported</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            {schemas.map((item) => (
-              <tr key={item.schemaName} className="mb-3">
-                <td className="py-2 px-4 text-left">{item.schemaName}</td>
-                <td className="py-2 px-4 text-left">
-                  <SvgIcon
-                    name={item.alreadyImported ? 'check' : 'close'}
-                    className={
-                      item.alreadyImported ? 'text-green-700' : 'text-red-700'
-                    }
-                    width={30}
-                    height={22}
+      <table className="w-full">
+        <thead>
+          <tr>
+            <th className="py-2 px-4 text-left">Schema Name</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          {schemas.map((item) => (
+            <tr key={item.schema_name} className="mb-3">
+              <td className="py-2 px-4 text-left">{item.schema_name}</td>
+              <td className="py-2 px-4 text-left">
+                {!isExist(item) && (
+                  <Button
+                    label="Import tables"
+                    color="primary"
+                    onClick={() => onImportTables(item)}
                   />
-                </td>
-                <td className="py-2 px-4 text-left">
-                  {!isExist(item) && (
-                    <Button
-                      label="Import tables"
-                      color="primary"
-                      onClick={() => onImportTables(item)}
-                    />
-                  )}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
