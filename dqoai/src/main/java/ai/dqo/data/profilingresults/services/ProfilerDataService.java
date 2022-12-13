0fb6464e3f260942f54acfe7a133cdd6c0762a45
Piotr Czarnas <piotr.czarnas@gmail.com>
2022-12-13 17:39:09 +0100

    Profiler result service can return results for a single column.

--- dqoai/src/main/java/ai/dqo/data/profilingresults/services/ProfilerDataService.java
+++ dqoai/src/main/java/ai/dqo/data/profilingresults/services/ProfilerDataService.java
@@ -1,5 +1,6 @@
 package ai.dqo.data.profilingresults.services;
 
+import ai.dqo.data.profilingresults.services.models.ProfilerResultsForColumnModel;
 import ai.dqo.data.profilingresults.services.models.ProfilerResultsForTableModel;
 import ai.dqo.metadata.sources.PhysicalTableName;
 
@@ -17,4 +18,17 @@ public interface ProfilerDataService {
     ProfilerResultsForTableModel getMostRecentProfilerMetricsForTable(String connectionName,
                                                                       PhysicalTableName physicalTableName,
                                                                       String dataStreamName);
+
+    /**
+     * Retrieves the most recent table profiler results for a given column.
+     * @param connectionName    Connection name.
+     * @param physicalTableName Full table name (schema and table).
+     * @param columName         Column name.
+     * @param dataStreamName    Data stream name.
+     * @return Profiler results for the given table.
+     */
+    ProfilerResultsForColumnModel getMostRecentProfilerMetricsForColumn(String connectionName,
+                                                                        PhysicalTableName physicalTableName,
+                                                                        String columName,
+                                                                        String dataStreamName);
 }
