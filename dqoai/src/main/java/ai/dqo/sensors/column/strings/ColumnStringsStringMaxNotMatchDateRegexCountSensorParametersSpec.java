e8392456816443d5f2c0fb1dae26028ce80b8c97
Hoang Viet Do <hoang_viet_do@epam.com>
2022-12-21 15:53:40 +0100

    Created sensors - max_invalid_date_regex_count

---
+++ dqoai/src/main/java/ai/dqo/sensors/column/strings/ColumnStringsStringMaxNotMatchDateRegexCountSensorParametersSpec.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright © 2021 DQO.ai (support@dqo.ai)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ai.dqo.sensors.column.strings;
+
+import ai.dqo.metadata.id.ChildHierarchyNodeFieldMap;
+import ai.dqo.metadata.id.ChildHierarchyNodeFieldMapImpl;
+import ai.dqo.sensors.column.AbstractColumnSensorParametersSpec;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.EqualsAndHashCode;
+
+import java.util.Objects;
+
+/**
+ * Column level sensor that calculates the number of values that does not fit to a date regex in a column.
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@EqualsAndHashCode(callSuper = true)
+public class ColumnStringsStringMaxNotMatchDateRegexCountSensorParametersSpec extends AbstractColumnSensorParametersSpec {
+    public static final ChildHierarchyNodeFieldMapImpl<ColumnStringsStringMaxNotMatchDateRegexCountSensorParametersSpec> FIELDS = new ChildHierarchyNodeFieldMapImpl<>(AbstractColumnSensorParametersSpec.FIELDS) {
+        {
+        }
+    };
+
+    @JsonPropertyDescription("Desired date format. Sensor will try to parse the column records and cast the data using this format.")
+    private BuiltInDateFormats namedDateFormat = BuiltInDateFormats.ISO8601;
+
+    /**
+     * Returns the child map on the spec class with all fields.
+     *
+     * @return Return the field map.
+     */
+    @Override
+    protected ChildHierarchyNodeFieldMap getChildMap() {
+        return FIELDS;
+    }
+
+    /**
+     * Returns the sensor definition name. This is the folder name that keeps the sensor definition files.
+     *
+     * @return Sensor definition name.
+     */
+    @Override
+    public String getSensorDefinitionName() {
+        return "column/strings/string_max_not_match_date_regex_count";
+    }
+
+    /**
+     * Returns a desired format to parse the date.
+     * @return Date format.
+     */
+    public BuiltInDateFormats getNamedDateFormat() {
+        return namedDateFormat;
+    }
+
+    /**
+     * Sets a desired format to parse the date.
+     * @param namedDateFormat Date format.
+     */
+    public void setNamedDateFormat(BuiltInDateFormats namedDateFormat) {
+        this.setDirtyIf(this.namedDateFormat != namedDateFormat);
+        this.namedDateFormat = namedDateFormat;
+    }
+}
+
