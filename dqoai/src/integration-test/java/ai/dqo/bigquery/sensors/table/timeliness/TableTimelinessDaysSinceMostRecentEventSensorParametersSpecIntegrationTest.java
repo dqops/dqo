626c8d13c71691105aa9b9ae4adba0decfd3ad8f
Paweł Duda <pawel.duda@documati.io>
2023-01-03 10:39:39 +0100

    Table timeliness - integration tests templates

---
+++ dqoai/src/integration-test/java/ai/dqo/bigquery/sensors/table/timeliness/TableTimelinessDaysSinceMostRecentEventSensorParametersSpecIntegrationTest.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright © 2021 DQO.ai (support@dqo.ai)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ai.dqo.bigquery.sensors.table.timeliness;
+
+import ai.dqo.bigquery.BaseBigQueryIntegrationTest;
+import ai.dqo.checks.table.checkspecs.timeliness.TableDaysSinceMostRecentEventCheckSpec;
+import ai.dqo.checks.table.timeliness.TableTimelinessAverageDelayCheckSpec;
+import ai.dqo.connectors.ProviderType;
+import ai.dqo.execution.sensors.DataQualitySensorRunnerObjectMother;
+import ai.dqo.execution.sensors.SensorExecutionResult;
+import ai.dqo.execution.sensors.SensorExecutionRunParameters;
+import ai.dqo.execution.sensors.SensorExecutionRunParametersObjectMother;
+import ai.dqo.metadata.groupings.TimeSeriesConfigurationSpecObjectMother;
+import ai.dqo.metadata.groupings.TimeSeriesGradient;
+import ai.dqo.metadata.storage.localfiles.userhome.UserHomeContext;
+import ai.dqo.metadata.storage.localfiles.userhome.UserHomeContextObjectMother;
+import ai.dqo.sampledata.IntegrationTestSampleDataObjectMother;
+import ai.dqo.sampledata.SampleCsvFileNames;
+import ai.dqo.sampledata.SampleTableMetadata;
+import ai.dqo.sampledata.SampleTableMetadataObjectMother;
+import ai.dqo.sensors.table.timeliness.BuiltInTimeScale;
+import ai.dqo.sensors.table.timeliness.TableTimelinessAverageDelaySensorParametersSpec;
+import ai.dqo.sensors.table.timeliness.TableTimelinessDaysSinceMostRecentEventSensorParametersSpec;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import tech.tablesaw.api.Table;
+
+@SpringBootTest
+public class TableTimelinessDaysSinceMostRecentEventSensorParametersSpecIntegrationTest extends BaseBigQueryIntegrationTest {
+    private TableTimelinessDaysSinceMostRecentEventSensorParametersSpec sut;
+    private UserHomeContext userHomeContext;
+    private TableDaysSinceMostRecentEventCheckSpec checkSpec;
+    private SampleTableMetadata sampleTableMetadata;
+
+    /**
+     * Called before each test.
+     * This method should be overridden in derived super classes (test classes), but remember to add {@link BeforeEach} annotation in a derived test class. JUnit5 demands it.
+     *
+     * @throws Throwable
+     */
+    @Override
+    @BeforeEach
+    protected void setUp() throws Throwable {
+        super.setUp();
+        this.sampleTableMetadata = SampleTableMetadataObjectMother.createSampleTableMetadataForCsvFile(SampleCsvFileNames.test_average_delay, ProviderType.bigquery);
+        IntegrationTestSampleDataObjectMother.ensureTableExists(sampleTableMetadata);
+        this.userHomeContext = UserHomeContextObjectMother.createInMemoryFileHomeContextForSampleTable(sampleTableMetadata);
+        this.sut = new TableTimelinessDaysSinceMostRecentEventSensorParametersSpec();
+        this.checkSpec = new TableDaysSinceMostRecentEventCheckSpec();
+        this.checkSpec.setParameters(this.sut);
+    }
+
+    @Test
+    void runSensor_whenSensorExecuted_thenReturnsValues() {
+        SensorExecutionRunParameters runParameters = SensorExecutionRunParametersObjectMother.createForTableForAdHocCheck(
+                sampleTableMetadata, this.checkSpec);
+        runParameters.setTimeSeries(TimeSeriesConfigurationSpecObjectMother.createTimestampColumnTimeSeries("date2", TimeSeriesGradient.DAY));
+        SensorExecutionResult sensorResult = DataQualitySensorRunnerObjectMother.executeSensor(this.userHomeContext, runParameters);
+
+        Table resultTable = sensorResult.getResultTable();
+        Assertions.assertEquals(10, resultTable.rowCount());
+        Assertions.assertEquals("actual_value", resultTable.column(0).name());
+        Assertions.assertEquals(9.0, resultTable.column(0).get(0));
+
+    }
+
+    @Test
+    void runSensor_whenSensorExecuted_thenReturnsValues2() {
+        SensorExecutionRunParameters runParameters = SensorExecutionRunParametersObjectMother.createForTableForAdHocCheck(
+                sampleTableMetadata, this.checkSpec);
+        runParameters.setTimeSeries(TimeSeriesConfigurationSpecObjectMother.createTimestampColumnTimeSeries("date2", TimeSeriesGradient.DAY));
+        SensorExecutionResult sensorResult = DataQualitySensorRunnerObjectMother.executeSensor(this.userHomeContext, runParameters);
+
+        Table resultTable = sensorResult.getResultTable();
+        Assertions.assertEquals(10, resultTable.rowCount());
+        Assertions.assertEquals("actual_value", resultTable.column(0).name());
+        Assertions.assertEquals(9.5, resultTable.column(0).get(1));
+
+    }
+}
