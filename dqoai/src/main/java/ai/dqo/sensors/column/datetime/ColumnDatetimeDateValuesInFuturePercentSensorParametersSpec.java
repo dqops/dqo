97e5b36808980f67237f2479fd0a8d58c01f3934
Hoang Viet Do <hoang_viet_do@epam.com>
2023-01-04 23:39:31 +0100

    Fixed query in date_values_in_future_percent

--- dqoai/src/main/java/ai/dqo/sensors/column/datetime/ColumnDatetimeDateValuesInFuturePercentSensorParametersSpec.java
+++ dqoai/src/main/java/ai/dqo/sensors/column/datetime/ColumnDatetimeDateValuesInFuturePercentSensorParametersSpec.java
@@ -42,7 +42,7 @@ public class ColumnDatetimeDateValuesInFuturePercentSensorParametersSpec extends
 
     @JsonPropertyDescription("Timestamp column to measure timeliness. Other data types will be cast as Timestamp if possible.")
     @ControlType(ParameterDataType.column_name_type)
-    private String columnName;
+    private ParameterDataType columnName = ParameterDataType.column_name_type;
 
     /**
      * Returns the child map on the spec class with all fields.
@@ -68,7 +68,7 @@ public class ColumnDatetimeDateValuesInFuturePercentSensorParametersSpec extends
      * Returns the column's name.
      * @return Column's name.
      */
-    public String getColumnName() {
+    public ParameterDataType getColumnName() {
         return columnName;
     }
 
@@ -76,7 +76,7 @@ public class ColumnDatetimeDateValuesInFuturePercentSensorParametersSpec extends
      * Sets the column's name.
      * @param columnName
      */
-    public void setColumnName(String columnName) {
+    public void setColumnName(ParameterDataType columnName) {
         this.setDirtyIf(!Objects.equals(this.columnName, columnName));
         this.columnName = columnName;
     }
