890b33b32e36bf2a6b13a4b5b8f92667ab43afa7
Hoang Viet Do <hoang_viet_do@epam.com>
2023-01-04 15:51:22 +0100

    Changed the parameters in date_velues_in_future_percent

--- dqoai/src/main/java/ai/dqo/sensors/column/datetime/ColumnDatetimeDateValuesInFuturePercentSensorParametersSpec.java
+++ dqoai/src/main/java/ai/dqo/sensors/column/datetime/ColumnDatetimeDateValuesInFuturePercentSensorParametersSpec.java
@@ -15,13 +15,18 @@
  */
 package ai.dqo.sensors.column.datetime;
 
+import ai.dqo.metadata.fields.ControlType;
+import ai.dqo.metadata.fields.ParameterDataType;
 import ai.dqo.metadata.id.ChildHierarchyNodeFieldMap;
 import ai.dqo.metadata.id.ChildHierarchyNodeFieldMapImpl;
 import ai.dqo.sensors.column.AbstractColumnSensorParametersSpec;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.EqualsAndHashCode;
+
+import java.util.Objects;
 
 /**
  * Column level sensor that calculates the percentage of rows with a date value in the future, compared with the current date.
@@ -34,6 +39,10 @@ public class ColumnDatetimeDateValuesInFuturePercentSensorParametersSpec extends
         {
         }
     };
+
+    @JsonPropertyDescription("Timestamp column to measure timeliness. Other data types will be cast as Timestamp if possible.")
+    @ControlType(ParameterDataType.column_name_type)
+    private String column;
 
     /**
      * Returns the child map on the spec class with all fields.
@@ -53,5 +62,22 @@ public class ColumnDatetimeDateValuesInFuturePercentSensorParametersSpec extends
     @Override
     public String getSensorDefinitionName() {
         return "column/datetime/date_values_in_future_percent";
+    }
+
+    /**
+     * Returns the column's name.
+     * @return Column's name.
+     */
+    public String getColumn() {
+        return column;
+    }
+
+    /**
+     * Sets the column's name.
+     * @param column
+     */
+    public void setColumn(String column) {
+        this.setDirtyIf(!Objects.equals(this.column, column));
+        this.column = column;
     }
 }
