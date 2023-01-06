cb45f259bb24f6e56f681a8a5a984533fa953f0e
Lazar Miuchin <lazar.miuchin@dqo.ai>
2023-01-06 20:28:23 +0100

    feat: Decrease the font size in trees (checks and dashboards) to 13px

--- dqoai/src/main/frontend/src/components/Dashboards/LeftView/index.tsx
+++ dqoai/src/main/frontend/src/components/Dashboards/LeftView/index.tsx
@@ -12,19 +12,19 @@ const LeftView = () => {
     <div className="fixed left-0 top-0 h-full w-70 shadow border-r border-gray-300 p-4 pt-12 bg-white">
       {dashboardFolders.map((folder, index) => (
         <div key={index} className="mb-3">
-          <div className="flex space-x-1 items-center mb-1">
+          <div className="flex space-x-1.5 items-center mb-1">
             <SvgIcon name="folder" className="w-4 h-4" />
-            <div className="text-sm leading-1.5">{folder.folder_name}</div>
+            <div className="text-[13px] leading-1.5">{folder.folder_name}</div>
           </div>
-          <div className="pl-7">
+          <div className="pl-5">
             {folder.dashboards?.map((dashboard, jIndex) => (
               <div
                 key={jIndex}
-                className="cursor-pointer flex space-x-1 items-center"
+                className="cursor-pointer flex space-x-1.5 items-center"
                 onClick={() => changeActiveTab(dashboard, folder.folder_name)}
               >
                 <SvgIcon name="grid" className="w-4 h-4" />
-                <div className="text-sm leading-1.5">{dashboard.dashboard_name}</div>
+                <div className="text-[13px] leading-1.5">{dashboard.dashboard_name}</div>
               </div>
             ))}
           </div>
