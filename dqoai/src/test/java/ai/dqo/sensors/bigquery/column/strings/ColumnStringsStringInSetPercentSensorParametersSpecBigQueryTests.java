022829ebd35bfb27e31e4c2d71b2f8662b0abc54
maciej.krawczyk <maciej.krawczyk@imagetemplates.com>
2023-01-17 14:32:09 +0100

    initial commit - fix failing unit tests

--- dqoai/src/test/java/ai/dqo/sensors/bigquery/column/strings/ColumnStringsStringInSetPercentSensorParametersSpecBigQueryTests.java
+++ dqoai/src/test/java/ai/dqo/sensors/bigquery/column/strings/ColumnStringsStringInSetPercentSensorParametersSpecBigQueryTests.java
@@ -153,7 +153,7 @@ public class ColumnStringsStringInSetPercentSensorParametersSpecBigQueryTests ex
         String renderedTemplate = JinjaTemplateRenderServiceObjectMother.renderBuiltInTemplate(runParameters);
         String target_query = """
             SELECT
-                0 AS actual_value
+                0.0 AS actual_value
             FROM `%s`.`%s`.`%s` AS analyzed_table
             WHERE %s""";
 
