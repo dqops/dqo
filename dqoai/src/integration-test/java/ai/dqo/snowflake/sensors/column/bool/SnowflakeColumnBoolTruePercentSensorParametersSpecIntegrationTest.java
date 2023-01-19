ebf63f07c2e8a2e15fb62ddecbf6a30021ff024b
Paweł Duda <pawel.duda@documati.io>
2023-01-19 11:38:06 +0100

    Changed path to checkspec after merge v2

--- dqoai/src/integration-test/java/ai/dqo/snowflake/sensors/column/bool/SnowflakeColumnBoolTruePercentSensorParametersSpecIntegrationTest.java
+++ dqoai/src/integration-test/java/ai/dqo/snowflake/sensors/column/bool/SnowflakeColumnBoolTruePercentSensorParametersSpecIntegrationTest.java
@@ -16,7 +16,7 @@
 package ai.dqo.snowflake.sensors.column.bool;
 
 import ai.dqo.checks.CheckTimeScale;
-import ai.dqo.checks.column.bool.ColumnTruePercentCheckSpec;
+import ai.dqo.checks.column.checkspecs.bool.ColumnTruePercentCheckSpec;
 import ai.dqo.connectors.ProviderType;
 import ai.dqo.execution.sensors.DataQualitySensorRunnerObjectMother;
 import ai.dqo.execution.sensors.SensorExecutionResult;
