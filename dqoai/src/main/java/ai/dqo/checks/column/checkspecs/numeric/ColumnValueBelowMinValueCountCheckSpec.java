21cc1e0dc8d7c2616c25981bfe619120f1e5f7b8
Hoang Viet Do <hoang_viet_do@epam.com>
2023-02-01 09:45:35 +0100

    Updated the checkspec for value_below_min_value_count

--- dqoai/src/main/java/ai/dqo/checks/column/checkspecs/numeric/ColumnValueBelowMinValueCountCheckSpec.java
+++ dqoai/src/main/java/ai/dqo/checks/column/checkspecs/numeric/ColumnValueBelowMinValueCountCheckSpec.java
@@ -161,6 +161,6 @@ public class ColumnValueBelowMinValueCountCheckSpec
      */
     @Override
     public DefaultDataQualityDimensions getDefaultDataQualityDimension() {
-        return DefaultDataQualityDimensions.Completeness;
+        return DefaultDataQualityDimensions.Validity;
     }
 }
