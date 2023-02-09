508e003f95fb68faaa92a8571e6a96d44e1d7a41
Andrzej Nagalski <andrzej.nagalski@documati.com>
2023-02-09 17:24:50 +0100

    Added missing Big Query unit tests for checks. Improving some sensors and integration tests.

--- dqoai/src/main/java/ai/dqo/checks/column/checkspecs/strings/ColumnStringMatchRegexPercentCheckSpec.java
+++ dqoai/src/main/java/ai/dqo/checks/column/checkspecs/strings/ColumnStringMatchRegexPercentCheckSpec.java
@@ -22,7 +22,7 @@ import ai.dqo.metadata.id.ChildHierarchyNodeFieldMapImpl;
 import ai.dqo.rules.comparison.MinPercentRule95ParametersSpec;
 import ai.dqo.rules.comparison.MinPercentRule98ParametersSpec;
 import ai.dqo.rules.comparison.MinPercentRule99ParametersSpec;
-import ai.dqo.sensors.column.strings.ColumnStringsStringRegexMatchPercentSensorParametersSpec;
+import ai.dqo.sensors.column.strings.ColumnStringsStringMatchRegexPercentSensorParametersSpec;
 import ai.dqo.utils.serialization.IgnoreEmptyYamlSerializer;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
@@ -40,7 +40,7 @@ import java.util.Objects;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @EqualsAndHashCode(callSuper = true)
 public class ColumnStringMatchRegexPercentCheckSpec
-        extends AbstractCheckSpec<ColumnStringsStringRegexMatchPercentSensorParametersSpec, MinPercentRule98ParametersSpec, MinPercentRule99ParametersSpec, MinPercentRule95ParametersSpec> {
+        extends AbstractCheckSpec<ColumnStringsStringMatchRegexPercentSensorParametersSpec, MinPercentRule98ParametersSpec, MinPercentRule99ParametersSpec, MinPercentRule95ParametersSpec> {
     public static final ChildHierarchyNodeFieldMapImpl<ColumnStringMatchRegexPercentCheckSpec> FIELDS = new ChildHierarchyNodeFieldMapImpl<>(AbstractCheckSpec.FIELDS) {
         {
         }
@@ -49,7 +49,7 @@ public class ColumnStringMatchRegexPercentCheckSpec
     @JsonPropertyDescription("Data quality check parameters")
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     @JsonSerialize(using = IgnoreEmptyYamlSerializer.class)
-    private ColumnStringsStringRegexMatchPercentSensorParametersSpec parameters = new ColumnStringsStringRegexMatchPercentSensorParametersSpec();
+    private ColumnStringsStringMatchRegexPercentSensorParametersSpec parameters = new ColumnStringsStringMatchRegexPercentSensorParametersSpec();
 
     @JsonPropertyDescription("Default alerting threshold for a minimum percentage of rows with matching regex in a column that raises a data quality error (alert).")
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
@@ -71,7 +71,7 @@ public class ColumnStringMatchRegexPercentCheckSpec
      * @return Sensor parameters.
      */
     @Override
-    public ColumnStringsStringRegexMatchPercentSensorParametersSpec getParameters() {
+    public ColumnStringsStringMatchRegexPercentSensorParametersSpec getParameters() {
         return parameters;
     }
 
@@ -79,7 +79,7 @@ public class ColumnStringMatchRegexPercentCheckSpec
      * Sets a new row count sensor parameter object.
      * @param parameters Row count parameters.
      */
-    public void setParameters(ColumnStringsStringRegexMatchPercentSensorParametersSpec parameters) {
+    public void setParameters(ColumnStringsStringMatchRegexPercentSensorParametersSpec parameters) {
         this.setDirtyIf(!Objects.equals(this.parameters, parameters));
         this.parameters = parameters;
         this.propagateHierarchyIdToField(parameters, "parameters");
--- dqoai/src/test/java/ai/dqo/utils/docs/sensors/SensorDocumentationModelFactoryImplTests.java
+++ dqoai/src/test/java/ai/dqo/utils/docs/sensors/SensorDocumentationModelFactoryImplTests.java
@@ -4,7 +4,7 @@ import ai.dqo.BaseTest;
 import ai.dqo.metadata.storage.localfiles.dqohome.DqoHomeContext;
 import ai.dqo.metadata.storage.localfiles.dqohome.DqoHomeDirectFactory;
 import ai.dqo.services.check.mapping.SpecToUiCheckMappingServiceImpl;
-import ai.dqo.sensors.column.strings.ColumnStringsStringRegexMatchPercentSensorParametersSpec;
+import ai.dqo.sensors.column.strings.ColumnStringsStringMatchRegexPercentSensorParametersSpec;
 import ai.dqo.utils.reflection.ReflectionServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -28,15 +28,15 @@ public class SensorDocumentationModelFactoryImplTests extends BaseTest {
 
     @Test
     void createSensorDocumentation_whenCalledForClassWithSensorParameterFields_thenCreatesDocumentationModel() {
-        ColumnStringsStringRegexMatchPercentSensorParametersSpec parametersSpec = new ColumnStringsStringRegexMatchPercentSensorParametersSpec();
+        ColumnStringsStringMatchRegexPercentSensorParametersSpec parametersSpec = new ColumnStringsStringMatchRegexPercentSensorParametersSpec();
         SensorDocumentationModel sensorDocumentation = this.sut.createSensorDocumentation(parametersSpec);
 
         Assertions.assertNotNull(sensorDocumentation);
         Assertions.assertEquals("Column level sensor that calculates the percent of values that fit to a regex in a column.", sensorDocumentation.getSensorParametersJavaDoc());
         Assertions.assertEquals("column", sensorDocumentation.getTarget());
         Assertions.assertEquals("strings", sensorDocumentation.getCategory());
-        Assertions.assertEquals("string_regex_match_percent", sensorDocumentation.getSensorName());
-        Assertions.assertEquals("column/strings/string_regex_match_percent", sensorDocumentation.getFullSensorName());
+        Assertions.assertEquals("string_match_regex_percent", sensorDocumentation.getSensorName());
+        Assertions.assertEquals("column/strings/string_match_regex_percent", sensorDocumentation.getFullSensorName());
 
         Assertions.assertNotNull(sensorDocumentation.getDefinition());
         Assertions.assertEquals(1, sensorDocumentation.getDefinition().getSpec().getFields().size());
