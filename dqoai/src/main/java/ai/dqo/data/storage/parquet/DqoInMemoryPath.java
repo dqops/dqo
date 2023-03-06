d5809fbba560be84e8834288dc0a05bbf8696d31
Piotr Czarnas <piotr.czarnas@documati.com>
2023-03-06 23:49:29 +0100

    Parquet write operations are buffered in-memory before saving to disk. No crc checksums written.

--- dqoai/src/main/java/ai/dqo/data/storage/parquet/DqoInMemoryPath.java
+++ dqoai/src/main/java/ai/dqo/data/storage/parquet/DqoInMemoryPath.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2021 DQO.ai (support@dqo.ai)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ai.dqo.data.storage.parquet;
 
 import ai.dqo.utils.exceptions.DqoRuntimeException;
@@ -13,15 +28,15 @@ import java.util.Objects;
  * Custom hadoop path to access files in the in-memory file system.
  */
 public class DqoInMemoryPath extends Path {
-    private InMemoryFileSystem fileSystem;
+    private FileSystem fileSystem;
 
-    public DqoInMemoryPath(URI aUri, InMemoryFileSystem fileSystem) {
+    public DqoInMemoryPath(URI aUri, FileSystem fileSystem) {
         super(aUri);
-        assert Objects.equals(aUri.getScheme(), "ramfs");
+//        assert Objects.equals(aUri.getScheme(), "ramfs");
         this.fileSystem = fileSystem;
     }
 
-    public DqoInMemoryPath(String path, InMemoryFileSystem fileSystem) {
+    public DqoInMemoryPath(String path, FileSystem fileSystem) {
         this(makeUri(path), fileSystem);
     }
 
--- dqoai/src/main/java/ai/dqo/data/storage/parquet/DqoTablesawParquetReader.java
+++ dqoai/src/main/java/ai/dqo/data/storage/parquet/DqoTablesawParquetReader.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2021 DQO.ai (support@dqo.ai)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ai.dqo.data.storage.parquet;
 
 /*-
@@ -20,7 +35,6 @@ package ai.dqo.data.storage.parquet;
  * #L%
  */
 
-import lombok.extern.slf4j.Slf4j;
 import net.tlabs.tablesaw.parquet.TablesawParquetReadOptions;
 import net.tlabs.tablesaw.parquet.TablesawParquetReader;
 import net.tlabs.tablesaw.parquet.TablesawReadSupport;
@@ -137,7 +151,7 @@ public class DqoTablesawParquetReader extends TablesawParquetReader {
      */
     private ParquetReader<Row> makeReaderFromStream(final InputStream inStream,
                                                     final TablesawReadSupport readSupport) throws IOException {
-        InMemoryFileSystem inMemoryFileSystem = new InMemoryFileSystem(new Path("ramfs://").toUri(), this.configuration);
+        DqoInMemoryFileSystem inMemoryFileSystem = new DqoInMemoryFileSystem(new Path("ramfs://").toUri(), this.configuration);
         DqoInMemoryPath inMemoryFilePath = new DqoInMemoryPath("ramfs://file.parquet", inMemoryFileSystem);
         try (FSDataOutputStream fsDataOutputStream = inMemoryFileSystem.create(inMemoryFilePath)) {
             IOUtils.copyLarge(inStream, fsDataOutputStream);
