a861747fb5b50c38e1e962bb919dd333e1edf9d2
Hoang Viet Do <hoang_viet_do@epam.com>
2023-03-09 15:59:12 +0100

    Renamed ad hoc to profiling

--- dqoai/src/main/frontend/src/Routes.tsx
+++ dqoai/src/main/frontend/src/Routes.tsx
@@ -36,8 +36,8 @@ const Routes = () => {
       <Route exact path={ROUTES.PATTERNS.CONNECTION} component={ConnectionPage} />
       <Route exact path={ROUTES.PATTERNS.SCHEMA} component={SchemaPage} />
       <Route exact path={ROUTES.PATTERNS.TABLE} component={TablePage} />
-      <Route exact path={ROUTES.PATTERNS.TABLE_AD_HOCS} component={TableProfilingsView} />
-      <Route exact path={ROUTES.PATTERNS.TABLE_AD_HOCS_FILTER} component={TableProfilingChecksUIFilterView} />
+      <Route exact path={ROUTES.PATTERNS.TABLE_PROFILINGS} component={TableProfilingsView} />
+      <Route exact path={ROUTES.PATTERNS.TABLE_PROFILINGS_FILTER} component={TableProfilingChecksUIFilterView} />
       <Route exact path={ROUTES.PATTERNS.TABLE_CHECKPOINTS_DAILY} component={TableDailyChecksView} />
       <Route exact path={ROUTES.PATTERNS.TABLE_CHECKPOINTS_MONTHLY} component={TableMonthlyChecksView} />
       <Route exact path={ROUTES.PATTERNS.TABLE_CHECKPOINTS_FILTER} component={TableCheckpointsUIFilterView} />
@@ -46,8 +46,8 @@ const Routes = () => {
       <Route exact path={ROUTES.PATTERNS.TABLE_PARTITIONED_FILTER} component={TablePartitionedChecksUIFilterView} />
       <Route exact path={ROUTES.PATTERNS.TABLE_COLUMNS} component={TableColumnsView} />
       <Route exact path={ROUTES.PATTERNS.COLUMN} component={ColumnView} />
-      <Route exact path={ROUTES.PATTERNS.COLUMN_AD_HOCS} component={ColumnProfilingsView} />
-      <Route exact path={ROUTES.PATTERNS.COLUMN_AD_HOCS_FILTER} component={ColumnProfilingChecksUIFilterView} />
+      <Route exact path={ROUTES.PATTERNS.COLUMN_PROFILINGS} component={ColumnProfilingsView} />
+      <Route exact path={ROUTES.PATTERNS.COLUMN_PROFILINGS_FILTER} component={ColumnProfilingChecksUIFilterView} />
       <Route exact path={ROUTES.PATTERNS.COLUMN_CHECKPOINTS_DAILY} component={ColumnDailyChecksView} />
       <Route exact path={ROUTES.PATTERNS.COLUMN_CHECKPOINTS_MONTHLY} component={ColumnMonthlyChecksView} />
       <Route exact path={ROUTES.PATTERNS.COLUMN_CHECKPOINTS_FILTER} component={ColumnCheckpointsUIFilterView} />
