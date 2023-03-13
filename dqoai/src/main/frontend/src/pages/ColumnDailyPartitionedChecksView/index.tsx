b1fe5b4f2b84de17a5f64e70785fe828a8c1e809
Lazar Miuchin <lazar.miuchin@dqo.ai>
2023-03-13 17:50:12 +0100

    feat: add description & sync icons for tabs

--- dqoai/src/main/frontend/src/pages/ColumnDailyPartitionedChecksView/index.tsx
+++ dqoai/src/main/frontend/src/pages/ColumnDailyPartitionedChecksView/index.tsx
@@ -77,8 +77,8 @@ const ColumnDailyPartitionedChecksView = () => {
     <ConnectionLayout>
       <div className="flex justify-between px-4 py-2 border-b border-gray-300 mb-2 min-h-14">
         <div className="flex items-center space-x-2">
-          <SvgIcon name="database" className="w-5 h-5" />
-          <div className="text-xl font-semibold">{`Daily Partitioned Checks for ${connectionName}.${schemaName}.${tableName}.${columnName}`}</div>
+          <SvgIcon name="column-check" className="w-5 h-5" />
+          <div className="text-xl font-semibold">{`Daily partition checks for ${connectionName}.${schemaName}.${tableName}.${columnName}`}</div>
         </div>
         <Button
           color={isUpdated ? 'primary' : 'secondary'}
