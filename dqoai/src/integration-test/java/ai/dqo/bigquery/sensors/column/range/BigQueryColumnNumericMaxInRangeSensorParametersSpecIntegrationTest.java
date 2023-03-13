cbca0ca96df103cf36311c0e176ee51ae8b3e438
Hoang Viet Do <hoang_viet_do@epam.com>
2023-03-13 14:45:22 +0100

    Added missing integration tests

--- dqoai/src/integration-test/java/ai/dqo/bigquery/sensors/column/range/BigQueryColumnNumericMaxInRangeSensorParametersSpecIntegrationTest.java
+++ dqoai/src/integration-test/java/ai/dqo/bigquery/sensors/column/range/BigQueryColumnNumericMaxInRangeSensorParametersSpecIntegrationTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ai.dqo.bigquery.sensors.column.numeric;
+package ai.dqo.bigquery.sensors.column.range;
 
 import ai.dqo.bigquery.BaseBigQueryIntegrationTest;
 import ai.dqo.checks.CheckTimeScale;
