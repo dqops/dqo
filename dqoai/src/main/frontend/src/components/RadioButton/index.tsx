8f97108287d98c016669be2080ece503831b486a
Lazar Miuchin <lazar.miuchin@dqo.ai>
2023-03-16 18:52:49 +0100

    fix: allow chose one option in settings for every check settings opened

--- dqoai/src/main/frontend/src/components/RadioButton/index.tsx
+++ dqoai/src/main/frontend/src/components/RadioButton/index.tsx
@@ -5,12 +5,13 @@ interface RadioButtonProps {
   label?: string;
   onClick: () => void;
   checked?: boolean;
+  className?: string;
 }
 
-const RadioButton = ({ label, onClick, checked }: RadioButtonProps) => {
+const RadioButton = ({ label, onClick, checked, className }: RadioButtonProps) => {
   return (
     <div
-      className="flex items-center space-x-2 cursor-pointer"
+      className={clsx("flex items-center space-x-2 cursor-pointer", className)}
       onClick={onClick}
     >
       <div className="w-5 h-5 border border-primary rounded-full flex items-center justify-center">
