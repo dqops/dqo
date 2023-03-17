ddb33ab8d4c6a726d48a15b23a9d7f00d0a5aee5
Hoang Viet Do <hoang_viet_do@epam.com>
2023-03-17 14:41:50 +0100

    fixed the diff_percent rule

--- home/rules/comparison/diff_percent.py
+++ home/rules/comparison/diff_percent.py
@@ -61,7 +61,7 @@ class RuleExecutionResult:
 
 # rule evaluation method that should be modified for each type of rule
 def evaluate_rule(rule_parameters: RuleExecutionRunParameters) -> RuleExecutionResult:
-    if not hasattr(rule_parameters, 'expected_value') nad hasattr(rule_parameters, 'actual_value'):
+    if not hasattr(rule_parameters, 'expected_value') and hasattr(rule_parameters, 'actual_value'):
         return RuleExecutionResult()
 
     expected_value = None
