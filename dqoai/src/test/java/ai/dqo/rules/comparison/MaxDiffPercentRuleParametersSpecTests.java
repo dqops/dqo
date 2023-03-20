7fbb8435751c9d8cf93f0f158a1df5e1340abb41
Hoang Viet Do <hoang_viet_do@epam.com>
2023-03-20 11:29:31 +0100

    fixed the diff_percent rule

---
+++ dqoai/src/test/java/ai/dqo/rules/comparison/MaxDiffPercentRuleParametersSpecTests.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright © 2021 DQO.ai (support@dqo.ai)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ai.dqo.rules.comparison;
+
+import ai.dqo.BaseTest;
+import ai.dqo.execution.rules.RuleExecutionResult;
+import ai.dqo.execution.rules.runners.python.PythonRuleRunnerObjectMother;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MaxDiffPercentRuleParametersSpecTests extends BaseTest {
+    private MaxDiffPercentRule1ParametersSpec sut;
+
+    @BeforeEach
+    void setUp() {
+        this.sut = new MaxDiffPercentRule1ParametersSpec();
+    }
+
+    @Test
+    void executeRule_whenActualValueIsAboveMinValue_thenReturnsPassed() {
+        this.sut.setMaxDiffPercent(1.0);
+        RuleExecutionResult ruleExecutionResult = PythonRuleRunnerObjectMother.executeBuiltInRule(1.0, this.sut);
+        Assertions.assertTrue(ruleExecutionResult.isPassed());
+        Assertions.assertNull(ruleExecutionResult.getExpectedValue());
+        Assertions.assertEquals(1.0, ruleExecutionResult.getLowerBound());
+        Assertions.assertNull(ruleExecutionResult.getUpperBound());
+    }
+
+    @Test
+    void executeRule_whenActualValueIsEqualMinValue_thenReturnsPassed() {
+        this.sut.setMaxDiffPercent(1.0);
+        RuleExecutionResult ruleExecutionResult = PythonRuleRunnerObjectMother.executeBuiltInRule(1.0, this.sut);
+        Assertions.assertTrue(ruleExecutionResult.isPassed());
+        Assertions.assertNull(ruleExecutionResult.getExpectedValue());
+        Assertions.assertEquals(1.0, ruleExecutionResult.getLowerBound());
+        Assertions.assertNull(ruleExecutionResult.getUpperBound());
+    }
+
+    @Test
+    void executeRule_whenActualValueIsBelowMinValue_thenReturnsFailed() {
+        this.sut.setMaxDiffPercent(1.0);
+        RuleExecutionResult ruleExecutionResult = PythonRuleRunnerObjectMother.executeBuiltInRule(1.0, this.sut);
+        Assertions.assertFalse(ruleExecutionResult.isPassed());
+        Assertions.assertNull(ruleExecutionResult.getExpectedValue());
+        Assertions.assertEquals(1.0, ruleExecutionResult.getLowerBound());
+        Assertions.assertNull(ruleExecutionResult.getUpperBound());
+    }
+
+    @Test
+    void isDirty_whenMinValueSet_thenIsDirtyIsTrue() {
+        this.sut.setMaxDiffPercent(1.0);
+        Assertions.assertEquals(1, this.sut.getMaxDiffPercent());
+        Assertions.assertTrue(this.sut.isDirty());
+        this.sut.clearDirty(true);
+        Assertions.assertFalse(this.sut.isDirty());
+    }
+
+    @Test
+    void isDirty_whenMinValueNumberSameAsCurrentSet_thenIsDirtyIsFalse() {
+        this.sut.setMaxDiffPercent(1.0);
+        Assertions.assertTrue(this.sut.isDirty());
+        this.sut.clearDirty(true);
+        Assertions.assertFalse(this.sut.isDirty());
+        this.sut.setMaxDiffPercent(1.0);
+        Assertions.assertFalse(this.sut.isDirty());
+    }
+
+    @Test
+    void executeRule_whenActualValueIsNull_thenReturnsPassed() {
+        RuleExecutionResult ruleExecutionResult = PythonRuleRunnerObjectMother.executeBuiltInRule(null, this.sut);
+        Assertions.assertTrue(ruleExecutionResult.isPassed());
+        Assertions.assertNull(ruleExecutionResult.getExpectedValue());
+        Assertions.assertNull(ruleExecutionResult.getLowerBound());
+        Assertions.assertNull(ruleExecutionResult.getUpperBound());
+    }
+}
