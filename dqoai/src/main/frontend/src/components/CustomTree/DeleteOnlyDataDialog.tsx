a8305e2bb659b3a59d7b36d05f7a0c19f013c38e
Lazar Miuchin <lazar.miuchin@dqo.ai>
2023-03-30 12:35:56 +0200

    feat: repair settings switching in "delete checks results" window

--- dqoai/src/main/frontend/src/components/CustomTree/DeleteOnlyDataDialog.tsx
+++ dqoai/src/main/frontend/src/components/CustomTree/DeleteOnlyDataDialog.tsx
@@ -47,73 +47,77 @@ const DeleteOnlyDataDialog = ({ open, onClose, onDelete }: DeleteOnlyDataDialogP
     <Dialog open={open} handler={onClose} className="min-w-200 p-4">
       <DialogHeader className="font-bold text-center justify-center">Delete all collected results for the time range:</DialogHeader>
       <DialogBody>
-        <Radio
-          id="all"
-          name="delete_mode"
-          value="all"
-          label="All"
-          checked={mode === 'all'}
-          onChange={(e) => setMode('all')}
-          className="outline-none"
-          color="teal"
-        />
-        <div>
-          <div className="flex mt-4 items-center">
+        <div className="flex flex-col">
+          <div>
             <Radio
-              id="part"
+              id="all"
               name="delete_mode"
-              value="part"
-              checked={mode === 'part'}
-              onChange={(e) => setMode('part')}
+              value="all"
+              label="All"
+              checked={mode === 'all'}
+              onChange={() => setMode('all')}
+              className="outline-none"
               color="teal"
             />
-            <div className="flex space-x-6 items-center">
-              <DatePicker
-                showIcon
-                placeholderText='Select date start'
-                onChange={setStartDate}
-                selected={startDate}
-                disabled={isDisabled}
+          </div>
+          <div>
+            <div className="flex mt-4 items-center">
+              <Radio
+                id="part"
+                name="delete_mode"
+                value="part"
+                checked={mode === 'part'}
+                onChange={() => setMode('part')}
+                color="teal"
               />
-              <span>to</span>
-              <DatePicker
-                showIcon
-                placeholderText='Select date end'
-                onChange={setEndDate}
-                selected={endDate}
+              <div className="flex space-x-6 items-center">
+                <DatePicker
+                  showIcon
+                  placeholderText='Select date start'
+                  onChange={setStartDate}
+                  selected={startDate}
+                  disabled={isDisabled}
+                />
+                <span>to</span>
+                <DatePicker
+                  showIcon
+                  placeholderText='Select date end'
+                  onChange={setEndDate}
+                  selected={endDate}
+                  disabled={isDisabled}
+                />
+              </div>
+            </div>
+            <div className="flex flex-col gap-4 px-4 my-4">
+              <Checkbox
+                checked={params.deleteErrors}
+                onChange={(deleteErrors) => onChangeParams({ deleteErrors })}
+                label="Error"
                 disabled={isDisabled}
+                checkClassName="bg-teal-500"
+              />
+              <Checkbox
+                checked={params.deleteProfilingResults}
+                onChange={(deleteProfilingResults) => onChangeParams({ deleteProfilingResults })}
+                label="Profiling Results"
+                disabled={isDisabled}
+                checkClassName="bg-teal-500"
+              />
+              <Checkbox
+                checked={params.deleteRuleResults}
+                onChange={(deleteRuleResults) => onChangeParams({ deleteRuleResults })}
+                label="Rule Results"
+                disabled={isDisabled}
+                checkClassName="bg-teal-500"
+              />
+              <Checkbox
+                checked={params.deleteSensorReadouts}
+                onChange={(deleteSensorReadouts) => onChangeParams({ deleteSensorReadouts })}
+                label="Sensor Readouts"
+                disabled={isDisabled}
+                checkClassName="bg-teal-500"
               />
             </div>
-          </div>
-          <div className="flex flex-col gap-4 px-4 my-4">
-            <Checkbox
-              checked={params.deleteErrors}
-              onChange={(deleteErrors) => onChangeParams({ deleteErrors })}
-              label="Error"
-              disabled={isDisabled}
-              checkClassName="bg-teal-500"
-            />
-            <Checkbox
-              checked={params.deleteProfilingResults}
-              onChange={(deleteProfilingResults) => onChangeParams({ deleteProfilingResults })}
-              label="Profiling Results"
-              disabled={isDisabled}
-              checkClassName="bg-teal-500"
-            />
-            <Checkbox
-              checked={params.deleteRuleResults}
-              onChange={(deleteRuleResults) => onChangeParams({ deleteRuleResults })}
-              label="Rule Results"
-              disabled={isDisabled}
-              checkClassName="bg-teal-500"
-            />
-            <Checkbox
-              checked={params.deleteSensorReadouts}
-              onChange={(deleteSensorReadouts) => onChangeParams({ deleteSensorReadouts })}
-              label="Sensor Readouts"
-              disabled={isDisabled}
-              checkClassName="bg-teal-500"
-            />
           </div>
         </div>
       </DialogBody>
