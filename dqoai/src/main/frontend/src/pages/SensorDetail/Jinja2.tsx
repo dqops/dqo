ee2bb59a0e931e3ca55022f0fe15dfda604abfba
Lazar Miuchin <lazar.miuchin@dqo.ai>
2023-04-28 14:10:53 +0200

    feat: add actions to edit sensor screen

--- dqoai/src/main/frontend/src/pages/SensorDetail/Jinja2.tsx
+++ dqoai/src/main/frontend/src/pages/SensorDetail/Jinja2.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import CodeMirror from '@uiw/react-codemirror';
 import { ProviderSensorModel } from "../../api";
-import { RuleActionGroup } from "../../components/Sensors/RuleActionGroup";
 import { StreamLanguage } from '@codemirror/language';
 import { jinja2 } from '@codemirror/legacy-modes/mode/jinja2';
 
@@ -13,8 +12,6 @@ type Jinja2Props = {
 const Jinja2Code = ({ providerSensor, onChange }: Jinja2Props) => {
   return (
     <div className="flex-1 overflow-auto max-h-100 min-h-100">
-      <RuleActionGroup />
-
       <CodeMirror
         value={providerSensor?.sqlTemplate}
         onChange={(value) => onChange({ sqlTemplate: value })}
