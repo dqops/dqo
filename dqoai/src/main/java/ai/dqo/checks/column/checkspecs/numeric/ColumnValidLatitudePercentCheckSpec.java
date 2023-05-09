1edf9ac9e7844c3fc4c540c2d98e23862d74e9f5
Andrzej Nagalski <andrzej.nagalski@documati.com>
2023-05-09 11:52:33 +0200

    Fixed wrongly assigned rules in valid latitude and longitude checks

--- dqoai/src/main/java/ai/dqo/checks/column/checkspecs/numeric/ColumnValidLatitudePercentCheckSpec.java
+++ dqoai/src/main/java/ai/dqo/checks/column/checkspecs/numeric/ColumnValidLatitudePercentCheckSpec.java
@@ -19,9 +19,9 @@ import ai.dqo.checks.AbstractCheckSpec;
 import ai.dqo.checks.DefaultDataQualityDimensions;
 import ai.dqo.metadata.id.ChildHierarchyNodeFieldMap;
 import ai.dqo.metadata.id.ChildHierarchyNodeFieldMapImpl;
-import ai.dqo.rules.comparison.MaxPercentRule95ParametersSpec;
-import ai.dqo.rules.comparison.MaxPercentRule98ParametersSpec;
-import ai.dqo.rules.comparison.MaxPercentRule99ParametersSpec;
+import ai.dqo.rules.comparison.MinPercentRule95ParametersSpec;
+import ai.dqo.rules.comparison.MinPercentRule98ParametersSpec;
+import ai.dqo.rules.comparison.MinPercentRule99ParametersSpec;
 import ai.dqo.sensors.column.numeric.ColumnNumericValidLatitudePercentSensorParametersSpec;
 import ai.dqo.utils.serialization.IgnoreEmptyYamlSerializer;
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -40,7 +40,7 @@ import java.util.Objects;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @EqualsAndHashCode(callSuper = true)
 public class ColumnValidLatitudePercentCheckSpec
-        extends AbstractCheckSpec<ColumnNumericValidLatitudePercentSensorParametersSpec, MaxPercentRule99ParametersSpec, MaxPercentRule98ParametersSpec, MaxPercentRule95ParametersSpec> {
+        extends AbstractCheckSpec<ColumnNumericValidLatitudePercentSensorParametersSpec, MinPercentRule99ParametersSpec, MinPercentRule98ParametersSpec, MinPercentRule95ParametersSpec> {
     public static final ChildHierarchyNodeFieldMapImpl<ColumnValidLatitudePercentCheckSpec> FIELDS = new ChildHierarchyNodeFieldMapImpl<>(AbstractCheckSpec.FIELDS) {
         {
         }
@@ -54,17 +54,17 @@ public class ColumnValidLatitudePercentCheckSpec
     @JsonPropertyDescription("Alerting threshold that raises a data quality warning that is considered as a passed data quality check")
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     @JsonSerialize(using = IgnoreEmptyYamlSerializer.class)
-    private MaxPercentRule99ParametersSpec warning;
+    private MinPercentRule99ParametersSpec warning;
 
     @JsonPropertyDescription("Default alerting threshold for a set percentage of rows with valid latitude value in a column that raises a data quality alert")
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     @JsonSerialize(using = IgnoreEmptyYamlSerializer.class)
-    private MaxPercentRule98ParametersSpec error;
+    private MinPercentRule98ParametersSpec error;
 
     @JsonPropertyDescription("Alerting threshold that raises a fatal data quality issue which indicates a serious data quality problem")
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     @JsonSerialize(using = IgnoreEmptyYamlSerializer.class)
-    private MaxPercentRule95ParametersSpec fatal;
+    private MinPercentRule95ParametersSpec fatal;
 
     /**
      * Returns the parameters of the sensor.
@@ -93,7 +93,7 @@ public class ColumnValidLatitudePercentCheckSpec
      * @return Warning severity rule parameters.
      */
     @Override
-    public MaxPercentRule99ParametersSpec getWarning() {
+    public MinPercentRule99ParametersSpec getWarning() {
         return this.warning;
     }
 
@@ -102,7 +102,7 @@ public class ColumnValidLatitudePercentCheckSpec
      *
      * @param warning Warning alerting threshold to set.
      */
-    public void setWarning(MaxPercentRule99ParametersSpec warning) {
+    public void setWarning(MinPercentRule99ParametersSpec warning) {
         this.setDirtyIf(!Objects.equals(this.warning, warning));
         this.warning = warning;
         this.propagateHierarchyIdToField(warning, "warning");
@@ -114,7 +114,7 @@ public class ColumnValidLatitudePercentCheckSpec
      * @return Default "error" alerting thresholds.
      */
     @Override
-    public MaxPercentRule98ParametersSpec getError() {
+    public MinPercentRule98ParametersSpec getError() {
         return this.error;
     }
 
@@ -123,7 +123,7 @@ public class ColumnValidLatitudePercentCheckSpec
      *
      * @param error Error alerting threshold to set.
      */
-    public void setError(MaxPercentRule98ParametersSpec error) {
+    public void setError(MinPercentRule98ParametersSpec error) {
         this.setDirtyIf(!Objects.equals(this.error, error));
         this.error = error;
         this.propagateHierarchyIdToField(error, "error");
@@ -135,7 +135,7 @@ public class ColumnValidLatitudePercentCheckSpec
      * @return Fatal severity rule parameters.
      */
     @Override
-    public MaxPercentRule95ParametersSpec getFatal() {
+    public MinPercentRule95ParametersSpec getFatal() {
         return this.fatal;
     }
 
@@ -144,7 +144,7 @@ public class ColumnValidLatitudePercentCheckSpec
      *
      * @param fatal Fatal alerting threshold to set.
      */
-    public void setFatal(MaxPercentRule95ParametersSpec fatal) {
+    public void setFatal(MinPercentRule95ParametersSpec fatal) {
         this.setDirtyIf(!Objects.equals(this.fatal, fatal));
         this.fatal = fatal;
         this.propagateHierarchyIdToField(fatal, "fatal");
