b4789fce37ba960afae2a5f77139831b112a17d4
Aleksy <psychologianauki@gmail.com>
2023-05-10 13:36:54 +0200

    first commi, deleted child of synchronize multiple folders

--- dqoai/src/main/frontend/src/components/NotificationMenu/JobItem/index.tsx
+++ dqoai/src/main/frontend/src/components/NotificationMenu/JobItem/index.tsx
@@ -1,11 +1,19 @@
-import { DqoJobHistoryEntryModel, DqoJobHistoryEntryModelStatusEnum } from "../../../api";
-import React, { useState } from "react";
-import SvgIcon from "../../SvgIcon";
-import { Accordion, AccordionBody, AccordionHeader } from "@material-tailwind/react";
-import moment from "moment";
+import {
+  DqoJobHistoryEntryModel,
+  DqoJobHistoryEntryModelStatusEnum
+} from '../../../api';
+import React, { useState } from 'react';
+import SvgIcon from '../../SvgIcon';
+import {
+  Accordion,
+  AccordionBody,
+  AccordionHeader
+} from '@material-tailwind/react';
+import moment from 'moment';
 
 const JobItem = ({ job }: { job: DqoJobHistoryEntryModel }) => {
   const [open, setOpen] = useState(false);
+  const [open2, setOpen2] = useState(false);
 
   const renderValue = (value: any) => {
     if (typeof value === 'boolean') {
@@ -37,124 +45,132 @@ const JobItem = ({ job }: { job: DqoJobHistoryEntryModel }) => {
 
   return (
     <Accordion open={open}>
-      <AccordionHeader onClick={() => setOpen(!open)}>
-        <div className="flex justify-between items-center text-sm w-full text-gray-700">
-          <div className="flex space-x-1 items-center">
-            <div>{job.jobType}</div>
-            {renderStatus()}
+      {job.jobType !== 'synchronize folder' ? (
+        <AccordionHeader onClick={() => setOpen(!open)}>
+          <div className="flex justify-between items-center text-sm w-full text-gray-700">
+            <div className="flex space-x-1 items-center">
+              <div>{job.jobType}</div>
+              {renderStatus()}
+            </div>
+            <div>
+              {moment(job?.statusChangedAt).format('YYYY-MM-DD HH:mm:ss')}
+            </div>
           </div>
-          <div>
-            {moment(job?.statusChangedAt).format('YYYY-MM-DD HH:mm:ss')}
-          </div>
-        </div>
-      </AccordionHeader>
+        </AccordionHeader>
+      ) : (
+        ''
+      )}
       <AccordionBody>
         <table className="text-gray-700">
           <tbody>
-          <tr>
-            <td className="px-2 capitalize">Status</td>
-            <td className="px-2 max-w-76">
-              {job?.status}
-            </td>
-          </tr>
-          <tr>
-            <td className="px-2 capitalize">Last Changed</td>
-            <td className="px-2 max-w-76">
-              {moment(job?.statusChangedAt).format('YYYY-MM-DD HH:mm:ss')}
-            </td>
-          </tr>
-          {job?.errorMessage && (
-            <>
-              <tr>
-                <td className="px-2 capitalize">Error Message</td>
-                <td className="px-2 max-w-76">
-                  {job?.errorMessage}
-                </td>
-              </tr>
-            </>
-          )}
-          {job?.parameters?.runChecksParameters?.checkSearchFilters &&
-          Object.entries(
-            job?.parameters?.runChecksParameters?.checkSearchFilters
-          ).map(([key, value], index) => (
-            <tr key={index}>
-              <td className="px-2 capitalize">{key}</td>
-              <td className="px-2 max-w-76">{renderValue(value)}</td>
+            <tr>
+              <td className="px-2 capitalize">Status</td>
+              <td className="px-2 max-w-76">{job?.status}</td>
             </tr>
-          ))}
-          {job?.parameters?.importSchemaParameters &&(
-            <>
-              <tr>
-                <td className="px-2 capitalize">Connection Name</td>
-                <td className="px-2 max-w-76">
-                  {job?.parameters?.importSchemaParameters?.connectionName}
-                </td>
-              </tr>
-              <tr>
-                <td className="px-2 capitalize">Schema Name</td>
-                <td className="px-2 max-w-76">
-                  {job?.parameters?.importSchemaParameters?.schemaName}
-                </td>
-              </tr>
-              <tr>
-                <td className="px-2 capitalize align-top">Tables pattern</td>
-                <td className="px-2 max-w-76">
-                  {job?.parameters?.importSchemaParameters?.tableNamePattern}
-                </td>
-              </tr>
-            </>
-          )}
-          {job?.parameters?.synchronizeRootFolderParameters &&(
-            <>
-              <tr>
-                <td className="px-2 capitalize">Synchronized folder</td>
-                <td className="px-2 max-w-76">
-                  {job?.parameters?.synchronizeRootFolderParameters?.synchronizationParameter?.folder}
-                </td>
-              </tr>
-              <tr>
-                <td className="px-2 capitalize">Synchronization direction</td>
-                <td className="px-2 max-w-76">
-                  {job?.parameters?.synchronizeRootFolderParameters?.synchronizationParameter?.direction}
-                </td>
-              </tr>
-            </>
-          )}
-          {job?.parameters?.collectStatisticsParameters?.statisticsCollectorSearchFilters &&
-          Object.entries(
-            job?.parameters?.collectStatisticsParameters?.statisticsCollectorSearchFilters
-          ).map(([key, value], index) => (
-            <tr key={index}>
-              <td className="px-2 capitalize">{key}</td>
-              <td className="px-2 max-w-76">{renderValue(value)}</td>
+            <tr>
+              <td className="px-2 capitalize">Last Changed</td>
+              <td className="px-2 max-w-76">
+                {moment(job?.statusChangedAt).format('YYYY-MM-DD HH:mm:ss')}
+              </td>
             </tr>
-          ))}
-          {job?.parameters?.importTableParameters && (
-            <>
-              <tr>
-                <td className="px-2 capitalize">Connection Name</td>
-                <td className="px-2 max-w-76">
-                  {job?.parameters?.importTableParameters?.connectionName}
-                </td>
-              </tr>
-              <tr>
-                <td className="px-2 capitalize">Schema Name</td>
-                <td className="px-2 max-w-76">
-                  {job?.parameters?.importTableParameters?.schemaName}
-                </td>
-              </tr>
-              <tr>
-                <td className="px-2 capitalize align-top">Tables</td>
-                <td className="px-2 max-w-76">
-                  {job?.parameters?.importTableParameters?.tableNames?.map(
-                    (item, index) => (
-                      <div key={index}>{item}</div>
-                    )
-                  )}
-                </td>
-              </tr>
-            </>
-          )}
+            {job?.errorMessage && (
+              <>
+                <tr>
+                  <td className="px-2 capitalize">Error Message</td>
+                  <td className="px-2 max-w-76">{job?.errorMessage}</td>
+                </tr>
+              </>
+            )}
+            {job?.parameters?.runChecksParameters?.checkSearchFilters &&
+              Object.entries(
+                job?.parameters?.runChecksParameters?.checkSearchFilters
+              ).map(([key, value], index) => (
+                <tr key={index}>
+                  <td className="px-2 capitalize">{key}</td>
+                  <td className="px-2 max-w-76">{renderValue(value)}</td>
+                </tr>
+              ))}
+            {job?.parameters?.importSchemaParameters && (
+              <>
+                <tr>
+                  <td className="px-2 capitalize">Connection Name</td>
+                  <td className="px-2 max-w-76">
+                    {job?.parameters?.importSchemaParameters?.connectionName}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="px-2 capitalize">Schema Name</td>
+                  <td className="px-2 max-w-76">
+                    {job?.parameters?.importSchemaParameters?.schemaName}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="px-2 capitalize align-top">Tables pattern</td>
+                  <td className="px-2 max-w-76">
+                    {job?.parameters?.importSchemaParameters?.tableNamePattern}
+                  </td>
+                </tr>
+              </>
+            )}
+            {job?.parameters?.synchronizeRootFolderParameters && (
+              <>
+                <tr>
+                  <td className="px-2 capitalize">Synchronized folder</td>
+                  <td className="px-2 max-w-76">
+                    {
+                      job?.parameters?.synchronizeRootFolderParameters
+                        ?.synchronizationParameter?.folder
+                    }
+                  </td>
+                </tr>
+                <tr>
+                  <td className="px-2 capitalize">Synchronization direction</td>
+                  <td className="px-2 max-w-76">
+                    {
+                      job?.parameters?.synchronizeRootFolderParameters
+                        ?.synchronizationParameter?.direction
+                    }
+                  </td>
+                </tr>
+              </>
+            )}
+            {job?.parameters?.collectStatisticsParameters
+              ?.statisticsCollectorSearchFilters &&
+              Object.entries(
+                job?.parameters?.collectStatisticsParameters
+                  ?.statisticsCollectorSearchFilters
+              ).map(([key, value], index) => (
+                <tr key={index}>
+                  <td className="px-2 capitalize">{key}</td>
+                  <td className="px-2 max-w-76">{renderValue(value)}</td>
+                </tr>
+              ))}
+            {job?.parameters?.importTableParameters && (
+              <>
+                <tr>
+                  <td className="px-2 capitalize">Connection Name</td>
+                  <td className="px-2 max-w-76">
+                    {job?.parameters?.importTableParameters?.connectionName}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="px-2 capitalize">Schema Name</td>
+                  <td className="px-2 max-w-76">
+                    {job?.parameters?.importTableParameters?.schemaName}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="px-2 capitalize align-top">Tables</td>
+                  <td className="px-2 max-w-76">
+                    {job?.parameters?.importTableParameters?.tableNames?.map(
+                      (item, index) => (
+                        <div key={index}>{item}</div>
+                      )
+                    )}
+                  </td>
+                </tr>
+              </>
+            )}
           </tbody>
         </table>
       </AccordionBody>
--- dqoai/src/main/frontend/src/components/NotificationMenu/index.tsx
+++ dqoai/src/main/frontend/src/components/NotificationMenu/index.tsx
@@ -11,10 +11,10 @@ import { useSelector } from 'react-redux';
 import { IRootState } from '../../redux/reducers';
 import { useActionDispatch } from '../../hooks/useActionDispatch';
 import { toggleMenu } from '../../redux/actions/job.actions';
-import { useError, IError } from "../../contexts/errrorContext";
-import JobItem from "./JobItem";
-import ErrorItem from "./ErrorItem";
-import moment from "moment";
+import { useError, IError } from '../../contexts/errrorContext';
+import JobItem from './JobItem';
+import ErrorItem from './ErrorItem';
+import moment from 'moment';
 
 const NotificationMenu = () => {
   const { jobs, isOpen } = useSelector((state: IRootState) => state.job);
@@ -37,16 +37,18 @@ const NotificationMenu = () => {
 
   const getNotificationDate = (notification: any) => {
     if (notification.type === 'job') {
-      return notification.item.jobId?.createdAt
+      return notification.item.jobId?.createdAt;
     }
-    return notification.item.date
+    return notification.item.date;
   };
 
   const data = useMemo(() => {
     const jobsData = jobs?.jobs
-      ? jobs?.jobs.sort((a, b) => {
-        return (b.jobId?.jobId || 0) - (a.jobId?.jobId || 0);
-      }).map((item) => ({ type: 'job', item }))
+      ? jobs?.jobs
+          .sort((a, b) => {
+            return (b.jobId?.jobId || 0) - (a.jobId?.jobId || 0);
+          })
+          .map((item) => ({ type: 'job', item }))
       : [];
     const errorData = errors.map((item: IError) => ({ type: 'error', item }));
 
@@ -62,11 +64,14 @@ const NotificationMenu = () => {
     return newData;
   }, [jobs, errors]);
 
-
   return (
     <Popover placement="bottom-end" open={isOpen} handler={toggleOpen}>
       <PopoverHandler>
-        <IconButton className="!mr-3 !bg-transparent" ripple={false} variant="text">
+        <IconButton
+          className="!mr-3 !bg-transparent"
+          ripple={false}
+          variant="text"
+        >
           <div className="relative">
             <SvgIcon name="bell" className="w-5 h-5 text-gray-700" />
             <span className="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 rounded-full bg-teal-500 text-white px-1 py-0.5 text-xxs">
@@ -80,11 +85,13 @@ const NotificationMenu = () => {
           <div>Notifications</div>
         </div>
         <div className="overflow-auto max-h-100 py-4 px-4">
-          {data.map((notification: any, index) => notification.type === 'error' ? (
-            <ErrorItem error={notification.item} key={index} />
-          ) : (
-            <JobItem job={notification.item} key={index} />
-          ))}
+          {data.map((notification: any, index) =>
+            notification.type === 'error' ? (
+              <ErrorItem error={notification.item} key={index} />
+            ) : (
+              <JobItem job={notification.item} key={index} />
+            )
+          )}
         </div>
       </PopoverContent>
     </Popover>
