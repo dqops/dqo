6c2b782baf3dac99b4cd5d0b30fc767dd2a39e1d
Rafal Klimek <rafal.klimek@dqo.ai>
2023-05-15 14:09:32 +0200

    Rename numeric sensors to reflect names in home

--- dqoai/src/main/java/ai/dqo/checks/column/checkspecs/numeric/ColumnPercentile10InRangeCheckSpec.java
+++ dqoai/src/main/java/ai/dqo/checks/column/checkspecs/numeric/ColumnPercentile10InRangeCheckSpec.java
@@ -20,7 +20,7 @@ import ai.dqo.checks.DefaultDataQualityDimensions;
 import ai.dqo.metadata.id.ChildHierarchyNodeFieldMap;
 import ai.dqo.metadata.id.ChildHierarchyNodeFieldMapImpl;
 import ai.dqo.rules.comparison.BetweenFloatsRuleParametersSpec;
-import ai.dqo.sensors.column.numeric.ColumnNumericPercentile10InRangeSensorParametersSpec;
+import ai.dqo.sensors.column.numeric.ColumnNumericPercentile10SensorParametersSpec;
 import ai.dqo.utils.serialization.IgnoreEmptyYamlSerializer;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
@@ -38,7 +38,7 @@ import java.util.Objects;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @EqualsAndHashCode(callSuper = true)
 public class ColumnPercentile10InRangeCheckSpec
-        extends AbstractCheckSpec<ColumnNumericPercentile10InRangeSensorParametersSpec, BetweenFloatsRuleParametersSpec, BetweenFloatsRuleParametersSpec, BetweenFloatsRuleParametersSpec> {
+        extends AbstractCheckSpec<ColumnNumericPercentile10SensorParametersSpec, BetweenFloatsRuleParametersSpec, BetweenFloatsRuleParametersSpec, BetweenFloatsRuleParametersSpec> {
     public static final ChildHierarchyNodeFieldMapImpl<ColumnPercentile10InRangeCheckSpec> FIELDS = new ChildHierarchyNodeFieldMapImpl<>(AbstractCheckSpec.FIELDS) {
         {
         }
@@ -47,7 +47,7 @@ public class ColumnPercentile10InRangeCheckSpec
     @JsonPropertyDescription("Data quality check parameters")
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     @JsonSerialize(using = IgnoreEmptyYamlSerializer.class)
-    private ColumnNumericPercentile10InRangeSensorParametersSpec parameters = new ColumnNumericPercentile10InRangeSensorParametersSpec();
+    private ColumnNumericPercentile10SensorParametersSpec parameters = new ColumnNumericPercentile10SensorParametersSpec();
 
     @JsonPropertyDescription("Alerting threshold that raises a data quality warning that is considered as a passed data quality check")
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
@@ -69,7 +69,7 @@ public class ColumnPercentile10InRangeCheckSpec
      * @return Sensor parameters.
      */
     @Override
-    public ColumnNumericPercentile10InRangeSensorParametersSpec getParameters() {
+    public ColumnNumericPercentile10SensorParametersSpec getParameters() {
         return parameters;
     }
 
@@ -77,7 +77,7 @@ public class ColumnPercentile10InRangeCheckSpec
      * Sets a new row count sensor parameter object.
      * @param parameters Row count parameters.
      */
-    public void setParameters(ColumnNumericPercentile10InRangeSensorParametersSpec parameters) {
+    public void setParameters(ColumnNumericPercentile10SensorParametersSpec parameters) {
         this.setDirtyIf(!Objects.equals(this.parameters, parameters));
         this.parameters = parameters;
         this.propagateHierarchyIdToField(parameters, "parameters");
