445f2a37ae1438ee23caeb78bc86e12bb4e08a16
Piotr Czarnas <piotr.czarnas@gmail.com>
2023-05-22 17:05:22 +0200

    string and number in set checks moved to Consistency dimension.

--- dqoai/src/main/java/ai/dqo/checks/column/checkspecs/numeric/ColumnNumbersInSetPercentCheckSpec.java
+++ dqoai/src/main/java/ai/dqo/checks/column/checkspecs/numeric/ColumnNumbersInSetPercentCheckSpec.java
@@ -162,6 +162,6 @@ public class ColumnNumbersInSetPercentCheckSpec
      */
     @Override
     public DefaultDataQualityDimensions getDefaultDataQualityDimension() {
-        return DefaultDataQualityDimensions.Completeness;
+        return DefaultDataQualityDimensions.Consistency;
     }
 }
