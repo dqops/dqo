38f89ee494692a3145dad87057f1ebb81017577a
Rafal Klimek <rafal.klimek@dqo.ai>
2023-05-23 14:33:55 +0200

    between_change_X_days rules

---
+++ dqoai/src/main/java/ai/dqo/rules/change/BetweenChange1DayRuleParametersSpec.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright © 2021 DQO.ai (support@dqo.ai)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ai.dqo.rules.change;
+
+import ai.dqo.metadata.fields.SampleValues;
+import ai.dqo.metadata.id.ChildHierarchyNodeFieldMap;
+import ai.dqo.metadata.id.ChildHierarchyNodeFieldMapImpl;
+import ai.dqo.rules.AbstractRuleParametersSpec;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.EqualsAndHashCode;
+
+import java.util.Objects;
+
+/**
+ * Data quality rule that verifies if data quality sensor readout value changed by a value between the provided bounds compared to yesterday.
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@EqualsAndHashCode(callSuper = true)
+public class BetweenChange1DayRuleParametersSpec extends AbstractRuleParametersSpec {
+    private static final ChildHierarchyNodeFieldMapImpl<BetweenChange1DayRuleParametersSpec> FIELDS = new ChildHierarchyNodeFieldMapImpl<>(AbstractRuleParametersSpec.FIELDS) {
+        {
+        }
+    };
+
+
+    /**
+     * Default constructor.
+     */
+    public BetweenChange1DayRuleParametersSpec() {
+    }
+
+    @JsonPropertyDescription("Minimal accepted change with regards to the previous readout (inclusive).")
+    @SampleValues(values = "10")
+    private Double from;
+
+    @JsonPropertyDescription("Maximal accepted change with regards to the previous readout (inclusive).")
+    @SampleValues(values = "20")
+    private Double to;
+
+    @JsonPropertyDescription("Whether to compare the actual value to the readout exactly 1 day in the past. " +
+            "If the flag is false, the rule searches for the newest readout, 60 days in the past, having skipped the readouts from the current day.")
+    @SampleValues(values = "false")
+    private Boolean exact = false;
+
+    /**
+     * Gets the lower bound for the accepted change for a data quality check readout.
+     * @return Minimal accepted change for a data quality check readout.
+     */
+    public Double getFrom() {
+        return from;
+    }
+
+    /**
+     * Changes the lower bound for a data quality readout's change.
+     * @param from New lower bound.
+     */
+    public void setFrom(Double from) {
+        this.setDirtyIf(!Objects.equals(this.from, from));
+        this.from = from;
+    }
+
+    /**
+     * Gets the upper bound for the accepted change for a data quality check readout.
+     * @return Maximal accepted change for a data quality check readout.
+     */
+    public Double getTo() {
+        return to;
+    }
+
+    /**
+     * Changes the upper bound for a data quality readout's change.
+     * @param to New upper bound.
+     */
+    public void setTo(Double to) {
+        this.setDirtyIf(!Objects.equals(this.to, to));
+        this.to = to;
+    }
+
+    /**
+     * Gets the flag that makes the rule abstain from searching for the latest readout if there was no readout on the previous day.
+     * @return Flag <code>exact</code>'s value.
+     */
+    public Boolean getExact() {
+        return exact;
+    }
+
+    /**
+     * Set the flag that makes the rule abstain from searching for the latest readout if there was no readout on the previous day.
+     * @param exact New flag <code>exact</code>'s value.
+     */
+    public void setExact(Boolean exact) {
+        this.exact = exact;
+    }
+
+    /**
+     * Returns the child map on the spec class with all fields.
+     *
+     * @return Return the field map.
+     */
+    @Override
+    protected ChildHierarchyNodeFieldMap getChildMap() {
+        return FIELDS;
+    }
+
+    /**
+     * Returns a rule definition name. It is a name of a python module (file) without the ".py" extension. Rule names are related to the "rules" folder in DQO_HOME.
+     *
+     * @return Rule definition name (python module name without .py extension).
+     */
+    @Override
+    public String getRuleDefinitionName() {
+        return "change/between_change_1_day";
+    }
+}
