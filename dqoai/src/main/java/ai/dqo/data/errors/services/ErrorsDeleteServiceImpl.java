2b05e276df9f1a8b9263a3b390a6815ad6ba1162
Rafal Klimek <rafal.klimek@dqo.ai>
2023-05-25 17:11:41 +0200

    Use new SearchPatterns in data delete services

--- dqoai/src/main/java/ai/dqo/data/errors/services/ErrorsDeleteServiceImpl.java
+++ dqoai/src/main/java/ai/dqo/data/errors/services/ErrorsDeleteServiceImpl.java
@@ -20,15 +20,11 @@ import ai.dqo.data.errors.models.ErrorsFragmentFilter;
 import ai.dqo.data.errors.snapshot.ErrorsSnapshot;
 import ai.dqo.data.errors.snapshot.ErrorsSnapshotFactory;
 import ai.dqo.data.models.DataDeleteResult;
-import ai.dqo.data.models.DataDeleteResultPartition;
-import ai.dqo.data.normalization.CommonColumnNames;
-import ai.dqo.data.storage.LoadedMonthlyPartition;
-import ai.dqo.data.storage.ParquetPartitionId;
+import ai.dqo.data.storage.FileStorageSettings;
 import ai.dqo.data.storage.ParquetPartitionMetadataService;
 import ai.dqo.metadata.sources.PhysicalTableName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import tech.tablesaw.api.Table;
 
 import java.time.LocalDate;
 import java.util.*;
@@ -61,71 +57,57 @@ public class ErrorsDeleteServiceImpl implements ErrorsDeleteService {
             Set<String> wrappedValue = new HashSet<>(){{add(columnValue);}};
             conditions.put(columnName, wrappedValue);
         }
-        // TODO: up to this point można wyjebać do czegoś co korzysta z ParquetDataFragmentFilter
 
         if (filter.getColumnNames() != null && !filter.getColumnNames().isEmpty()) {
             conditions.put(ErrorsColumnNames.COLUMN_NAME_COLUMN_NAME, new HashSet<>(filter.getColumnNames()));
-        } // UNIKALNA NAZWA KOLUMNY! tego się nie wyrzuci
+        }
 
         DataDeleteResult dataDeleteResult = new DataDeleteResult();
 
-        Collection<PhysicalTableName> tablesToDelete;
-        if (filter.getTableSearchFilters().getSchemaTableName() == null) {
-            tablesToDelete = this.parquetPartitionMetadataService.listTablesForConnection(
-                    filter.getTableSearchFilters().getConnectionName(),
-                    ErrorsSnapshot.createErrorsStorageSettings()
-            );
-            // TODO: w praktyce chcemy uwspolnic te czesc i robić tak:
-            //  Listuj tabele, pofiltruj zgodnie z dostarczonym patternem (jeśli nie ma, to *.*),
-            //  no i te co zostaną usuń.
-            //  Taki filtrownik może być osobnym serwisem, całkiem ogólna funkcjonalność.
-            //  Albo lepiej, utility klasa do patternow, po prostu dajesz jej nazwe i ona ci mowi czy wchodzi w pattern, boola zwraca.
-        } else {
-            tablesToDelete = new LinkedList<>();
-            tablesToDelete.add(PhysicalTableName.fromSchemaTableFilter(filter.getTableSearchFilters().getSchemaTableName()));
-        }
-
-        if (tablesToDelete == null) {
-            // No matching tables specified or found.
+        FileStorageSettings fileStorageSettings = ErrorsSnapshot.createErrorsStorageSettings();
+        List<String> connections = this.parquetPartitionMetadataService.listConnections(fileStorageSettings);
+        if (connections == null) {
+            // No connections present.
             return dataDeleteResult;
         }
 
-        Collection<ErrorsSnapshot> errorsSnapshots = tablesToDelete.stream()
-                .map(tableName -> this.errorsSnapshotFactory.createSnapshot(
-                        filter.getTableSearchFilters().getConnectionName(),
-                        tableName
-                )).collect(Collectors.toList());
+        List<String> filteredConnections = connections.stream()
+                .filter(filter.getTableSearchFilters().getConnectionNameSearchPattern()::match)
+                .collect(Collectors.toList());
 
-        for (ErrorsSnapshot currentSnapshot: errorsSnapshots) {
-            LocalDate startDeletionRange = filter.getDateStart();
-            LocalDate endDeletionRange = filter.getDateEnd();
+        for (String connectionName: filteredConnections) {
+            List<PhysicalTableName> tables = this.parquetPartitionMetadataService.listTablesForConnection(
+                    connectionName, fileStorageSettings);
 
-            currentSnapshot.markSelectedForDeletion(startDeletionRange, endDeletionRange, conditions);
-
-            if (currentSnapshot.getLoadedMonthlyPartitions() == null) {
+            if (tables == null) {
+                // No tables present for this connection.
                 continue;
             }
-            Set<String> deletedIds = currentSnapshot.getTableDataChanges().getDeletedIds();
 
-            for (Map.Entry<ParquetPartitionId, LoadedMonthlyPartition> loadedPartitionEntry:
-                    currentSnapshot.getLoadedMonthlyPartitions().entrySet()) {
-                ParquetPartitionId partitionId = loadedPartitionEntry.getKey();
-                Table loadedPartitionTable = loadedPartitionEntry.getValue().getData();
-                if (loadedPartitionTable == null) {
+            Collection<ErrorsSnapshot> errorsSnapshots = tables.stream()
+                    .filter(tableName -> filter.getTableSearchFilters().getSchemaTableNameSearchPattern()
+                            .match(tableName.toTableSearchFilter()))
+                    .map(tableName -> this.errorsSnapshotFactory.createSnapshot(
+                            filter.getTableSearchFilters().getConnectionName(),
+                            tableName
+                    ))
+                    .collect(Collectors.toList());
+
+            for (ErrorsSnapshot currentSnapshot: errorsSnapshots) {
+                LocalDate startDeletionRange = filter.getDateStart();
+                LocalDate endDeletionRange = filter.getDateEnd();
+
+                currentSnapshot.markSelectedForDeletion(startDeletionRange, endDeletionRange, conditions);
+
+                if (currentSnapshot.getLoadedMonthlyPartitions() == null) {
                     continue;
                 }
 
-                int deletedRows = loadedPartitionTable
-                        .textColumn(CommonColumnNames.ID_COLUMN_NAME)
-                        .isIn(deletedIds)
-                        .size();
-                boolean allRowsDeleted = deletedRows == loadedPartitionTable.rowCount();
-                DataDeleteResultPartition partitionResult = new DataDeleteResultPartition(deletedRows, allRowsDeleted);
+                DataDeleteResult snapshotDataDeleteResult = currentSnapshot.getDeleteResults();
+                dataDeleteResult.concat(snapshotDataDeleteResult);
 
-                dataDeleteResult.getPartitionResults().put(partitionId, partitionResult);
+                currentSnapshot.save();
             }
-
-            currentSnapshot.save();
         }
 
         return dataDeleteResult;
