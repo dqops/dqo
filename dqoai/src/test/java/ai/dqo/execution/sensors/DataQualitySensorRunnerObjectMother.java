d37ddc725659553fa7c8f91d564289ede3f535bd
Piotr Czarnas <piotr.czarnas@documati.com>
2023-06-08 13:16:13 +0200

    Dependencies to jdbc drivers updated, show jinja2 errors in integration tests.

--- dqoai/src/test/java/ai/dqo/execution/sensors/DataQualitySensorRunnerObjectMother.java
+++ dqoai/src/test/java/ai/dqo/execution/sensors/DataQualitySensorRunnerObjectMother.java
@@ -47,6 +47,11 @@ public class DataQualitySensorRunnerObjectMother {
         DataQualitySensorRunnerImpl sensorRunner = getDefault();
         CheckExecutionProgressListenerStub progressListener = new CheckExecutionProgressListenerStub();
         SensorPrepareResult sensorPrepareResult = sensorRunner.prepareSensor(executionContext, sensorRunParameters, progressListener);
+        if (!sensorPrepareResult.isSuccess()) {
+            throw new ConnectionQueryException("Failed to prepare a sensor (render the SQL): " + sensorPrepareResult.getPrepareException().getMessage(),
+                    sensorPrepareResult.getPrepareException());
+        }
+
         SensorExecutionResult sensorExecutionResult = sensorRunner.executeSensor(executionContext, sensorPrepareResult, progressListener, false,
                 JobCancellationToken.createDummyJobCancellationToken());
         if (!sensorExecutionResult.isSuccess()) {
