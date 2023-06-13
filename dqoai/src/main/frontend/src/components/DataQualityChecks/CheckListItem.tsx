2ce05a939f1408ac78c0fe4ef6653b3d656ea216
Aleksy <psychologianauki@gmail.com>
2023-06-13 13:44:29 +0200

    sol init

--- dqoai/src/main/frontend/src/components/DataQualityChecks/CheckListItem.tsx
+++ dqoai/src/main/frontend/src/components/DataQualityChecks/CheckListItem.tsx
@@ -191,6 +191,26 @@ const CheckListItem = ({
     setShowDetails(!showDetails);
   };
 
+  const getLocalDateInTimeZone = (timeZone: string): string => {
+    const date = new Date();
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false,
+      timeZone
+    };
+
+    return date.toLocaleString('en-US', options);
+  };
+
+  const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  const localDate = getLocalDateInTimeZone(userTimeZone);
+  console.log('Local date in user time zone:', localDate);
+
   return (
     <>
       <tr
