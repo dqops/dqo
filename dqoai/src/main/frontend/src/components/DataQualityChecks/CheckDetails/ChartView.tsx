761abc7390cbe3adc7dd645ebd73b4f981191312
Aleksy <psychologianauki@gmail.com>
2023-06-23 10:38:38 +0200

    i think its done, waiting for confirmation about styling, before creating pull request

--- dqoai/src/main/frontend/src/components/DataQualityChecks/CheckDetails/ChartView.tsx
+++ dqoai/src/main/frontend/src/components/DataQualityChecks/CheckDetails/ChartView.tsx
@@ -1,107 +1,111 @@
 import React from 'react';
-import { CheckResultDetailedSingleModel } from "../../../api";
-import { Line } from "react-chartjs-2";
+import { CheckResultDetailedSingleModel } from '../../../api';
+import { Line } from 'react-chartjs-2';
 
 type ChartViewProps = {
   data: CheckResultDetailedSingleModel[];
 };
 
 export const ChartView = ({ data }: ChartViewProps) => {
-  const isFatalLowerExist = data.find(item => item.fatalLowerBound);
-  const isErrorLowerExist = data.find(item => item.errorLowerBound);
+  const isFatalLowerExist = data.find((item) => item.fatalLowerBound);
+  const isErrorLowerExist = data.find((item) => item.errorLowerBound);
 
-  const isFatalUpperExist = data.find(item => item.fatalUpperBound);
-  const isErrorUpperExist = data.find(item => item.errorUpperBound);
+  const isFatalUpperExist = data.find((item) => item.fatalUpperBound);
+  const isErrorUpperExist = data.find((item) => item.errorUpperBound);
 
   const dataSource: any = {
     datasets: [
-      {
-        label: 'Fatal (lower bound)',
-        data: data.map((item) => ({
-          x: item.timePeriod,
-          y: item.fatalLowerBound
-        })),
-        fill: 'start',
-        borderColor: '#E3170A',
-        backgroundColor: '#E3170A30',
-      },
-      {
-        label: 'Error (lower bound)',
-        data: data.map((item) => ({
-          x: item.timePeriod,
-          y: item.errorLowerBound
-        })),
-        fill: isFatalLowerExist ? '-1' : 'start',
-        borderColor: '#FF9900',
-        backgroundColor: '#FF990030',
-      },
-      {
-        label: 'Warning (lower bound)',
-        data: data.map((item) => ({
-          x: item.timePeriod,
-          y: item.warningLowerBound
-        })),
-        fill: isFatalLowerExist || isErrorLowerExist ? '-1' : 'start',
-        borderColor: '#EBE51E',
-        backgroundColor: '#EBE51E30',
-      },
-      {
-        label: 'Actual value',
-        data: data.map((item) => ({
-          x: item.timePeriod,
-          y: item.actualValue
-        })),
-        fill: false,
-        borderColor: 'rgb(54, 162, 235)',
-        backgroundColor: 'rgb(54, 162, 235)',
-      },
-      {
-        label: 'Expected value',
-        data: data.map((item) => ({
-          x: item.timePeriod,
-          y: item.expectedValue
-        })),
-        fill: false,
-        borderColor: 'rgb(201, 203, 207)',
-        backgroundColor: 'rgb(201, 203, 207)',
-      },
-      {
-        label: 'Warning (upper bound)',
-        data: data.map((item) => ({
-          x: item.timePeriod,
-          y: item.warningUpperBound
-        })),
-        fill: isErrorUpperExist || isFatalUpperExist ? '+1' : 'end',
-        borderColor: '#EBE51E',
-        backgroundColor: '#EBE51E30',
-      },
-      {
-        label: 'Error (upper bound)',
-        data: data.map((item) => ({
-          x: item.timePeriod,
-          y: item.errorUpperBound
-        })),
-        fill: isFatalUpperExist ? '+1' : 'end',
-        borderColor: '#FF9900',
-        backgroundColor: '#FF990030',
-      },
-      {
-        label: 'Fatal (upper bound)',
-        data: data.map((item) => ({
-          x: item.timePeriod,
-          y: item.fatalUpperBound
-        })),
-        fill: 'end',
-        borderColor: '#E3170A',
-        backgroundColor: '#E3170A30',
-      }
-    ]
+      data.some((item) => item.errorLowerBound !== undefined)
+        ? {
+            label: 'Error (lower bound)',
+            data: data.map((item) => ({
+              x: item.timePeriod,
+              y: item.errorLowerBound
+            })),
+            fill: isFatalLowerExist ? '-1' : 'start',
+            borderColor: '#FF9900',
+            backgroundColor: '#FF990030'
+          }
+        : null,
+      data.some((item) => item.warningLowerBound !== undefined)
+        ? {
+            label: 'Warning (lower bound)',
+            data: data.map((item) => ({
+              x: item.timePeriod,
+              y: item.warningLowerBound
+            })),
+            fill: isFatalLowerExist || isErrorLowerExist ? '-1' : 'start',
+            borderColor: '#EBE51E',
+            backgroundColor: '#EBE51E30'
+          }
+        : null,
+      data.some((item) => item.actualValue !== undefined)
+        ? {
+            label: 'Actual value',
+            data: data.map((item) => ({
+              x: item.timePeriod,
+              y: item.actualValue
+            })),
+            fill: false,
+            borderColor: 'rgb(54, 162, 235)',
+            backgroundColor: 'rgb(54, 162, 235)'
+          }
+        : null,
+      data.some((item) => item.expectedValue !== undefined)
+        ? {
+            label: 'Expected value',
+            data: data.map((item) => ({
+              x: item.timePeriod,
+              y: item.expectedValue
+            })),
+            fill: false,
+            borderColor: 'rgb(201, 203, 207)',
+            backgroundColor: 'rgb(201, 203, 207)'
+          }
+        : null,
+      data.some((item) => item.warningUpperBound !== undefined)
+        ? {
+            label: 'Warning (upper bound)',
+            data: data.map((item) => ({
+              x: item.timePeriod,
+              y: item.warningUpperBound
+            })),
+            fill: isErrorUpperExist || isFatalUpperExist ? '+1' : 'end',
+            borderColor: '#EBE51E',
+            backgroundColor: '#EBE51E30'
+          }
+        : null,
+      data.some((item) => item.errorUpperBound !== undefined)
+        ? {
+            label: 'Error (upper bound)',
+            data: data.map((item) => ({
+              x: item.timePeriod,
+              y: item.errorUpperBound
+            })),
+            fill: isFatalUpperExist ? '+1' : 'end',
+            borderColor: '#FF9900',
+            backgroundColor: '#FF990030'
+          }
+        : null,
+      data.some((item) => item.fatalUpperBound !== undefined)
+        ? {
+            label: 'Fatal (upper bound)',
+            data: data.map((item) => ({
+              x: item.timePeriod,
+              y: item.fatalUpperBound
+            })),
+            fill: 'end',
+            borderColor: '#E3170A',
+            backgroundColor: '#E3170A30'
+          }
+        : null
+    ].filter((item) => item !== null)
   };
 
   const options = {
     plugins: {
       title: {
-        display: false,
+        display: false
       },
       filler: {
         propagate: false
@@ -111,7 +115,7 @@ export const ChartView = ({ data }: ChartViewProps) => {
       }
     },
     interaction: {
-      intersect: false,
+      intersect: false
     },
     scales: {
       x: {
@@ -121,14 +125,14 @@ export const ChartView = ({ data }: ChartViewProps) => {
         }
       },
       y: {
-        stacked: false,
-      },
+        stacked: false
+      }
     }
   };
 
   return (
-    <div className="my-8">
+    <div className="my-8 w-1/2">
       <Line data={dataSource} options={options as any} />
     </div>
   );
-}
+};
