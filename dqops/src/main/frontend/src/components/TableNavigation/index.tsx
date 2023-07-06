2dc14dfdb128a596efe3819e16087ea3e8378130
Aleksy <psychologianauki@gmail.com>
2023-07-06 11:33:54 +0200

    work in progress have to clean a code now

--- dqops/src/main/frontend/src/components/TableNavigation/index.tsx
+++ dqops/src/main/frontend/src/components/TableNavigation/index.tsx
@@ -1,15 +1,15 @@
-import React, { useMemo } from "react";
-import { CheckTypes, ROUTES } from "../../shared/routes";
-import clsx from "clsx";
-import SvgIcon from "../SvgIcon";
-import { addFirstLevelTab } from "../../redux/actions/source.actions";
-import { useDispatch } from "react-redux";
-import { useHistory, useParams } from "react-router-dom";
+import React, { useMemo } from 'react';
+import { CheckTypes, ROUTES } from '../../shared/routes';
+import clsx from 'clsx';
+import SvgIcon from '../SvgIcon';
+import { addFirstLevelTab } from '../../redux/actions/source.actions';
+import { useDispatch } from 'react-redux';
+import { useHistory, useParams } from 'react-router-dom';
 
 type NavigationMenu = {
   label: string;
   value: CheckTypes;
-}
+};
 
 const navigations: NavigationMenu[] = [
   {
@@ -27,7 +27,7 @@ const navigations: NavigationMenu[] = [
   {
     label: 'Partition checks',
     value: CheckTypes.PARTITIONED
-  },
+  }
 ];
 
 type TableNavigationProps = {
@@ -36,7 +36,19 @@ type TableNavigationProps = {
 
 const TableNavigation = ({ defaultTab }: TableNavigationProps) => {
   const dispatch = useDispatch();
-  const { connection, schema, table, tab: activeTab, checkTypes }: { connection: string, schema: string, table: string, tab: string, checkTypes: CheckTypes } = useParams();
+  const {
+    connection,
+    schema,
+    table,
+    tab: activeTab,
+    checkTypes
+  }: {
+    connection: string;
+    schema: string;
+    table: string;
+    tab: string;
+    checkTypes: CheckTypes;
+  } = useParams();
   const history = useHistory();
 
   const activeIndex = useMemo(() => {
@@ -47,29 +59,77 @@ const TableNavigation = ({ defaultTab }: TableNavigationProps) => {
     if (checkTypes === item.value) {
       return;
     }
-    let url = ROUTES.TABLE_LEVEL_PAGE(item.value, connection, schema, table, 'detail');
+    let url = ROUTES.TABLE_LEVEL_PAGE(
+      item.value,
+      connection,
+      schema,
+      table,
+      'statistics'
+    );
+    console.log(url);
     let value = ROUTES.TABLE_LEVEL_VALUE(item.value, connection, schema, table);
-
+    console.log(url);
     if (defaultTab) {
+      console.log('inside');
       if (item.value === CheckTypes.RECURRING) {
-        url = ROUTES.TABLE_RECURRING(item.value, connection, schema, table, defaultTab);
-        value = ROUTES.TABLE_RECURRING_VALUE(item.value, connection, schema, table);
+        url = ROUTES.TABLE_RECURRING(
+          item.value,
+          connection,
+          schema,
+          table,
+          defaultTab
+        );
+        value = ROUTES.TABLE_RECURRING_VALUE(
+          item.value,
+          connection,
+          schema,
+          table
+        );
       } else if (item.value === CheckTypes.PARTITIONED) {
-        url = ROUTES.TABLE_PARTITIONED(item.value, connection, schema, table, defaultTab);
-        value = ROUTES.TABLE_PARTITIONED_VALUE(item.value, connection, schema, table);
+        url = ROUTES.TABLE_PARTITIONED(
+          item.value,
+          connection,
+          schema,
+          table,
+          defaultTab
+        );
+        value = ROUTES.TABLE_PARTITIONED_VALUE(
+          item.value,
+          connection,
+          schema,
+          table
+        );
+      } else if (item.value === CheckTypes.PROFILING) {
+        console.log(url);
+        url = ROUTES.TABLE_LEVEL_PAGE(
+          item.value,
+          connection,
+          schema,
+          table,
+          'statistics'
+        );
+        value = ROUTES.TABLE_LEVEL_VALUE(item.value, connection, schema, table);
       }
     } else {
-      const tab = item.value === CheckTypes.RECURRING || item.value === CheckTypes.PARTITIONED ? 'daily' : 'detail';
+      const tab =
+        item.value === CheckTypes.RECURRING ||
+        item.value === CheckTypes.PARTITIONED
+          ? 'daily'
+          : item.value === CheckTypes.PROFILING
+          ? 'statistics'
+          : 'detail';
       url = ROUTES.TABLE_LEVEL_PAGE(item.value, connection, schema, table, tab);
     }
-
-    dispatch(addFirstLevelTab(item.value, {
-      url,
-      value,
-      state: {},
-      label: table
-    }))
-
+    console.log(url);
+    dispatch(
+      addFirstLevelTab(item.value, {
+        url,
+        value,
+        state: {},
+        label: table
+      })
+    );
+    console.log(url);
     history.push(url);
   };
 
@@ -77,13 +137,24 @@ const TableNavigation = ({ defaultTab }: TableNavigationProps) => {
     <div className="flex space-x-3 px-4 pt-2 border-b border-gray-300 pb-4 mb-2">
       {navigations.map((item, index) => (
         <div
-          className={clsx("flex items-center cursor-pointer w-70", activeIndex === index ? "font-bold" : "")}
+          className={clsx(
+            'flex items-center cursor-pointer w-70',
+            activeIndex === index ? 'font-bold' : ''
+          )}
           key={item.value}
           onClick={() => onChangeNavigation(item)}
         >
-          {activeIndex > index ? <SvgIcon name="chevron-left" className="w-3 mr-2" /> : ''}
+          {activeIndex > index ? (
+            <SvgIcon name="chevron-left" className="w-3 mr-2" />
+          ) : (
+            ''
+          )}
           <span>{item.label}</span>
-          {activeIndex < index ? <SvgIcon name="chevron-right" className="w-6 ml-2" /> : ''}
+          {activeIndex < index ? (
+            <SvgIcon name="chevron-right" className="w-6 ml-2" />
+          ) : (
+            ''
+          )}
         </div>
       ))}
     </div>
