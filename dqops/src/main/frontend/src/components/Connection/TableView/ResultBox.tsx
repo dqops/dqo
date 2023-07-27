aa9981a76f42176619d3a79fb03efadadcac4a39
Aleksy <psychologianauki@gmail.com>
2023-07-27 11:32:03 +0200

    added missmatches popup

--- dqops/src/main/frontend/src/components/Connection/TableView/ResultBox.tsx
+++ dqops/src/main/frontend/src/components/Connection/TableView/ResultBox.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ComparisonCheckResultModel } from '../../../api';
+import clsx from 'clsx';
 
 interface data {
   item: ComparisonCheckResultModel;
@@ -8,7 +9,7 @@ interface data {
 const ResultBox = ({ item }: data) => {
   console.log(item);
   return (
-    <tr className="flex flex-col text-xs font-light justify-start items-start bg-green-500">
+    <tr className="flex flex-col text-xs font-light justify-start items-start  absolute top-0">
       <td className="flex justify-between w-2/3 ">
         <th className="text-xs font-light">Valid:</th>
         {item.valid_results}
@@ -26,8 +27,23 @@ const ResultBox = ({ item }: data) => {
         {item.warnings}
       </td>
       <td>
-        <a className="text-blue-300 underline" style={{ whiteSpace: 'nowrap' }}>
+        <a className="group relative text-blue-300 underline whitespace-nowrap cursor-pointer">
           Show mismatches
+          <section
+            className={clsx(
+              'hidden group-hover:grid grid-cols-3 absolute top-4 left-0 px-1 gap-y-1 rounded-md border border-gray-400 z-50 bg-white text-black no-underline',
+              item.not_matching_data_groups ? 'w-40 h-29 ' : 'w-40 h-10'
+            )}
+            style={{ right: '20' }}
+          >
+            {item.not_matching_data_groups ? (
+              item.not_matching_data_groups.map((x, index) => (
+                <span key={index}>{x}</span>
+              ))
+            ) : (
+              <span>No mismatches detected</span>
+            )}
+          </section>
         </a>
       </td>
     </tr>
--- dqops/src/main/frontend/src/components/Connection/TableView/ResultPanel.tsx
+++ dqops/src/main/frontend/src/components/Connection/TableView/ResultPanel.tsx
@@ -30,25 +30,25 @@ export default function ResultPanel({
   };
 
   return (
-    <tr className=" h-50 bg-blue-200 w-full">
-      <th className="text-left pr-4 py-1.5 bg-red-500 "></th>
-      <th className="text-left px-4 py-1.5 bg-green-500 "></th>
-      <th className="text-center px-4 py-1.5 pr-1 w-1/12">
+    <tr className=" h-50 w-full">
+      <th className="text-left pr-4 py-1.5  w-1/6 "></th>
+      <th className="text-left px-4 py-1.5 w-1/6 "></th>
+      <th className=" px-4 py-1.5 pr-1  relative">
         {minBool && <ResultBox item={prepareObj('min_match')} />}
       </th>
-      <th className=" px-4 py-1.5 pr-1 w-1/12">
+      <th className=" px-4 py-1.5 pr-1  relative">
         {maxBool && <ResultBox item={prepareObj('max_match')} />}
       </th>
-      <th className=" px-4 py-1.5 pr-1 w-1/12 flex justify-start">
+      <th className=" px-4 py-1.5 pr-1 relative">
         {sumBool && <ResultBox item={prepareObj('sum_match')} />}
       </th>
-      <th className="px-4 py-1.5 pr-1 w-1/12 ">
+      <th className="px-4 py-1.5 pr-1 relative">
         {meanBool && <ResultBox item={prepareObj('mean_match')} />}
       </th>
-      <th className="text-center px-4 py-1.5 pr-1 w-1/12">
+      <th className="text-center px-4 py-1.5 pr-1 relative">
         {nullCount && <ResultBox item={prepareObj('null_count_match')} />}
       </th>
-      <th className="text-center px-4 py-1.5 pr-1 w-1/12">
+      <th className="text-center px-4 py-1.5 pr-1 relative">
         {notNullCount && (
           <ResultBox item={prepareObj('not_null_count_match')} />
         )}
