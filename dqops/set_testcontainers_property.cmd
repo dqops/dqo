0cd692358476e875abc3198d8f8609576aacfead
DQOps Data Quality Operations Center <piotr.czarnas@dqo.ai>
2023-07-27 13:32:04 +0200

    Enable reusable test containers script updated.

--- dqops/set_testcontainers_property.cmd
+++ dqops/set_testcontainers_property.cmd
@@ -16,7 +16,7 @@
 
 @echo off
 
-if exist "%~dp0target\test-classes\ai\dqo\connectors\testcontainers\SetTestContainersUserConfigProperty.class" (
+if exist "%~dp0target\test-classes\com\dqops\connectors\testcontainers\SetTestContainersUserConfigProperty.class" (
     call "%~dp0..\mvnw.cmd" exec:java -Dexec.classpathScope=test -Dexec.includeProjectDependencies=true -Dexec.mainClass=com.dqops.connectors.testcontainers.SetTestContainersUserConfigProperty -Dexec.args="%*"
 ) else (
     echo ERROR: Main class not found, compile the whole project with Maven first
