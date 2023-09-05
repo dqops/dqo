83a10f1d718d03f4c6751da79f2086e22ea1485e
Aleksy <psychologianauki@gmail.com>
2023-09-05 08:51:06 +0200

    definitions

--- dqops/src/main/frontend/src/components/Sensors/RuleActionGroup.tsx
+++ dqops/src/main/frontend/src/components/Sensors/RuleActionGroup.tsx
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import { getFirstLevelSensorState } from '../../redux/selectors';
 import { useActionDispatch } from '../../hooks/useActionDispatch';
 import { updateRule } from '../../redux/actions/definition.actions';
+import { IRootState } from '../../redux/reducers';
 
 type RuleActionGroupProps = {
   onSave: () => void;
@@ -12,6 +13,9 @@ type RuleActionGroupProps = {
 export const RuleActionGroup = ({ onSave }: RuleActionGroupProps) => {
   const { full_rule_name, ruleDetail, isUpdating, isUpdatedRuleDetail } =
     useSelector(getFirstLevelSensorState);
+    const { userProfile } = useSelector(
+      (state: IRootState) => state.job || {}
+    );
   const dispatch = useActionDispatch();
 
   const handleSave = () => {
@@ -32,6 +36,7 @@ export const RuleActionGroup = ({ onSave }: RuleActionGroupProps) => {
           variant="outlined"
           label="Delete rule"
           className="w-40 !h-10"
+          disabled={userProfile.can_manage_definitions === false}
         />
       )}
       <Button
@@ -39,7 +44,7 @@ export const RuleActionGroup = ({ onSave }: RuleActionGroupProps) => {
         variant="contained"
         label="Save"
         className="w-40 !h-10"
-        disabled={!isUpdatedRuleDetail}
+        disabled={!isUpdatedRuleDetail || userProfile.can_manage_definitions === false}
         onClick={handleSave}
         loading={isUpdating}
       />
