47dee89d790925a807e1c7f60f0d0180407c2319
Aleksy <psychologianauki@gmail.com>
2023-09-29 21:00:13 +0200

    suporting changing users password

---
+++ dqops/src/main/frontend/src/pages/UserListDetail/ChangeUserPasswordDialog.tsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import { Dialog, DialogBody, DialogFooter, Input } from '@material-tailwind/react'; 
+import Button from '../../components/Button';
+
+interface AddColumnDialogProps {
+  open: boolean;
+  onClose: () => void;
+  handleSubmit: (newEmail: string) => Promise<void>
+}
+const generatePassword = (length : number) => {
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  return Array.from({ length }, () => charset[Math.floor(Math.random() * charset.length)]).join('');
+}
+
+const ChangeUserPasswordDialog = ({ open, onClose, handleSubmit }: AddColumnDialogProps) => {
+  const [password, setPassword] = useState('');
+
+  useEffect(()=> {
+    setPassword(generatePassword(8))
+  },[open])
+  
+  return (
+    <Dialog open={open} handler={onClose} size="xs">
+      <DialogBody className="pt-6 pb-2 px-8">
+        <div className="flex flex-col">
+          <h1 className="text-center mb-4 text-gray-700 text-2xl">
+            Change Password
+          </h1>
+          <div className="mb-6">
+            <Input
+              label="New Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+        </div>
+      </DialogBody>
+      <DialogFooter className="justify-center space-x-6 pb-8">
+        <Button
+          color="primary"
+          variant="outlined"
+          className="px-8"
+          onClick={onClose}
+          label="Cancel"
+        />
+        <Button
+          color="primary"
+          className="px-8"
+          onClick={() => handleSubmit(password)}
+          label="Save"
+        />
+      </DialogFooter>
+    </Dialog>
+  );
+};
+
+export default ChangeUserPasswordDialog;
--- dqops/src/main/frontend/src/pages/UserListDetail/index.tsx
+++ dqops/src/main/frontend/src/pages/UserListDetail/index.tsx
@@ -3,43 +3,70 @@ import DefinitionLayout from '../../components/DefinitionLayout'
 import { UsersApi } from '../../services/apiClient'
 import { DqoCloudUserModel } from '../../api'
 import Button from '../../components/Button'
+import { useSelector } from 'react-redux'
+import { IRootState } from '../../redux/reducers'
+import ChangeUserPasswordDialog from './ChangeUserPasswordDialog'
 
 
 export default function UserListDetail() {
-    const [dqoCloudUsers, setDqoCloudUsers] = useState<DqoCloudUserModel[]>()
+    const { userProfile } = useSelector((state: IRootState) => state.job || {});
+    const [dqoCloudUsers, setDqoCloudUsers] = useState<DqoCloudUserModel[]>([])
+    const [reshreshUsersIndicator, setRefreshUsersIndicator] = useState<boolean>(false)
+    const [selectedEmail, setSelectedEmail] = useState('')
+
 
     useEffect(() => {
        UsersApi.getAllUsers()
        .then((res) => setDqoCloudUsers(res.data))
        .catch((err) => console.error(err))
-       , []
-    })
+    }, [reshreshUsersIndicator])
 
-    const editDqoCloudUser = () => {}
+    const editDqoCloudUser = (
+        //TODO: opening new tab
+    ) => {}
 
-    const deleteDqoCloudUser = () => {}
+    const deleteDqoCloudUser = async (email: string) => {
+       await UsersApi.deleteUser(email)
+       .then(() => setRefreshUsersIndicator(!reshreshUsersIndicator))
+       .catch((err) => console.error(err))
+    }
 
-    const changeDqoCloudUserPassword = () => {}
+    const changeDqoCloudUserPassword =async ( newEmail: string) => {
+        await UsersApi.updateUser(selectedEmail, {email: newEmail })
+        .then(() => setRefreshUsersIndicator(!reshreshUsersIndicator))
+        .catch((err) => console.error(err))
+    }
+
+    const addDqoCloudUser = () => {}
+
 
   return (
     <DefinitionLayout>
-        <table className='w-full'>
-            <thead className='border-b w-full border-b-gray-400'>
+        <table className='w-full '>
+            <thead className='border-b w-full border-b-gray-400 relative'>
                 <th className="px-6 py-4 text-left">User email</th>
                 <th className="px-6 py-4 text-left">User role</th>
+                <Button label='Add user'
+                 color='primary'
+                 variant='contained'
+                 className='absolute right-2 top-2 w-40'
+                 onClick={addDqoCloudUser}
+                 disabled={!!(userProfile.users_limit && userProfile.users_limit <= dqoCloudUsers?.length)} 
+                />
             </thead>
             <tbody>
-                {dqoCloudUsers?.map((x, index) => 
+                {dqoCloudUsers?.map((user, index) => 
                 <tr key={index}>
-                    <td className='px-6 py-2 text-left'>{x.email}</td>
-                    <td className='px-6 py-2 text-left'>{x.accountRole}</td>
-                    <td className='px-6 py-2 text-left'><Button label='edit' onClick={editDqoCloudUser}/></td>
-                    <td className="px-6 py-2 text-left"><Button label='delete' onClick={deleteDqoCloudUser}/></td>
-                    <td className="px-6 py-2 text-left"><Button label='change password' onClick={changeDqoCloudUserPassword}/></td>
+                    <td className='px-6 py-2 text-left'>{user.email}</td>
+                    <td className='px-6 py-2 text-left'>{user.accountRole}</td>
+                    <td className='px-6 py-2 text-left'><Button label='edit' variant='text' color='primary' onClick={editDqoCloudUser}/></td>
+                    <td className="px-6 py-2 text-left"><Button label='delete' variant='text' color='primary' onClick={() => deleteDqoCloudUser(user.email ?? '')}/></td>
+                    <td className="px-6 py-2 text-left"><Button label='change password' variant='text' color='primary' onClick={() => setSelectedEmail(user.email ?? '')} disabled={userProfile.account_role !== "admin"}/></td>
                 </tr>
                 )}
             </tbody>
         </table>
+        <ChangeUserPasswordDialog open={selectedEmail.length!==0} onClose={() => setSelectedEmail('')} handleSubmit={changeDqoCloudUserPassword}  />
     </DefinitionLayout>
   )
 }
