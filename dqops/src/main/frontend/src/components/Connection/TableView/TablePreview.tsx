55e7d12fc6776c11558539e044aa584803952dd8
Aleksy <psychologianauki@gmail.com>
2023-10-05 11:39:01 +0200

    code optymalization

--- dqops/src/main/frontend/src/components/Connection/TableView/TablePreview.tsx
+++ dqops/src/main/frontend/src/components/Connection/TableView/TablePreview.tsx
@@ -41,76 +41,23 @@ export default function TablePreview({statistics} : tablePreviewProps) {
         return value;
       };
 
-    const nullPercentData = statistics?.column_statistics?.map((x) =>
-    x.statistics
-      ?.filter((item) => item.collector === 'nulls_percent')
-      .map((item) => Number(renderValue(item.result)))
-  );
-  const uniqueCountData = statistics?.column_statistics?.map((x) =>
-    x.statistics
-      ?.filter((item) => item.collector === 'distinct_count')
-      .map((item) => item.result)
-  );
-  const nullCountData = statistics?.column_statistics?.map((x) =>
-    x.statistics
-      ?.filter((item) => item.collector === 'nulls_count')
-      .map((item) => renderValue(item.result))
-  );
-  const detectedDatatypeVar = statistics?.column_statistics?.map((x) =>
-    x.statistics
-      ?.filter((item) => item.collector === 'string_datatype_detect')
-      .map((item) => item.result)
-  );
+    const dataArray = (statistics?.column_statistics || []).map((x) => {
+        const newData = {
+          null_percent: Number(x.statistics?.find((item) => item.collector === 'nulls_percent')?.result),
+          unique_value: Number(x.statistics?.find((item) => item.collector === 'distinct_count')?.result),
+          null_count: Number(x.statistics?.find((item) => item.collector === 'nulls_count')?.result),
+          detectedDatatypeVar: Number(x.statistics?.find((item) => item.collector === 'string_datatype_detect')?.result),
+          nameOfCol: x.column_name,
+          minimalValue: x.statistics?.find((item) => item.collector === 'min_value')?.result,
+          maximumValue: x.statistics?.find((item) => item.collector === 'max_value')?.result,
+          length: x.type_snapshot?.length,
+          scale: x.type_snapshot?.scale,
+          importedDatatype: String(x.type_snapshot?.column_type),
+        };
+        return newData;
+      });
 
-  const columnNameData = statistics?.column_statistics?.map(
-    (x) => x.column_name
-  );
-  const minimalValueData = statistics?.column_statistics?.map((x) =>
-    x.statistics
-      ?.filter((item) => item.collector === 'min_value')
-      .map((item) => item.result)
-  );
-
-  const maximumValueData = statistics?.column_statistics?.map((x) =>
-    x.statistics
-      ?.filter((item) => item.collector === 'max_value')
-      .map((item) => item.result)
-  );
-  const lengthData = statistics?.column_statistics?.map(
-    (x) => x.type_snapshot?.length
-  );
-
-  const scaleData = statistics?.column_statistics?.map(
-    (x) => x.type_snapshot?.scale
-  );
-
-  const typeData = statistics?.column_statistics?.map(
-    (x) => x.type_snapshot?.column_type
-  );
-
-  const hashData = statistics?.column_statistics?.map((x) => x.column_hash);
-
-  const dataArray: MyData[] = [];
-  if (columnNameData && hashData) {
-    const maxLength = Math.max(columnNameData.length, hashData.length);
-
-    for (let i = 0; i < maxLength; i++) {
-      const newData: MyData = {
-        null_percent: Number(renderValue(nullPercentData?.[i])),
-        unique_value: Number(renderValue(uniqueCountData?.[i])),
-        null_count: Number(renderValue(nullCountData?.[i])),
-        detectedDatatypeVar: Number(detectedDatatypeVar?.[i]),
-        nameOfCol: columnNameData?.[i],
-        minimalValue: renderValue(minimalValueData?.[i]),
-        maximumValue: renderValue(maximumValueData?.[i]),
-        length: renderValue(lengthData?.[i]),
-        scale: renderValue(scaleData?.[i]),
-        importedDatatype: String(renderValue(typeData?.[i])),
-      };
-
-      dataArray.push(newData);
-    }
-  }
+  console.log(statistics)
 
   return (
         <table className='w-screen mt-5 p-4'>
@@ -124,7 +71,7 @@ export default function TablePreview({statistics} : tablePreviewProps) {
                 <tr key= {index} className='flex'>
                     <td className='px-6 py-2 text-left  block w-50 border border-gray-300' >{x.label}</td>
                     {dataArray.map((y, jIndex) => 
-                    <td key={jIndex} className='px-6 py-2 text-left block w-50 border border-gray-300'>{y[x.value as keyof MyData]}</td>
+                    <td key={jIndex} className='px-6 py-2 text-left block w-50 border border-gray-300'>{renderValue(y[x.value as keyof MyData])}</td>
                     )}
                     
                 </tr>
