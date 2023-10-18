6a2a74f0147572da1c47f18246122f4c757c8c97
Patryk Jatczak <patryk.jatczak@dqops.com>
2023-10-18 12:39:02 +0200

    extracted common methods

---
+++ distribution/python/dqops/airflow/tools/rule_severity_level_utility.py
@@ -0,0 +1,17 @@
+from dqops.client.models.rule_severity_level import RuleSeverityLevel
+
+def get_severity_value(rule_severity: RuleSeverityLevel) -> int:
+    """
+    Maps the rule severity level to the corresponding int value.
+
+    Parameters
+    ----------
+    rule_severity: RuleSeverityLevel
+        The rule severity.
+    """
+
+    if rule_severity == RuleSeverityLevel.VALID:    return 0
+    if rule_severity == RuleSeverityLevel.WARNING:  return 1
+    if rule_severity == RuleSeverityLevel.ERROR:    return 2
+    if rule_severity == RuleSeverityLevel.FATAL:    return 3
+    raise Exception("Unrecognized rule severity level : " + str(rule_severity))
---
+++ distribution/python/dqops/airflow/tools/timeout/dqo_timeout.py
@@ -0,0 +1,20 @@
+import logging
+from airflow.exceptions import AirflowException
+
+def handle_dqo_timeout(fail_on_timeout: bool):
+    """
+    Method handles timeout from dqo api.
+
+    Parameters
+    ----------
+    fail_on_timeout: bool
+        When set to True it causes that airflow task is marked as Failed by throwing an AirflowException.
+    """
+
+    timeout_message: str = "DQOps' job has timed out!"
+
+    if fail_on_timeout:
+        logging.error(timeout_message)
+        raise AirflowException(timeout_message)
+    else:
+        logging.info(timeout_message)
