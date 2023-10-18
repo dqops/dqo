be990157eeaf2ec7b15fbaf56064bed15040cc12
Patryk Jatczak <patryk.jatczak@dqops.com>
2023-10-18 12:41:29 +0200

    assert table status airflow operator, dags

---
+++ distribution/python/dqops/airflow/check_results/dqo_assert_table_status_operator.py
@@ -0,0 +1,122 @@
+import json
+import logging
+from typing import Any, Dict, Union
+
+from httpx import ReadTimeout
+
+from airflow.models.baseoperator import BaseOperator
+from dqops.airflow.exceptions.dqops_data_quality_issue_detected_exception import (
+    DqopsDataQualityIssueDetectedException,
+)
+from dqops.airflow.exceptions.dqops_empty_response_exception import (
+    DqopsEmptyResponseException,
+)
+from dqops.airflow.tools.rule_severity_level_utility import get_severity_value
+from dqops.airflow.tools.url_resolver import extract_base_url
+from dqops.airflow.tools.timeout.python_client_timeout import handle_python_timeout
+from dqops.client import Client
+from dqops.client.api.check_results.get_table_data_quality_status import sync_detailed
+from dqops.client.models.check_time_scale import CheckTimeScale
+from dqops.client.models.check_type import CheckType
+from dqops.client.models.rule_severity_level import RuleSeverityLevel
+from dqops.client.models.table_data_quality_status_model import (
+    TableDataQualityStatusModel,
+)
+from dqops.client.types import UNSET, Response, Unset
+from dqops.airflow.tools.client_creator import create_client
+
+class DqoAssertTableStatusOperator(BaseOperator):
+    """
+    Airflow require table data quality status operator for receiving DQOps table status.
+
+    """
+
+    def __init__(
+        self,
+        connection_name: str,
+        schema_name: str,
+        table_name: str,
+        *,
+        months: Union[Unset, None, int] = UNSET,
+        check_type: Union[Unset, None, CheckType] = UNSET,
+        check_time_scale: Union[Unset, None, CheckTimeScale] = UNSET,
+        base_url: str = "http://localhost:8888/",
+        wait_timeout: int = UNSET,
+        fail_on_timeout: bool = True,
+        maximum_severity_threshold: RuleSeverityLevel = RuleSeverityLevel.ERROR,
+        **kwargs
+    ) -> Union[Dict[str, Any], None]:
+        """
+        Parameters
+        ----------
+        connection_name : str
+            The connection name to the data source in DQOps. When not set, all connection names will be used.
+        schema_name : str
+            The schema name.
+        table_name : str 
+            The table name.
+        months : Union[Unset, None, int] = UNSET,
+            Optional filter - the number of months to review the data quality check results. 
+            For partitioned checks, it is the number of months to analyze. 
+            The default value is 1 (which is the current month and 1 previous month).
+        check_type : Union[Unset, None, CheckType] = UNSET
+            Specifies type of checks to be executed. When not set, all types of checks will be executed. <br/> The enum is stored in _dqops.client.models.check_type_ module.
+        check_time_scale : Union[Unset, None, CheckTimeScale] = UNSET
+            Time scale filter for monitoring and partitioned checks (values: daily or monthly).
+        base_url : str [optional, default="http://localhost:8888/"]
+            The base url to DQOps application.
+        wait_timeout : int
+            Time in seconds for execution that client will wait. It prevents from hanging the task for an action that is never completed. If not set, the timeout is read form the client defaults, which value is 120 seconds.
+        fail_on_timeout : bool [optional, default=True]
+            Timeout is leading the task status to Failed by default. It can be omitted marking the task as Success by setting the flag to True.
+        maximum_severity_threshold: RuleSeverityLevel [optional, default=RuleSeverityLevel.ERROR]
+            The maximum level of rule severity that is accepted, causing that an airflow task finishes with succeeded status.
+        """
+
+        super().__init__(**kwargs)
+        self.connection_name: str = connection_name
+        self.schema_name: str = schema_name
+        self.table_name: str = table_name
+        self.months: Union[Unset, None, int] = months
+        self.check_type: Union[Unset, None, CheckType] = check_type
+        self.check_time_scale: Union[Unset, None, CheckTimeScale] = check_time_scale
+
+        self.base_url: str = extract_base_url(base_url)
+        self.wait_timeout: int = wait_timeout
+        self.fail_on_timeout: bool = fail_on_timeout
+
+        self.maximum_severity_threshold: RuleSeverityLevel = maximum_severity_threshold
+
+    def execute(self, context):
+        
+        client: Client = create_client(base_url=self.base_url, wait_timeout=self.wait_timeout)
+
+        try:
+            response: Response[TableDataQualityStatusModel] = sync_detailed(
+                connection_name=self.connection_name,
+                schema_name=self.schema_name,
+                table_name=self.table_name,
+                client=client,
+                months=self.months,
+                check_type=self.check_type,
+                check_time_scale=self.check_time_scale,
+            )
+        except ReadTimeout as exception:
+            handle_python_timeout(exception, self.fail_on_timeout)
+            return None
+
+        # When timeout is too short, returned object is empty
+        if response.content.decode("utf-8") == "":
+            raise DqopsEmptyResponseException()
+
+        table_dq_status: TableDataQualityStatusModel = (
+            TableDataQualityStatusModel.from_dict(
+                json.loads(response.content.decode("utf-8"))
+            )
+        )
+        logging.info(table_dq_status.to_dict())
+
+        if table_dq_status.highest_severity_issue > get_severity_value(self.maximum_severity_threshold):
+            raise DqopsDataQualityIssueDetectedException()
+
+        return table_dq_status.to_dict()
--- tools/airflow-local/dags/dqops_assert_table_status.py
+++ tools/airflow-local/dags/dqops_assert_table_status.py
@@ -1,19 +1,21 @@
 import datetime
 import pendulum
 from airflow import DAG
-from dqops.airflow.run_checks.dqops_run_checks_operator import DqopsRunChecksOperator
+from dqops.airflow.check_results.dqo_assert_table_status_operator import DqoAssertTableStatusOperator
 from dqops.client.models.check_type import CheckType
 
 with DAG(
-    dag_id="my_connection_dqops_run_checks",
+    dag_id="example_connection_dqops_check_status",
     schedule=datetime.timedelta(hours=12),
     start_date=pendulum.datetime(2023, 1, 1, tz="UTC"),
     catchup=False,
 ) as dag:
-    run_checks_task = DqopsRunChecksOperator(
-        task_id="dqops_run_checks_operator_task",
+    check_status_task = DqoAssertTableStatusOperator(
+        task_id="dqo_require_table_data_quality_status_operator_task",
         # local DQOps instance on a localhost can be reached from images with substitution the "host.docker.internal" in place of "localhost"
         base_url='http://host.docker.internal:8888',
         connection_name="example_connection",
+        schema_name="maven_restaurant_ratings",
+        table_name="consumers",
         check_type=CheckType.MONITORING
     )
