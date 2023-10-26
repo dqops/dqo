51674b47fa3bc6d4b2749c3720a75649f13d4ecd
Patryk Jatczak <patryk.jatczak@dqops.com>
2023-10-26 13:10:51 +0200

    table import dag example

---
+++ tools/airflow-local/dags/dqops_table_import.py
@@ -0,0 +1,19 @@
+import datetime
+import pendulum
+from airflow import DAG
+from dqops.airflow.table_import.dqo_table_import_operator import DqoTableImportOperator
+
+with DAG(
+    dag_id="example_connection_dqops_table_import",
+    schedule=datetime.timedelta(hours=12),
+    start_date=pendulum.datetime(2023, 1, 1, tz="UTC"),
+    catchup=False,
+) as dag:
+    check_status_task = DqoTableImportOperator(
+        task_id="dqo_connection_dqops_table_import_task",
+        # local DQOps instance on a localhost can be reached from images with substitution the "host.docker.internal" in place of "localhost"
+        base_url="http://host.docker.internal:8888",
+        connection_name="example_connection",
+        schema_name="maven_restaurant_ratings",
+        table_names=["ratings"]
+    )
