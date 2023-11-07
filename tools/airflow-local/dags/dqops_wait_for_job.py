ae7a389dc98c6cd6314f5f9ca3856566fc02caed
Patryk Jatczak <patryk.jatczak@dqops.com>
2023-11-07 09:56:50 +0100

    renamed dag example

--- tools/airflow-local/dags/dqops_wait_for_job.py
+++ tools/airflow-local/dags/dqops_wait_for_job.py
@@ -1,23 +1,15 @@
-
-"""Example DAG demonstrating the usage of XComs."""
-from __future__ import annotations
-
 import pendulum
-
-from airflow import DAG, XComArg
-from airflow.decorators import task
-from airflow.operators.bash import BashOperator
+from airflow import DAG
 
 from dqops.airflow.table_import.dqops_table_import_operator import DqopsTableImportOperator
 from dqops.airflow.wait_for_job.dqops_wait_for_job_operator import DqopsWaitForJobOperator
 
 with DAG(
-    dag_id="example_xcom_1",
+    dag_id="example_connection_wait_for_job",
     schedule="@once",
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
-    tags=["example"]
-    
+    tags=["dqops_example"]
 ) as dag:
     import_table_task = DqopsTableImportOperator(
         task_id="dqops_connection_dqops_table_import_task",
@@ -29,12 +21,16 @@ with DAG(
     )
     
     wait_for_job = DqopsWaitForJobOperator(
-        task_id="dqo_wait_for_job",
+        task_id="dqops_wait_for_job",
         # local DQOps instance on a localhost can be reached from images with substitution the "host.docker.internal" in place of "localhost"
         base_url="http://host.docker.internal:8888",
-        task_instance_name="dqops_connection_dqops_table_import_task",
+        # the total time in seconds for the operator to wait will be the product of retries number and the retry_delay
         retries=5,
-        retry_delay=10
+        retry_delay=10, # in seconds
+        trigger_rule="all_done"
+        # the below parameter is set automatically when the wait for job operator has the only one upstream task
+        # otherwise the parameter has to be set manually because the operator can only wait for a single job 
+        # task_id_to_wait_for="dqops_connection_dqops_table_import_task",
     )
 
     import_table_task >> wait_for_job
