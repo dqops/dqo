93b22260981920d850527e053df40533566641ec
Aleksy <psychologianauki@gmail.com>
2023-11-09 12:25:33 +0100

    fetching data corectly

--- dqops/src/main/frontend/src/components/Connection/TableView/TableQualityStatus.tsx
+++ dqops/src/main/frontend/src/components/Connection/TableView/TableQualityStatus.tsx
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { CheckResultApi } from '../../../services/apiClient'
+import { useParams } from 'react-router-dom';
+import { CheckTypes } from '../../../shared/routes';
+import { TableCurrentDataQualityStatusModel } from '../../../api';
 
 export default function TableQualityStatus() {
+    const {
+        checkTypes,
+        connection,
+        schema,
+        table
+      }: {
+        checkTypes: CheckTypes;
+        connection: string;
+        schema: string;
+        table: string;
+      } = useParams();
+    const [tableDataQualityStatus, setTableDataQualityStatus] = useState<TableCurrentDataQualityStatusModel>({})
+
+    const getTableDataQualityStatus = () => {   
+        CheckResultApi.getTableDataQualityStatus(connection, schema, table)
+        .then((res) =>  setTableDataQualityStatus(res.data))
+    }
+
+    useEffect(() => {
+        getTableDataQualityStatus()
+    }, [])
+console.log(tableDataQualityStatus)
   return (
     <div>TableQualityStatus</div>
   )
