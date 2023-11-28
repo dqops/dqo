8f9112b88b806e0e5c0ada5d28c3ed73c03fa58f
Patryk Jatczak <patryk.jatczak@dqops.com>
2023-11-28 14:20:28 +0100

    script tag modifier for scripts lazy loading by using: type="rocketlazyloadscript"

---
+++ tools/documentation/script_tag_modifier.py
@@ -0,0 +1,40 @@
+import re
+from file_handler import provide_file_content, save_lines_to_file
+
+def modify_script_tags(file_path: str):
+
+    lines: list[str] = provide_file_content(file_path)
+    modified_lines: list[str] = list()
+
+    for line in lines:
+        if _verify_tag_application(line):
+            modified_lines.append(_apply_tag_modification(line))
+        else:
+            modified_lines.append(line)
+
+    save_lines_to_file(file_path, modified_lines)
+
+def _verify_tag_application(line: str):
+
+    script_tag_opening: str = "<script"
+
+    script_exclusion_string: str = "lazyload.min.js"
+
+    if script_tag_opening not in line or script_exclusion_string in line:
+        return False
+    return True
+
+def _apply_tag_modification(line: str) -> str:
+
+    replacement: str = " type=\"rocketlazyloadscript\">"
+
+    regex_pattern: str = "<script[^<]*>"
+
+    compiled_regex = re.compile(regex_pattern)
+
+    result = compiled_regex.search(line)
+    match_text = result.group(0)
+
+    modified_line: str = line.replace(match_text, match_text.replace(">", replacement))
+
+    return modified_line
