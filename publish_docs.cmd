29e0f2a78bad10078fb3e8fcf175aea2b94754bd
Patryk Jatczak <patryk.jatczak@dqops.com>
2023-11-29 15:10:44 +0100

    search is enabled on web page

--- publish_docs.cmd
+++ publish_docs.cmd
@@ -1,4 +1,5 @@
 @echo off
 python -m pip install --upgrade --user -r ./requirements.txt
 python -m mkdocs build
-gsutil -m rsync -j html,txt,xml,png,js,css,json,svg,gif -r site gs://docs-dqo-ai/docs/
+python tools/documentation/docs_modifier.py site
+gsutil -m rsync -j html,txt,xml,png,js,css,json,svg,gif -r site gs://docs-dqo-ai/docs/temporary4/
