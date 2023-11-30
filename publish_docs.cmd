1f9ef4bc353ac76ef5ddf8144583565289b3b5b1
Patryk Jatczak <patryk.jatczak@dqops.com>
2023-11-30 09:51:11 +0100

    tested ENABLE_SEARCH with mkdocs-material=9.4, ready and commented in mkdocs.yml

--- publish_docs.cmd
+++ publish_docs.cmd
@@ -1,5 +1,6 @@
 @echo off
 python -m pip install --upgrade --user -r ./requirements.txt
+set ENABLE_SEARCH=true
 python -m mkdocs build
 python tools/documentation/docs_modifier.py site
-gsutil -m rsync -j html,txt,xml,png,js,css,json,svg,gif -r site gs://docs-dqo-ai/docs/temporary4/
+gsutil -m rsync -j html,txt,xml,png,js,css,json,svg,gif -r site gs://docs-dqo-ai/docs/
