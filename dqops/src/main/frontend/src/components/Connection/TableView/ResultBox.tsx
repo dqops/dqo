07bd20bac915900f6029b18ceda8dcd68fca2968
Aleksy <psychologianauki@gmail.com>
2023-12-11 20:15:00 +0100

    displaying tooltip only if length of array equals 1

--- dqops/src/main/frontend/src/components/Connection/TableView/ResultBox.tsx
+++ dqops/src/main/frontend/src/components/Connection/TableView/ResultBox.tsx
@@ -148,7 +148,9 @@ const ResultBox = ({
                 {item.not_matching_data_groups ? (
                   item.not_matching_data_groups.map((x, index) => (
                     <span key={index}>
-                      {index === 0 && x === 'no grouping'
+                      {index === 0 &&
+                      x === 'no grouping' &&
+                      item.not_matching_data_groups?.length === 1
                         ? 'No mismatches detected'
                         : x}
                     </span>
