ed1be68edaacbf075762ec6791c60a8fdc3d6116
Aleksy <psychologianauki@gmail.com>
2024-01-11 16:18:59 +0100

    default values trino db fix

--- dqops/src/main/frontend/src/pages/CreateConnection/index.tsx
+++ dqops/src/main/frontend/src/pages/CreateConnection/index.tsx
@@ -54,11 +54,9 @@ const CreateConnection = () => {
       case ConnectionModelProviderTypeEnum.trino: {
         copiedDatabase.trino = { 
           port: '8080', 
-          trino_engine_type: (database.trino?.trino_engine_type ? database?.trino?.trino_engine_type : nameOfDatabase?.toLowerCase() as TrinoParametersSpecTrinoEngineTypeEnum),
+          trino_engine_type: (nameOfDatabase?.toLowerCase() as TrinoParametersSpecTrinoEngineTypeEnum),
           catalog: (
-            (database.trino?.trino_engine_type 
-              ? database.trino?.trino_engine_type 
-              : nameOfDatabase?.toLowerCase() as TrinoParametersSpecTrinoEngineTypeEnum 
+            (nameOfDatabase?.toLowerCase() as TrinoParametersSpecTrinoEngineTypeEnum 
             ) === TrinoParametersSpecTrinoEngineTypeEnum.athena 
               ? "awsdatacatalog" : ""
           ),
