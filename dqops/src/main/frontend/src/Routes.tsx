54b02e14d7d80dc9950579e024d033dc365ba540
Aleksy <psychologianauki@gmail.com>
2024-01-30 14:10:37 +0100

    cleaned routes.tsx code

--- dqops/src/main/frontend/src/Routes.tsx
+++ dqops/src/main/frontend/src/Routes.tsx
@@ -1,52 +1,8 @@
-import TableProfilingChecksView from "./pages/TableProfilingChecksView";
-
 import React from 'react';
-
 import { Route, Switch, Redirect } from 'react-router';
-
-import CreateConnection from './pages/CreateConnection';
-import ChecksPage from './pages/Checks';
 import HomePage from './pages/Home';
-import Dashboards from './pages/Dashboards';
-import ConnectionPage from "./pages/Connection";
-import SchemaPage from "./pages/Schema";
-import TablePage from "./pages/Table";
-import TableDailyChecksView from "./pages/TableDailyMonitoringChecksView";
-import TableMonthlyChecksView from "./pages/TableMonthlyMonitoringChecksView";
-import TableDailyPartitionedChecksView from "./pages/TableDailyPartitionedChecksView";
-import TableMonthlyPartitionedChecksView from "./pages/TableMonthlyPartitionedChecksView";
-import TableProfilingChecksUIFilterView from "./pages/TableProfilingChecksUIFilterView";
-import TableColumnsView from "./pages/TableColumnsView";
-import ColumnView from "./pages/ColumnView";
-import ColumnProfilingChecksView from "./pages/ColumnProfilingChecksView";
-import ColumnProfilingChecksUIFilterView from "./pages/ColumnProfilingChecksUIFilterView";
-import ColumnDailyMonitoringChecksView from "./pages/ColumnDailyMonitoringChecksView";
-import ColumnMonitoringChecksUIFilterView from "./pages/ColumnMonitoringChecksUIFilterView";
-import ColumnMonthlyMonitoringChecksView from "./pages/ColumnMonthlyMonitoringChecksView";
-import ColumnDailyPartitionedChecksView from "./pages/ColumnDailyPartitionedChecksView";
-import ColumnMonthlyPartitionedChecksView from "./pages/ColumnMonthlyPartitionedChecksView";
-import ColumnPartitionedChecksUIFilterView from "./pages/ColumnPartitionedChecksUIFilterView";
-import TableMonitoringChecksUIFilterView from "./pages/TableMonitoringChecksUIFilterView";
-import TablePartitionedChecksUIFilterView from "./pages/TablePartitionedChecksUIFilterView";
 import { ROUTES } from "./shared/routes";
-import Definitions from "./pages/Definitions";
-import SensorDetail from "./pages/SensorDetail";
-import RuleDetail from "./pages/RuleDetail";
-import Incidents from "./pages/Incidents";
-import IncidentConnection from "./pages/IncidentConnection";
-import IncidentDetail from "./pages/IncidentDetail";
 import NotFound from "./pages/NotFound";
-import TableIncidents from "./pages/TableIncidents";
-import CheckDetail from "./pages/CheckDetail";
-import DefaultCheckDetail from "./pages/DefaultChecksDetail"
-import UserListDetail from "./pages/UserListDetail";
-import UserDetail from "./pages/UserListDetail/UserDetail";
-import DefaultSchedules from "./pages/DefaultSchedulesDetail";
-import DefaultWebhooksDetail from "./pages/DefaultWebhooksDetail";
-import SharedCredentialsDetail from "./pages/SharedCredentialsDetail";
-import SingleSharedCredential from "./pages/SharedCredentialsDetail/SingleSharedCredential";
-import DataDictionary from "./pages/DataDictionaryConfiguration";
-import DataDictionaryItemOverview from "./pages/DataDictionaryConfiguration/DataDictionaryItemOverview";
 import ConnectionLayout from "./components/ConnectionLayout";
 import DefinitionLayout from "./components/DefinitionLayout";
 import DashboardLayout from "./components/DashboardLayout";
