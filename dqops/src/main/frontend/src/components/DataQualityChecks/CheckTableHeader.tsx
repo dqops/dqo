19f30469af0f33b38e6c6d72f074cd2f0c033f3e
Piotr Czarnas <piotr.czarnas@gmail.com>
2024-02-02 14:18:13 +0100

    Make the check editor column for the warning threshold wider.

--- dqops/src/main/frontend/src/components/DataQualityChecks/CheckTableHeader.tsx
+++ dqops/src/main/frontend/src/components/DataQualityChecks/CheckTableHeader.tsx
@@ -361,7 +361,7 @@ const TableHeader = ({
           </div>
         </td>
         <td className="text-right whitespace-nowrap text-gray-700 py-1.5 px-4 font-semibold bg-gray-400"></td>
-        <td className="text-center whitespace-nowrap text-gray-700 py-1.5 px-4 border-b font-semibold bg-yellow-100 relative pl-1">
+        <td className="text-center whitespace-nowrap text-gray-700 py-1.5 px-4 border-b font-semibold bg-yellow-100 relative pl-1 min-w-44">
           Warning threshold
           <div className="w-5 bg-white absolute h-full right-0 top-0"></div>
 
