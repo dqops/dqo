234191e0735d0c8b7edeec5453f82601f71f08f7
Aleksy <psychologianauki@gmail.com>
2024-02-09 22:32:25 +0100

    styled button when deleting path

--- dqops/src/main/frontend/src/components/FileFormatConfiguration/FilePath.tsx
+++ dqops/src/main/frontend/src/components/FileFormatConfiguration/FilePath.tsx
@@ -16,7 +16,7 @@ export default function FilePath({
   onDeletePath
 }: TFilePathProps) {
   return (
-    <>
+    <div className="w-full">
       <div className="flex items-center font-bold ">
         <div className="text-left min-w-40 w-11/12 pr-4 py-2">
           File path or file name pattern
@@ -24,13 +24,13 @@ export default function FilePath({
         <div className=" py-2 text-center max-w-34 min-w-34 w-34">Action</div>
       </div>
       {paths.slice(0, paths.length - 1).map((x, index) => (
-        <div
-          className="pr-4 min-w-40 py-2 flex items-center w-full "
-          key={index}
-        >
-          <div className="pr-4 min-w-40 w-11/12 py-2">{x}</div>
-          <div className="px-8 max-w-34 min-w-34 py-2">
-            <div className="flex ">
+        <div className="flex items-center w-full " key={index}>
+          <div className="pr-4 min-w-40 w-11/12 py-2">
+            <div>{x}</div>
+          </div>
+          <div className="max-w-34 min-w-34 py-2">
+            <div className="flex justify-center">
+              {' '}
               <IconButton
                 color="teal"
                 size="sm"
@@ -64,6 +64,6 @@ export default function FilePath({
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
