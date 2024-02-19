72cc26ce45d5a3efbdc5b8a1c506cca9598b755f
Aleksy <psychologianauki@gmail.com>
2024-02-19 11:28:10 +0100

    removed default values from parquetFormatConfiguration

--- dqops/src/main/frontend/src/components/FileFormatConfiguration/FormatsConfiguration/ParquetFormatConfiguration.tsx
+++ dqops/src/main/frontend/src/components/FileFormatConfiguration/FormatsConfiguration/ParquetFormatConfiguration.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo } from 'react';
 import { ParquetFileFormatSpec } from '../../../api';
-import { TConfigurationItemRowBoolean } from './RowItem/TConfigurationItemRowBoolean'
-import FormatConfigurationRenderer from '../FormatConfigurationRenderer'
+import { TConfigurationItemRowBoolean } from './RowItem/TConfigurationItemRowBoolean';
+import FormatConfigurationRenderer from '../FormatConfigurationRenderer';
 
 type TParquetConfigurationProps = {
   configuration: ParquetFileFormatSpec;
@@ -12,43 +12,41 @@ export default function ParquetFormatConfiguration({
   configuration,
   onChangeConfiguration
 }: TParquetConfigurationProps) {
-  const parquetConfigurationBooleans: TConfigurationItemRowBoolean[] = useMemo(() => {
-    return [
-      {
-        label: 'Binary as string',
-        value: configuration?.binary_as_string,
-        onChange: (value) => onChangeConfiguration({ binary_as_string: value }),
-        defaultValue: false
-      },
-      {
-        label: 'Filename',
-        value: configuration?.filename,
-        onChange: (value) => onChangeConfiguration({ filename: value }),
-        defaultValue: false
-      },
-      {
-        label: 'File row number',
-        value: configuration?.file_row_number,
-        onChange: (value) => onChangeConfiguration({ file_row_number: value }),
-        defaultValue: false
-      },
-      {
-        label: 'Hive partitioning',
-        value: configuration?.hive_partitioning,
-        onChange: (value) => onChangeConfiguration({ hive_partitioning: value }),
-        defaultValue: false
-      },
-      {
-        label: 'Union by name',
-        value: configuration?.union_by_name,
-        onChange: (value) => onChangeConfiguration({ union_by_name: value }),
-        defaultValue: false
-      },
-    ];
-  }, [configuration]);
+  const parquetConfigurationBooleans: TConfigurationItemRowBoolean[] =
+    useMemo(() => {
+      return [
+        {
+          label: 'Binary as string',
+          value: configuration?.binary_as_string,
+          onChange: (value) =>
+            onChangeConfiguration({ binary_as_string: value })
+        },
+        {
+          label: 'Filename',
+          value: configuration?.filename,
+          onChange: (value) => onChangeConfiguration({ filename: value })
+        },
+        {
+          label: 'File row number',
+          value: configuration?.file_row_number,
+          onChange: (value) => onChangeConfiguration({ file_row_number: value })
+        },
+        {
+          label: 'Hive partitioning',
+          value: configuration?.hive_partitioning,
+          onChange: (value) =>
+            onChangeConfiguration({ hive_partitioning: value })
+        },
+        {
+          label: 'Union by name',
+          value: configuration?.union_by_name,
+          onChange: (value) => onChangeConfiguration({ union_by_name: value })
+        }
+      ];
+    }, [configuration]);
 
   return (
-    <FormatConfigurationRenderer 
+    <FormatConfigurationRenderer
       configurationBooleans={parquetConfigurationBooleans}
     />
   );
