90e9f8aca69716315df7a39c62d0b0a702d35d5e
Aleksy <psychologianauki@gmail.com>
2024-02-19 08:45:33 +0100

    dialogs init

---
+++ dqops/src/main/frontend/src/components/CustomTree/CollectStatisticsDialog.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CollectStatisticsDialog() {
+  return (
+    <div>CollectStatisticsDialog</div>
+  )
+}
---
+++ dqops/src/main/frontend/src/components/CustomTree/RunChecksDialog.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function RunChecksDialog() {
+  return (
+    <div>RunChecksDialog</div>
+  )
+}
