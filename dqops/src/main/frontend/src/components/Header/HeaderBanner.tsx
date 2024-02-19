5e3c9a84bf19485947fab315740e7eedab446a69
Aleksy <psychologianauki@gmail.com>
2024-02-19 20:51:52 +0100

    not displaying warning when user didnt perform actions and closes advisor

--- dqops/src/main/frontend/src/components/Header/HeaderBanner.tsx
+++ dqops/src/main/frontend/src/components/Header/HeaderBanner.tsx
@@ -10,7 +10,6 @@ import Checkbox from '../Checkbox';
 import { JobApiClient } from '../../services/apiClient';
 import { CheckTypes, ROUTES } from '../../shared/routes';
 import { useHistory, useParams } from 'react-router-dom';
-import AdvisorConfirmDialog from './AdvisorConfirmDialog';
 import SvgIcon from '../SvgIcon';
 import { useSelector } from 'react-redux';
 import { IRootState } from '../../redux/reducers';
@@ -25,12 +24,13 @@ export const HeaderBanner = ({ onClose }: HeaderBannerProps) => {
     connection
   }: { checkTypes: CheckTypes; connection: string; schema: string } =
     useParams();
-  const [confirmOpen, setConfirmOpen] = useState(false);
   const history = useHistory();
   const [scheduleConfigured, setScheduleConfigured] = useState(false);
   const [isCollected, setIsCollected] = useState(false);
   const [isProfilingChecked, setIsProfilingChecked] = useState(false);
-  const { advisorObject, userProfile } = useSelector((state: IRootState) => state.job);
+  const { advisorObject, userProfile } = useSelector(
+    (state: IRootState) => state.job
+  );
 
   const collectStatistics = () => {
     setIsCollected(true);
@@ -61,24 +61,19 @@ export const HeaderBanner = ({ onClose }: HeaderBannerProps) => {
     );
   };
 
-  const openAdvisor = () => {
-    if (scheduleConfigured && isCollected && isProfilingChecked) {
-      onClose();
-      return;
-    }
-    setConfirmOpen(true);
-  };
-
   return (
     <div className="absolute z-10 top-0 left-0 right-0">
       <Popover open={openPopover} handler={setOpenPopover}>
         <PopoverHandler>
-          <div className="h-12 bg-orange-500 flex justify-between items-center px-4 bg-opacity-90 cursor-pointer"
-          onClick={() => setOpenPopover(!openPopover)}>
+          <div
+            className="h-12 bg-orange-500 flex justify-between items-center px-4 bg-opacity-90 cursor-pointer"
+            onClick={() => setOpenPopover(!openPopover)}
+          >
             <div className="flex items-center gap-2">
               <SvgIcon name="info" className="text-white" />
               <h4 className="text-white font-bold">
-                New tables have been imported and the automatic monitoring has been scheduled. Click here for more actions.
+                New tables have been imported and the automatic monitoring has
+                been scheduled. Click here for more actions.
               </h4>
             </div>
 
@@ -93,9 +88,7 @@ export const HeaderBanner = ({ onClose }: HeaderBannerProps) => {
           </div>
         </PopoverHandler>
 
-        <PopoverContent
-          className="max-w-250 z-50 !top-12 !left-0 rounded-none text-gray-700"
-        >
+        <PopoverContent className="max-w-250 z-50 !top-12 !left-0 rounded-none text-gray-700">
           <div className="flex mb-4  items-center justify-between">
             <div className="grow">Collect basic statistics</div>
 
@@ -103,11 +96,12 @@ export const HeaderBanner = ({ onClose }: HeaderBannerProps) => {
               <Button
                 variant={'contained'}
                 color={isCollected ? 'secondary' : 'primary'}
-                label={isCollected ? "The job has started" :"Collect statistics"}
+                label={
+                  isCollected ? 'The job has started' : 'Collect statistics'
+                }
                 className="text-sm px-2 w-45"
                 onClick={isCollected ? undefined : collectStatistics}
-                disabled={userProfile.can_collect_statistics  !== true}
-                
+                disabled={userProfile.can_collect_statistics !== true}
               />
 
               <div className="px-4 flex items-center">
@@ -127,7 +121,11 @@ export const HeaderBanner = ({ onClose }: HeaderBannerProps) => {
               <Button
                 variant="contained"
                 color={isProfilingChecked ? 'secondary' : 'primary'}
-                label={isProfilingChecked ? "The job has started": "Run profiling checks"}
+                label={
+                  isProfilingChecked
+                    ? 'The job has started'
+                    : 'Run profiling checks'
+                }
                 className="text-sm px-2 w-45"
                 onClick={isProfilingChecked ? undefined : runProfilingChecks}
                 disabled={userProfile.can_run_checks !== true}
@@ -179,17 +177,11 @@ export const HeaderBanner = ({ onClose }: HeaderBannerProps) => {
               color="primary"
               label="Close advisor"
               className="px-2 my-4"
-              onClick={openAdvisor}
+              onClick={onClose}
             />
           </div>
         </PopoverContent>
       </Popover>
-
-      <AdvisorConfirmDialog
-        open={confirmOpen}
-        onClose={() => setConfirmOpen(false)}
-        onConfirm = {onClose}
-      />
     </div>
   );
 };
