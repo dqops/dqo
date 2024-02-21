dc7b100c6ce91a384ddd6ac1cb630690b7909661
Patryk Jatczak <patryk.jatczak@dqops.com>
2024-02-21 11:02:10 +0100

    duckdb connection fields for secrets in ui

--- dqops/src/main/frontend/src/components/Dashboard/DatabaseConnection/DuckDBConnection/index.tsx
+++ dqops/src/main/frontend/src/components/Dashboard/DatabaseConnection/DuckDBConnection/index.tsx
@@ -4,12 +4,15 @@ import SectionWrapper from '../../SectionWrapper';
 import {
   DuckdbParametersSpec,
   SharedCredentialListModel,
-  DuckdbParametersSpecSourceFilesTypeEnum
+  DuckdbParametersSpecSourceFilesTypeEnum,
+  DuckdbParametersSpecSecretsTypeEnum
 } from '../../../../api';
 import FileFormatConfiguration from '../../../FileFormatConfiguration/FileFormatConfiguration';
 import { TConfiguration } from '../../../../components/FileFormatConfiguration/TConfiguration';
 import FilePath from '../../../FileFormatConfiguration/FilePath';
 import KeyValueProperties from '../../../FileFormatConfiguration/KeyValueProperties';
+import Select from '../../../Select';
+import FieldTypeInput from '../../../Connection/ConnectionView/FieldTypeInput';
 
 interface IDuckdbConnectionProps {
   duckdb?: DuckdbParametersSpec;
@@ -54,8 +57,68 @@ const DuckdbConnection = ({
   const onChangeFile = (val: DuckdbParametersSpecSourceFilesTypeEnum) =>
     setFileFormatType(val);
 
+  const secretsTypeOptions = [
+    {
+      label: 'None',
+      value: undefined
+    },
+    {
+      label: 'AWS S3',
+      value: DuckdbParametersSpecSecretsTypeEnum.s3
+    },
+    {
+      label: 'Google Cloud Storage',
+      value: DuckdbParametersSpecSecretsTypeEnum.gcs
+    },
+    {
+      label: 'Azure Blob Storage',
+      value: DuckdbParametersSpecSecretsTypeEnum.azure
+    },
+    {
+      label: 'Cloudflare R2',
+      value: DuckdbParametersSpecSecretsTypeEnum.r2
+    }
+  ];
+
   return (
     <SectionWrapper title="DuckDB connection parameters" className="mb-4">
+      
+      <Select
+          label="Cloud storage type"
+          options={secretsTypeOptions}
+          className="mb-4"
+          value={ duckdb?.secrets_type }
+          onChange={(value) => { handleChange({ secrets_type: value })}}
+        />
+
+      { duckdb?.secrets_type === DuckdbParametersSpecSecretsTypeEnum.s3 &&
+        <>
+          <FieldTypeInput
+            data={sharedCredentials}
+            label="User name/Key ID"
+            className="mb-4"
+            value={duckdb?.user}
+            onChange={(value) => handleChange({ user: value })}
+          />
+          <FieldTypeInput
+            data={sharedCredentials}
+            label="Password/Token"
+            className="mb-4"
+            maskingType="password"
+            value={duckdb?.password}
+            onChange={(value) => handleChange({ password: value })}
+          />
+          <FieldTypeInput
+            data={sharedCredentials}
+            label="Region"
+            className="mb-4"
+            maskingType="region"
+            value={duckdb?.region}
+            onChange={(value) => handleChange({ region: value })}
+          />
+        </>
+      }
+
       <SectionWrapper
         title="File format configuration"
         className="text-sm my-4 text-black"
