b1271c643c07ea5dbfaef9e7ad94622af2f41f07
Patryk Jatczak <patryk.jatczak@dqops.com>
2024-02-21 11:25:33 +0100

    test for deep clone on JsonFileFormatSpec

--- dqops/src/test/java/com/dqops/metadata/sources/fileformat/JsonFileFormatSpecTest.java
+++ dqops/src/test/java/com/dqops/metadata/sources/fileformat/JsonFileFormatSpecTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -68,6 +69,38 @@ public class JsonFileFormatSpecTest extends BaseTest {
         JsonFileFormatSpec sut = new JsonFileFormatSpec();
         String output = sut.buildSourceTableOptionsString(List.of("/dev/table.json"), null);
         assertTrue(output.contains("read_json"));
+    }
+
+    @Test
+    void deepClone_onInstanceWithAllFieldsSet_clones(){
+        JsonFileFormatSpec sut = new JsonFileFormatSpec(){{
+            setAutoDetect(true);
+            setCompression("gzip");
+            setConvertStringsToIntegers(true);
+            setDateformat("%m/%d/%Y");
+            setFilename(true);
+            setFormat(JsonFormatType.array);
+            setHivePartitioning(true);
+            setIgnoreErrors(true);
+            setMaximumDepth(10L);
+            setMaximumObjectSize(200L);
+            setRecords("auto");
+            setTimestampformat("%A, %-d %B %Y - %I:%M:%S %p");
+        }};
+
+        JsonFileFormatSpec sutCloned = sut.deepClone();
+        assertNotNull(sutCloned.getAutoDetect());
+        assertNotNull(sutCloned.getCompression());
+        assertNotNull(sutCloned.getConvertStringsToIntegers());
+        assertNotNull(sutCloned.getDateformat());
+        assertNotNull(sutCloned.getFilename());
+        assertNotNull(sutCloned.getFormat());
+        assertNotNull(sutCloned.getHivePartitioning());
+        assertNotNull(sutCloned.getIgnoreErrors());
+        assertNotNull(sutCloned.getMaximumDepth());
+        assertNotNull(sutCloned.getMaximumObjectSize());
+        assertNotNull(sutCloned.getRecords());
+        assertNotNull(sutCloned.getTimestampformat());
     }
 
 }
