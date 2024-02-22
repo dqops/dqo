e4b4638b78c9dea150dce56c007214aec3cde7ca
Patryk Jatczak <patryk.jatczak@dqops.com>
2024-02-22 15:03:48 +0100

    aws config profile file provider, simplified accessing of the FileContent

---
+++ dqops/src/main/java/com/dqops/metadata/storage/localfiles/credentials/aws/AwsConfigSettingNames.java
@@ -0,0 +1,8 @@
+package com.dqops.metadata.storage.localfiles.credentials.aws;
+
+/**
+ * Constants with fields of the AWS default config profile file.
+ */
+public final class AwsConfigSettingNames {
+    public static final String REGION = "region";
+}
