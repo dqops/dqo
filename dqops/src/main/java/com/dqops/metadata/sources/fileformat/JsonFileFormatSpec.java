d4f1e8b1abf03d6143b8222932953e08eca09ab3
Patryk Jatczak <patryk.jatczak@dqops.com>
2024-02-26 09:09:22 +0100

    bugfix on presence of auto detect option on default in json format

--- dqops/src/main/java/com/dqops/metadata/sources/fileformat/JsonFileFormatSpec.java
+++ dqops/src/main/java/com/dqops/metadata/sources/fileformat/JsonFileFormatSpec.java
@@ -35,7 +35,7 @@ public class JsonFileFormatSpec extends AbstractSpec {
 
     @JsonPropertyDescription("Whether to auto-detect detect the names of the keys and data types of the values automatically")
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    private Boolean autoDetect;
+    private Boolean autoDetect = true;
 
     @JsonPropertyDescription("The compression type for the file. By default this will be detected automatically from the file extension (e.g., t.json.gz will use gzip, t.json will use none). Options are 'none', 'gzip', 'zstd', and 'auto'.")
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
