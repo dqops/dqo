c358b8b35fabf171c3ff067004c20446e35a4319
Patryk Jatczak <patryk.jatczak@dqops.com>
2024-02-29 09:10:41 +0100

    fixed types in integration tests for singlestore

--- dqops/src/integration-test/java/com/dqops/singlestore/sensors/column/patterns/SingleStoreDbColumnPatternsInvalidEmailFormatCountSensorParametersSpecIntegrationTest.java
+++ dqops/src/integration-test/java/com/dqops/singlestore/sensors/column/patterns/SingleStoreDbColumnPatternsInvalidEmailFormatCountSensorParametersSpecIntegrationTest.java
@@ -66,7 +66,7 @@ public class SingleStoreDbColumnPatternsInvalidEmailFormatCountSensorParametersS
         Table resultTable = sensorResult.getResultTable();
         Assertions.assertEquals(1, resultTable.rowCount());
         Assertions.assertEquals("actual_value", resultTable.column(0).name());
-        Assertions.assertEquals(19, (double) resultTable.column(0).get(0));
+        Assertions.assertEquals(22, (double) resultTable.column(0).get(0));
     }
 
     @Test
@@ -79,7 +79,7 @@ public class SingleStoreDbColumnPatternsInvalidEmailFormatCountSensorParametersS
         Table resultTable = sensorResult.getResultTable();
         Assertions.assertEquals(1, resultTable.rowCount());
         Assertions.assertEquals("actual_value", resultTable.column(0).name());
-        Assertions.assertEquals(19, (double) resultTable.column(0).get(0));
+        Assertions.assertEquals(22, (double) resultTable.column(0).get(0));
     }
 
     @Test
@@ -92,7 +92,7 @@ public class SingleStoreDbColumnPatternsInvalidEmailFormatCountSensorParametersS
         Table resultTable = sensorResult.getResultTable();
         Assertions.assertEquals(1, resultTable.rowCount());
         Assertions.assertEquals("actual_value", resultTable.column(0).name());
-        Assertions.assertEquals(19, (double) resultTable.column(0).get(0));
+        Assertions.assertEquals(22, (double) resultTable.column(0).get(0));
     }
 
     @Test
@@ -118,6 +118,6 @@ public class SingleStoreDbColumnPatternsInvalidEmailFormatCountSensorParametersS
         Table resultTable = sensorResult.getResultTable();
         Assertions.assertEquals(1, resultTable.rowCount());
         Assertions.assertEquals("actual_value", resultTable.column(0).name());
-        Assertions.assertEquals(19, (double) resultTable.column(0).get(0));
+        Assertions.assertEquals(22, (double) resultTable.column(0).get(0));
     }
 }
