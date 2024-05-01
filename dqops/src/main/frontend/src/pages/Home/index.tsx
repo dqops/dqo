b851f0d68de6f6f5f655e916d70729652571c872
Aleksy <psychologianauki@gmail.com>
2024-05-01 12:46:38 +0200

    separate routing to tables

--- dqops/src/main/frontend/src/pages/Home/index.tsx
+++ dqops/src/main/frontend/src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../../components/Header';
 import Tabs from '../../components/Tabs';
 import GlobalTables from './GlobalTables';
@@ -9,13 +10,17 @@ const tabs = [
   { label: 'Tables', value: 'tables' }
 ];
 const HomePage = () => {
-  const [activeTab, setActiveTab] = useState('home');
+  const history = useHistory();
+  const activeTab = location.pathname.replace('/', '');
+  const onChange = (activeTab: string) => {
+    history.push('/' + activeTab);
+  };
   return (
     <div className="min-h-screen overflow-hidden">
       <Header />
       tabs
       <div className="w-full mt-12 border-b border-gray-300 px-0">
-        <Tabs tabs={tabs} onChange={setActiveTab} activeTab={activeTab} />
+        <Tabs tabs={tabs} onChange={onChange} activeTab={activeTab} />
       </div>
       {activeTab === 'home' && <StaticHomePage />}
       {activeTab === 'tables' && <GlobalTables />}
