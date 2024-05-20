82e46c32e5b1e6fc9c76439e8c08994ff5ee025c
Piotr Czarnas <piotr.czarnas@gmail.com>
2024-05-20 19:11:19 +0200

    Return an empty column DQ status model when the status for the table is missing.

--- dqops/src/main/java/com/dqops/rest/controllers/ColumnsController.java
+++ dqops/src/main/java/com/dqops/rest/controllers/ColumnsController.java
@@ -182,7 +182,7 @@ public class ColumnsController {
                 TableCurrentDataQualityStatusModel currentTableStatus = this.tableStatusCache.getCurrentTableStatus(tableStatusKey, checkType.orElse(null));
                 if (currentTableStatus != null) {
                     ColumnCurrentDataQualityStatusModel columnQualityStatusModel = currentTableStatus.getColumns().get(listModel.getColumnName());
-                    listModel.setDataQualityStatus(columnQualityStatusModel != null ? columnQualityStatusModel.shallowCloneWithoutChecks() : null);
+                    listModel.setDataQualityStatus(columnQualityStatusModel != null ? columnQualityStatusModel.shallowCloneWithoutChecks() : new ColumnCurrentDataQualityStatusModel());
                 }
             });
 
@@ -199,7 +199,8 @@ public class ColumnsController {
                                         TableCurrentDataQualityStatusModel currentTableStatus = this.tableStatusCache.getCurrentTableStatus(tableStatusKey, checkType.orElse(null));
                                         if (currentTableStatus != null) {
                                             ColumnCurrentDataQualityStatusModel columnQualityStatusModel = currentTableStatus.getColumns().get(columnListModel.getColumnName());
-                                            columnListModel.setDataQualityStatus(columnQualityStatusModel != null ? columnQualityStatusModel.shallowCloneWithoutChecks() : null);
+                                            columnListModel.setDataQualityStatus(columnQualityStatusModel != null ?
+                                                    columnQualityStatusModel.shallowCloneWithoutChecks() : null);
                                         }
                                     }
                                     return columnListModel;
