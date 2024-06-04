97120a0877dd137ad13f76b4ab87d99704d40858
Aleksy <psychologianauki@gmail.com>
2024-06-04 17:34:25 +0200

    rouding dataQualityKpi number

--- dqops/src/main/frontend/src/pages/Schema/SchemaTableItemDimensions.tsx
+++ dqops/src/main/frontend/src/pages/Schema/SchemaTableItemDimensions.tsx
@@ -198,7 +198,11 @@ const renderSecondLevelTooltip = (
         <div className="flex gap-x-2">
           <div className="w-49">Data quality KPI:</div>
           <div>
-            {data?.data_quality_kpi ? data?.data_quality_kpi + '%' : ''}
+            {data?.data_quality_kpi
+              ? Number(data?.data_quality_kpi) === 100
+                ? '100%'
+                : Number(data?.data_quality_kpi).toFixed(2) + '%'
+              : ''}
           </div>
         </div>
       </div>
