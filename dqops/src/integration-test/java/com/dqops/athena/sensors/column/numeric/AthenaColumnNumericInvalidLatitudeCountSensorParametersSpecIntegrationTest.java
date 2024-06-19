3ace6295d7eb0b586088807509f0bbe4a005033a
Patryk Jatczak <patryk.jatczak@dqops.com>
2024-06-19 13:25:11 +0200

    error sampling templates for column/numeric/invalid_latitude_count

--- dqops/src/integration-test/java/com/dqops/athena/sensors/column/numeric/AthenaColumnNumericInvalidLatitudeCountSensorParametersSpecIntegrationTest.java
+++ dqops/src/integration-test/java/com/dqops/athena/sensors/column/numeric/AthenaColumnNumericInvalidLatitudeCountSensorParametersSpecIntegrationTest.java
@@ -23,6 +23,9 @@ import com.dqops.execution.sensors.DataQualitySensorRunnerObjectMother;
 import com.dqops.execution.sensors.SensorExecutionResult;
 import com.dqops.execution.sensors.SensorExecutionRunParameters;
 import com.dqops.execution.sensors.SensorExecutionRunParametersObjectMother;
+import com.dqops.metadata.groupings.DataGroupingConfigurationSpec;
+import com.dqops.metadata.groupings.DataGroupingDimensionSource;
+import com.dqops.metadata.groupings.DataGroupingDimensionSpec;
 import com.dqops.metadata.sources.ConnectionSpec;
 import com.dqops.metadata.storage.localfiles.userhome.UserHomeContext;
 import com.dqops.metadata.storage.localfiles.userhome.UserHomeContextObjectMother;
@@ -37,6 +40,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import tech.tablesaw.api.Table;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @SpringBootTest
@@ -140,5 +147,89 @@ public class AthenaColumnNumericInvalidLatitudeCountSensorParametersSpecIntegrat
         Assertions.assertEquals(4, resultTable.rowCount());
         Assertions.assertEquals("actual_value", resultTable.column(0).name());
         Assertions.assertEquals(5L, resultTable.column(0).get(0));
+    }
+
+    @Test
+    void runSensor_whenErrorSamplingSensorExecutedWithNoGroupingAndNoIdColumns_thenReturnsErrorSamples() {
+        SensorExecutionRunParameters runParameters = SensorExecutionRunParametersObjectMother.createForTableColumnForErrorSampling(
+                sampleTableMetadata, "gcs", this.checkSpec);
+
+        SensorExecutionResult sensorResult = DataQualitySensorRunnerObjectMother.executeSensor(this.userHomeContext, runParameters);
+
+        Table resultTable = sensorResult.getResultTable();
+        Assertions.assertEquals(13, resultTable.rowCount());
+        Assertions.assertEquals(1, resultTable.columnCount());
+        Assertions.assertEquals("actual_value", resultTable.column(0).name());
+        List<Double> sampleValues = List.of(resultTable.column("actual_value").asObjectArray())
+                .stream().map(val -> ValueConverter.toDouble(val))
+                .collect(Collectors.toList());
+
+        Assertions.assertTrue(sampleValues.contains(91.18464));
+    }
+
+    @Test
+    void runSensor_whenErrorSamplingSensorExecutedWithNoGroupingButWithIdColumns_thenReturnsErrorSamples() {
+        sampleTableMetadata.getTableSpec().getColumns().getAt(0).setId(true);
+
+        SensorExecutionRunParameters runParameters = SensorExecutionRunParametersObjectMother.createForTableColumnForErrorSampling(
+                sampleTableMetadata, "gcs", this.checkSpec);
+
+        SensorExecutionResult sensorResult = DataQualitySensorRunnerObjectMother.executeSensor(this.userHomeContext, runParameters);
+
+        Table resultTable = sensorResult.getResultTable();
+        Assertions.assertEquals(13, resultTable.rowCount());
+        Assertions.assertEquals(2, resultTable.columnCount());
+        Assertions.assertEquals("actual_value", resultTable.column(0).name());
+        Assertions.assertEquals("row_id_1", resultTable.column(1).name());
+        List<Double> sampleValues = List.of(resultTable.column("actual_value").asObjectArray())
+                .stream().map(val -> ValueConverter.toDouble(val))
+                .collect(Collectors.toList());
+        Assertions.assertTrue(sampleValues.contains(91.18464));
+
+        List<Integer> rowId1Values = List.of(resultTable.column("row_id_1").asObjectArray())
+                .stream().map(val -> ValueConverter.toInteger(val))
+                .collect(Collectors.toList());
+        Assertions.assertTrue(rowId1Values.contains(20));
+    }
+
+    @Test
+    void runSensor_whenErrorSamplingSensorExecutedWithDataGroupingAndWithIdColumns_thenReturnsErrorSamples() {
+        DataGroupingConfigurationSpec dataGroupingConfigurationSpec = new DataGroupingConfigurationSpec() {{
+            setLevel1(new DataGroupingDimensionSpec() {{
+                setSource(DataGroupingDimensionSource.column_value);
+                setColumn("gcs_ok");
+            }});
+        }};
+        sampleTableMetadata.getTableSpec().setDefaultDataGroupingConfiguration(dataGroupingConfigurationSpec);
+        sampleTableMetadata.getTableSpec().getColumns().getAt(0).setId(true);
+
+        SensorExecutionRunParameters runParameters = SensorExecutionRunParametersObjectMother.createForTableColumnForErrorSampling(
+                sampleTableMetadata, "gcs", this.checkSpec);
+
+        SensorExecutionResult sensorResult = DataQualitySensorRunnerObjectMother.executeSensor(this.userHomeContext, runParameters);
+
+        Table resultTable = sensorResult.getResultTable();
+        Assertions.assertEquals(13, resultTable.rowCount());
+        Assertions.assertEquals(4, resultTable.columnCount());
+        Assertions.assertEquals("actual_value", resultTable.column(0).name());
+        Assertions.assertEquals("sample_index", resultTable.column(1).name());
+        Assertions.assertEquals("grouping_level_1", resultTable.column(2).name());
+        Assertions.assertEquals("row_id_1", resultTable.column(3).name());
+        List<Double> sampleValues = List.of(resultTable.column("actual_value").asObjectArray())
+                .stream().map(val -> ValueConverter.toDouble(val))
+                .collect(Collectors.toList());
+        Assertions.assertTrue(sampleValues.contains(91.18464));
+
+        List<Integer> groupingLevel1Values = new ArrayList<>(
+                List.of(resultTable.column("grouping_level_1").asObjectArray())
+                        .stream().map(val -> ValueConverter.toInteger(val))
+                        .collect(Collectors.toSet()));
+        Assertions.assertEquals(1, groupingLevel1Values.size());
+        Assertions.assertTrue(groupingLevel1Values.contains(0));
+
+        List<Integer> rowId1Values = List.of(resultTable.column("row_id_1").asObjectArray())
+                .stream().map(val -> ValueConverter.toInteger(val))
+                .collect(Collectors.toList());
+        Assertions.assertTrue(rowId1Values.contains(20));
     }
 }
