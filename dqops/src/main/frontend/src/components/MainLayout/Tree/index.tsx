de296dfb63dd4b0888b14df3ab77d2103bd8b358
Aleksy <psychologianauki@gmail.com>
2024-06-24 18:34:14 +0200

    filtering tables in tree functionality

--- dqops/src/main/frontend/src/components/MainLayout/Tree/index.tsx
+++ dqops/src/main/frontend/src/components/MainLayout/Tree/index.tsx
@@ -41,7 +41,9 @@ const Tree = () => {
     refreshNode,
     setTreeData,
     loadMoreTables,
-    tablesLoading
+    tablesLoading,
+    searchTable,
+    loadedTables
   } = useTree();
   const { checkTypes }: { checkTypes: CheckTypes } = useDecodedParams();
   const [isOpen, setIsOpen] = useState(false);
@@ -69,13 +71,6 @@ const Tree = () => {
       }
     }
   }, [advisorJobId, job_dictionary_state[advisorJobId]]);
-
-  const onChangeSearchTable = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    parentId: string
-  ) => {
-    setSearch({ ...search, [parentId]: e.target.value });
-  };
 
   const handleNodeClick = (node: CustomTreeNode) => {
     switchTab(node, checkTypes);
@@ -497,22 +492,41 @@ const Tree = () => {
     );
 
     const searchForTable = () => {
+      const onChangeSearchTable = (
+        e: React.ChangeEvent<HTMLInputElement>,
+        parentId: string
+      ) => {
+        setSearch({ ...search, [parentId]: e.target.value });
+      };
+      const onKeyDownSearchTable = (
+        e: React.KeyboardEvent<HTMLInputElement>,
+        parentId: string
+      ) => {
+        if (e.key === 'Enter') {
+          searchTable(groupedData[parentId], groupedData, search[parentId]);
+        }
+      };
+
       return (
         <div
           className={clsx(
-            'ml-4 pl-7 cursor-pointer flex items-center gap-x-2 text-sm py-1.5 mb-0.5'
+            'ml-4 pl-7 cursor-pointer flex items-center gap-x-2 text-sm py-1 mb-0.5'
           )}
         >
           <Input
-            className="w-1/2"
+            className="w-1/2 !h-7"
             placeholder="Search"
             value={search[parentId]}
             onChange={(e) => onChangeSearchTable(e, parentId)}
+            onKeyDown={(e) => onKeyDownSearchTable(e, parentId)}
           />
           <Button
-            leftIcon={<SvgIcon name="search" className="w-4 h-4" />}
+            leftIcon={<SvgIcon name="search" className="w-3 h-3" />}
             color="primary"
-            className="ml-0 mr-0 pl-2 pr-2"
+            className="ml-0 mr-0 pl-2 pr-2 !h-7"
+            onClick={() =>
+              searchTable(groupedData[parentId], groupedData, search[parentId])
+            }
           ></Button>
         </div>
       );
@@ -526,13 +540,19 @@ const Tree = () => {
             <div key={item.id}>{renderTreeNode(item, deep)}</div>
           </>
         ))}
-        {isTableLevel && (
+        {isTableLevel && !loadedTables[parentId] && (
           <div
             className={clsx(
-              'ml-4 pl-7 cursor-pointer flex text-sm hover:bg-gray-100 py-1.5 mb-0.5',
+              'ml-4 pl-7 cursor-pointer flex text-sm hover:bg-gray-100 py-1.5 mb-0.5 text-teal-500',
               parentId === tablesLoading && 'pl-0'
             )}
-            onClick={() => loadMoreTables(groupedData[parentId], groupedData)}
+            onClick={() =>
+              loadMoreTables(
+                groupedData[parentId],
+                groupedData,
+                search[parentId]
+              )
+            }
           >
             {parentId === tablesLoading && (
               <Loader className="w-4 h-4 ml-1" isFull={false} />
