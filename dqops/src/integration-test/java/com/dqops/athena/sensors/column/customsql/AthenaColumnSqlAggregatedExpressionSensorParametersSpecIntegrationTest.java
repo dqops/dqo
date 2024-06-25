4dd32fdb4335a7f5193bbbc9898009304d723120
Patryk Jatczak <patryk.jatczak@dqops.com>
2024-06-25 14:55:27 +0200

    error sampling templates for column/customsql

--- dqops/src/integration-test/java/com/dqops/athena/sensors/column/customsql/AthenaColumnSqlAggregatedExpressionSensorParametersSpecIntegrationTest.java
+++ dqops/src/integration-test/java/com/dqops/athena/sensors/column/customsql/AthenaColumnSqlAggregatedExpressionSensorParametersSpecIntegrationTest.java
@@ -38,6 +38,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import tech.tablesaw.api.Table;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootTest
 public class AthenaColumnSqlAggregatedExpressionSensorParametersSpecIntegrationTest extends BaseAthenaIntegrationTest {
     private ColumnSqlAggregatedExpressionSensorParametersSpec sut;
@@ -129,5 +132,24 @@ public class AthenaColumnSqlAggregatedExpressionSensorParametersSpecIntegrationT
         Assertions.assertEquals(1, resultTable.rowCount());
         Assertions.assertEquals("actual_value", resultTable.column(0).name());
         Assertions.assertEquals(20L, resultTable.column(0).get(0));
+    }
+
+    @Test
+    void runSensor_whenErrorSamplingSensorExecutedWithNoGroupingAndNoIdColumns_thenReturnsDataSamples() {
+
+        SensorExecutionRunParameters runParameters = SensorExecutionRunParametersObjectMother.createForTableColumnForErrorSampling(
+                sampleTableMetadata, "surrounded_by_whitespace", this.checkSpec);
+
+        SensorExecutionResult sensorResult = DataQualitySensorRunnerObjectMother.executeSensor(this.userHomeContext, runParameters);
+
+        Table resultTable = sensorResult.getResultTable();
+        Assertions.assertEquals(27, resultTable.rowCount());
+        Assertions.assertEquals(1, resultTable.columnCount());
+        Assertions.assertEquals("actual_value", resultTable.column(0).name());
+        List<String> sampleValues = List.of(resultTable.column("actual_value").asObjectArray())
+                .stream().map(val -> String.valueOf(val))
+                .collect(Collectors.toList());
+
+        Assertions.assertTrue(sampleValues.contains("  name"));
     }
 }
