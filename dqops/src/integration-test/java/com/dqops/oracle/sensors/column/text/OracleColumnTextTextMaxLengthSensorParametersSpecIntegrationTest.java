01259ce817e58c13f0ee1461b80562df419bfc5a
Patryk Jatczak <patryk.jatczak@dqops.com>
2024-06-25 09:08:54 +0200

    error sampling templates for column/text

--- dqops/src/integration-test/java/com/dqops/oracle/sensors/column/text/OracleColumnTextTextMaxLengthSensorParametersSpecIntegrationTest.java
+++ dqops/src/integration-test/java/com/dqops/oracle/sensors/column/text/OracleColumnTextTextMaxLengthSensorParametersSpecIntegrationTest.java
@@ -37,6 +37,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import tech.tablesaw.api.Table;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @SpringBootTest
 public class OracleColumnTextTextMaxLengthSensorParametersSpecIntegrationTest extends BaseOracleIntegrationTest {
@@ -150,6 +153,25 @@ public class OracleColumnTextTextMaxLengthSensorParametersSpecIntegrationTest ex
         Assertions.assertEquals(1, resultTable.rowCount());
         Assertions.assertEquals("actual_value", resultTable.column(0).name());
         Assertions.assertEquals(9, ValueConverter.toInteger(resultTable.column(0).get(0)));
+    }
+
+    @Test
+    void runSensor_whenErrorSamplingSensorExecutedWithNoGroupingAndNoIdColumns_thenReturnsDataSamples() {
+
+        SensorExecutionRunParameters runParameters = SensorExecutionRunParametersObjectMother.createForTableColumnForErrorSampling(
+                sampleTableMetadata, "length_string", this.checkSpec);
+
+        SensorExecutionResult sensorResult = DataQualitySensorRunnerObjectMother.executeSensor(this.userHomeContext, runParameters);
+
+        Table resultTable = sensorResult.getResultTable();
+        Assertions.assertEquals(30, resultTable.rowCount());
+        Assertions.assertEquals(1, resultTable.columnCount());
+        Assertions.assertEquals("actual_value", resultTable.column(0).name());
+        List<String> sampleValues = List.of(resultTable.column("actual_value").asObjectArray())
+                .stream().map(val -> String.valueOf(val))
+                .collect(Collectors.toList());
+
+        Assertions.assertTrue(sampleValues.contains("abcd"));
     }
 
 }
