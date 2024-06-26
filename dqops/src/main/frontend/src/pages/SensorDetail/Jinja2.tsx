4e65947c1b50b3660f5a498db9c598ed70b2371f
Aleksy <psychologianauki@gmail.com>
2024-06-26 14:12:55 +0200

    added error sampling template to sensorDetail section

--- dqops/src/main/frontend/src/pages/SensorDetail/Jinja2.tsx
+++ dqops/src/main/frontend/src/pages/SensorDetail/Jinja2.tsx
@@ -1,25 +1,22 @@
-import React from "react";
-import CodeMirror from '@uiw/react-codemirror';
-import { ProviderSensorModel } from "../../api";
 import { StreamLanguage } from '@codemirror/language';
 import { jinja2 } from '@codemirror/legacy-modes/mode/jinja2';
-import { useSelector } from "react-redux";
-import { IRootState } from "../../redux/reducers";
+import CodeMirror from '@uiw/react-codemirror';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { IRootState } from '../../redux/reducers';
 
 type Jinja2Props = {
-  providerSensor?: ProviderSensorModel;
-  onChange: (value: Partial<ProviderSensorModel>) => void;
-}
+  value?: string;
+  onChange: (value: string) => void;
+};
 
-const Jinja2Code = ({ providerSensor, onChange }: Jinja2Props) => {
-  const {  userProfile } = useSelector(
-    (state: IRootState) => state.job || {}
-  );
+const Jinja2Code = ({ value, onChange }: Jinja2Props) => {
+  const { userProfile } = useSelector((state: IRootState) => state.job || {});
   return (
     <div className="flex-1 overflow-auto max-h-100 min-h-100">
       <CodeMirror
-        value={providerSensor?.sqlTemplate}
-        onChange={(value) => onChange({ sqlTemplate: value })}
+        value={value}
+        onChange={onChange}
         extensions={[StreamLanguage.define(jinja2)]}
         readOnly={userProfile.can_manage_definitions !== true}
       />
