e6d9cf246462bcd0090dfcfb9ab4b5d7d0391c30
Patryk Jatczak <patryk.jatczak@dqops.com>
2024-06-26 11:54:47 +0200

    error sampling templates for column/accepted values - country and currency

--- dqops/src/integration-test/java/com/dqops/redshift/sensors/column/acceptedvalues/RedshiftColumnTextTextValidCurrencyCodePercentSensorParametersSpecIntegrationTest.java
+++ dqops/src/integration-test/java/com/dqops/redshift/sensors/column/acceptedvalues/RedshiftColumnTextTextValidCurrencyCodePercentSensorParametersSpecIntegrationTest.java
@@ -22,6 +22,9 @@ import com.dqops.execution.sensors.DataQualitySensorRunnerObjectMother;
 import com.dqops.execution.sensors.SensorExecutionResult;
 import com.dqops.execution.sensors.SensorExecutionRunParameters;
 import com.dqops.execution.sensors.SensorExecutionRunParametersObjectMother;
+import com.dqops.metadata.groupings.DataGroupingConfigurationSpec;
+import com.dqops.metadata.groupings.DataGroupingDimensionSource;
+import com.dqops.metadata.groupings.DataGroupingDimensionSpec;
 import com.dqops.metadata.storage.localfiles.userhome.UserHomeContext;
 import com.dqops.metadata.storage.localfiles.userhome.UserHomeContextObjectMother;
 import com.dqops.redshift.BaseRedshiftIntegrationTest;
@@ -30,11 +33,16 @@ import com.dqops.sampledata.SampleCsvFileNames;
 import com.dqops.sampledata.SampleTableMetadata;
 import com.dqops.sampledata.SampleTableMetadataObjectMother;
 import com.dqops.sensors.column.acceptedvalues.ColumnTextTextValidCurrencyCodePercentSensorParametersSpec;
+import com.dqops.testutils.ValueConverter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import tech.tablesaw.api.Table;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @SpringBootTest
@@ -136,5 +144,93 @@ public class RedshiftColumnTextTextValidCurrencyCodePercentSensorParametersSpecI
         Assertions.assertEquals(1, resultTable.rowCount());
         Assertions.assertEquals("actual_value", resultTable.column(0).name());
         Assertions.assertEquals(64.0, resultTable.column(0).get(0));
+    }
+
+    @Test
+    void runSensor_whenErrorSamplingSensorExecutedWithNoGroupingAndNoIdColumns_thenReturnsErrorSamples() {
+        SensorExecutionRunParameters runParameters = SensorExecutionRunParametersObjectMother.createForTableColumnForErrorSampling(
+                sampleTableMetadata, "valid_currency_code", this.checkSpec);
+
+        SensorExecutionResult sensorResult = DataQualitySensorRunnerObjectMother.executeSensor(this.userHomeContext, runParameters);
+
+        Table resultTable = sensorResult.getResultTable();
+        Assertions.assertEquals(9, resultTable.rowCount());
+        Assertions.assertEquals(1, resultTable.columnCount());
+        Assertions.assertEquals("actual_value", resultTable.column(0).name());
+        List<String> sampleValues = List.of(resultTable.column("actual_value").asObjectArray())
+                .stream().map(val -> String.valueOf(val))
+                .collect(Collectors.toList());
+
+        Assertions.assertTrue(sampleValues.contains("denar"));
+    }
+
+    @Test
+    void runSensor_whenErrorSamplingSensorExecutedWithNoGroupingButWithIdColumns_thenReturnsErrorSamples() {
+        sampleTableMetadata.getTableSpec().getColumns().getAt(0).setId(true);
+        sampleTableMetadata.getTableSpec().getColumns().getAt(1).setId(true);
+
+        SensorExecutionRunParameters runParameters = SensorExecutionRunParametersObjectMother.createForTableColumnForErrorSampling(
+                sampleTableMetadata, "valid_currency_code", this.checkSpec);
+
+        SensorExecutionResult sensorResult = DataQualitySensorRunnerObjectMother.executeSensor(this.userHomeContext, runParameters);
+
+        Table resultTable = sensorResult.getResultTable();
+        Assertions.assertEquals(9, resultTable.rowCount());
+        Assertions.assertEquals(3, resultTable.columnCount());
+        Assertions.assertEquals("actual_value", resultTable.column(0).name());
+        Assertions.assertEquals("row_id_1", resultTable.column(1).name());
+        Assertions.assertEquals("row_id_2", resultTable.column(2).name());
+        List<String> sampleValues = List.of(resultTable.column("actual_value").asObjectArray())
+                .stream().map(val -> String.valueOf(val))
+                .collect(Collectors.toList());
+        Assertions.assertTrue(sampleValues.contains("denar"));
+
+        List<Integer> rowId1Values = List.of(resultTable.column("row_id_1").asObjectArray())
+                .stream().map(val -> ValueConverter.toInteger(val))
+                .collect(Collectors.toList());
+        Assertions.assertTrue(rowId1Values.contains(25));
+    }
+
+    @Test
+    void runSensor_whenErrorSamplingSensorExecutedWithDataGroupingAndWithIdColumns_thenReturnsErrorSamples() {
+        DataGroupingConfigurationSpec dataGroupingConfigurationSpec = new DataGroupingConfigurationSpec() {{
+            setLevel1(new DataGroupingDimensionSpec() {{
+                setSource(DataGroupingDimensionSource.column_value);
+                setColumn("valid_currency_code_ok");
+            }});
+        }};
+        sampleTableMetadata.getTableSpec().setDefaultDataGroupingConfiguration(dataGroupingConfigurationSpec);
+        sampleTableMetadata.getTableSpec().getColumns().getAt(0).setId(true);
+        sampleTableMetadata.getTableSpec().getColumns().getAt(1).setId(true);
+
+        SensorExecutionRunParameters runParameters = SensorExecutionRunParametersObjectMother.createForTableColumnForErrorSampling(
+                sampleTableMetadata, "valid_currency_code", this.checkSpec);
+
+        SensorExecutionResult sensorResult = DataQualitySensorRunnerObjectMother.executeSensor(this.userHomeContext, runParameters);
+
+        Table resultTable = sensorResult.getResultTable();
+        Assertions.assertEquals(9, resultTable.rowCount());
+        Assertions.assertEquals(5, resultTable.columnCount());
+        Assertions.assertEquals("actual_value", resultTable.column(0).name());
+        Assertions.assertEquals("sample_index", resultTable.column(1).name());
+        Assertions.assertEquals("grouping_level_1", resultTable.column(2).name());
+        Assertions.assertEquals("row_id_1", resultTable.column(3).name());
+        Assertions.assertEquals("row_id_2", resultTable.column(4).name());
+        List<String> sampleValues = List.of(resultTable.column("actual_value").asObjectArray())
+                .stream().map(val -> String.valueOf(val))
+                .collect(Collectors.toList());
+        Assertions.assertTrue(sampleValues.contains("denar"));
+
+        List<Integer> groupingLevel1Values = new ArrayList<>(
+                List.of(resultTable.column("grouping_level_1").asObjectArray())
+                        .stream().map(val -> ValueConverter.toInteger(val))
+                        .collect(Collectors.toSet()));
+        Assertions.assertEquals(1, groupingLevel1Values.size());
+        Assertions.assertTrue(groupingLevel1Values.contains(0));
+
+        List<Integer> rowId1Values = List.of(resultTable.column("row_id_1").asObjectArray())
+                .stream().map(val -> ValueConverter.toInteger(val))
+                .collect(Collectors.toList());
+        Assertions.assertTrue(rowId1Values.contains(25));
     }
 }
