c8b103c89a57f275927f8c53bc4f98fd821c4054
Aleksy <psychologianauki@gmail.com>
2024-07-15 19:26:57 +0200

    not displaying second level tabs if user is on free trial

--- dqops/src/main/frontend/src/pages/ColumnView/ColumnPartitionedChecksView.tsx
+++ dqops/src/main/frontend/src/pages/ColumnView/ColumnPartitionedChecksView.tsx
@@ -233,7 +233,8 @@ const ColumnPartitionedChecksView = () => {
       />
       {userProfile &&
         userProfile.license_type &&
-        userProfile.license_type?.toLowerCase() !== 'free' && (
+        userProfile.license_type?.toLowerCase() !== 'free' &&
+        !userProfile.trial_period_expires_at && (
           <div className="border-b border-gray-300">
             <Tabs tabs={tabs} activeTab={tab} onChange={onChangeTab} />
           </div>
