941dd1527eb09d457ce4849390d1a8411469e70e
Piotr Czarnas <piotr.czarnas@gmail.com>
2024-08-09 22:58:26 +0200

    Do not suggest date in range checks for columns without dates.

--- dqops/src/main/java/com/dqops/checks/column/checkspecs/datetime/ColumnDateInRangePercentCheckSpec.java
+++ dqops/src/main/java/com/dqops/checks/column/checkspecs/datetime/ColumnDateInRangePercentCheckSpec.java
@@ -278,8 +278,8 @@ public class ColumnDateInRangePercentCheckSpec
         boolean isDateType = columnTypeCategory == DataTypeCategory.datetime_date ||
                 columnTypeCategory == DataTypeCategory.datetime_datetime ||
                 columnTypeCategory == DataTypeCategory.datetime_timestamp ||
-                ((columnTypeCategory == DataTypeCategory.text || columnTypeCategory == null) && !columnDataAssetProfilingResults.getSampleValues().isEmpty() ||
-                        columnDataAssetProfilingResults.getSampleValues().stream().anyMatch(sample -> sample.getInstantValue() != null));
+                ((columnTypeCategory == DataTypeCategory.text || columnTypeCategory == null) && !columnDataAssetProfilingResults.getSampleValues().isEmpty() &&
+                        columnDataAssetProfilingResults.getSampleValues().stream().allMatch(sample -> sample.getInstantValue() != null));
 
         if (sourceProfilingCheck.getActualValue() == null) {
             if (!isDateType) {
