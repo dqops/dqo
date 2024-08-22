5feeb8991ec6222d380da4a070add01cee01d443
Patryk Jatczak <patryk.jatczak@dqops.com>
2024-08-22 12:33:44 +0200

    renamed default notifications to global incident notifications

--- dqops/src/main/frontend/src/pages/DefaultWebhooksDetail/index.tsx
+++ dqops/src/main/frontend/src/pages/DefaultWebhooksDetail/index.tsx
@@ -113,7 +113,7 @@ export default function DefaultWebhooksDetail() {
   };
 
   const onBack = () => {
-    dispatch(updateTabLabel('Default notifications', activeTab ?? ''));
+    dispatch(updateTabLabel('Global incident notifications', activeTab ?? ''));
 
     setPatternNameEdit('');
     setAddNotificationPattern(false);
@@ -155,7 +155,7 @@ export default function DefaultWebhooksDetail() {
           <div className="flex justify-between px-4 border-b border-gray-300 h-14 items-center flex-shrink-0">
             <div className="flex items-center justify-between w-full">
               <div className="text-lg font-semibold truncate">
-                Default incident notification configuration
+                Global incident notification configuration
               </div>
             </div>
           </div>
