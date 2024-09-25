f63ddc2cfa2a8456618c3d89492f509c5600169f
Aleksy <psychologianauki@gmail.com>
2024-09-25 15:45:01 +0200

    table data lineage init

--- dqops/src/main/frontend/src/components/Connection/TableView/SourceTables/SourceTables.tsx
+++ dqops/src/main/frontend/src/components/Connection/TableView/SourceTables/SourceTables.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { TableLineageModel } from '../../../../api';
+import { DataLineageApiClient } from '../../../../services/apiClient';
+import { useDecodedParams } from '../../../../utils';
 import Button from '../../../Button';
 import SvgIcon from '../../../SvgIcon';
 import TableActionGroup from '../TableActionGroup';
@@ -6,12 +9,31 @@ import SourceTableDetail from './SourceTableDetail';
 import SourceTablesTable from './SourceTablesTable';
 
 export default function SourceTables() {
+  const {
+    connection,
+    schema,
+    table
+  }: { connection: string; schema: string; table: string } = useDecodedParams();
   const [addSourceTable, setAddSourceTable] = React.useState(false);
   const [sourceTableEdit, setSourceTableEdit] = React.useState<{
     connection: string;
     schema: string;
     table: string;
   } | null>(null);
+
+  const [tableDataLineageGraph, setTableDataLineageGraph] =
+    React.useState<TableLineageModel>({});
+
+  useEffect(() => {
+    DataLineageApiClient.getTableDataLineageGraph(
+      connection,
+      schema,
+      table
+    ).then((response) => {
+      setTableDataLineageGraph(response.data);
+    });
+  }, []);
+  console.log(tableDataLineageGraph);
 
   const onBack = () => {
     setAddSourceTable(false);
