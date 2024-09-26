2257f0aecc3f26381e3ed88a63b8fbf8af64b7e3
Aleksy <psychologianauki@gmail.com>
2024-09-26 13:01:00 +0200

    label of data lineage

--- dqops/src/main/frontend/src/components/DataLineage/DataLineage.tsx
+++ dqops/src/main/frontend/src/components/DataLineage/DataLineage.tsx
@@ -4,7 +4,7 @@ import SourceTables from '../Connection/TableView/SourceTables/SourceTables';
 import DataLineageGraph from '../DataLineageGraph/DataLineageGraph';
 import Tabs from '../Tabs';
 const tabs = [
-  { label: 'Data lineage table', value: 'data-lineage' },
+  { label: 'Source tables', value: 'data-lineage' },
   { label: 'Data lineage graph', value: 'data-lineage-graph' }
 ];
 export default function DataLineage() {
