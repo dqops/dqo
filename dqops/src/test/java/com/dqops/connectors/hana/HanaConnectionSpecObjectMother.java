ca077d35da99a54953061cf0ade63d9a8e2b08b0
Patryk Jatczak <patryk.jatczak@dqops.com>
2024-09-30 11:08:12 +0200

    configuration for hana docker

--- dqops/src/test/java/com/dqops/connectors/hana/HanaConnectionSpecObjectMother.java
+++ dqops/src/test/java/com/dqops/connectors/hana/HanaConnectionSpecObjectMother.java
@@ -18,6 +18,7 @@ package com.dqops.connectors.hana;
 
 import com.dqops.connectors.ProviderType;
 import com.dqops.metadata.sources.ConnectionSpec;
+import org.testcontainers.containers.GenericContainer;
 
 public class HanaConnectionSpecObjectMother {
     private static HanaContainer sharedContainer;
@@ -26,20 +27,41 @@ public class HanaConnectionSpecObjectMother {
      * Creates a shared HANA container using Testcontainers. The container will be stopped when the unit/integration session will finish.
      * @return Shared container with a started HANA instance.
      */
-//    public static HanaContainer<?> getSharedContainer() {
-////        if (sharedContainer == null) {
-////            sharedContainer = new HanaContainer<>();
-////            sharedContainer.start();
-////        }
-////        return sharedContainer;
-//    }
+    public static HanaContainer<?> getSharedContainer() {
+        if (sharedContainer == null) {
+            sharedContainer = new HanaContainer<>();
+            sharedContainer.start();
+        }
+        return sharedContainer;
+    }
 
     /**
-     * Creates a default connection spec to HANA database that should be started by test containers.
+     * Creates a default connection spec to HANA database that is started by either test containers or manually.
+     * Running the test container handled instance takes much time. For the development purpose running docker
+     * manually is preferred. For that change the persistentContainer flag below.
+     *
      * @return Connection spec to a test container instance.
      */
     public static ConnectionSpec create() {
-//        GenericContainer testContainer = getSharedContainer();
+        String persistentContainerEnvVar = System.getenv("PERSISTENT_DATABASE_CONTAINER");
+        Boolean persistentContainer = Boolean.getBoolean(persistentContainerEnvVar);
+
+        if (persistentContainer) {
+            ConnectionSpec connectionSpec = new ConnectionSpec()
+            {{
+                setProviderType(ProviderType.hana);
+                setHana(new HanaParametersSpec()
+                {{
+                    setHost("localhost");
+                    setPort("39017");
+                    setUser(HanaContainer.USERNAME);
+                    setPassword(HanaContainer.PASSWORD);
+                }});
+            }};
+            return connectionSpec;
+        }
+
+        GenericContainer testContainer = getSharedContainer();
 
         ConnectionSpec connectionSpec = new ConnectionSpec()
         {{
@@ -47,8 +69,7 @@ public class HanaConnectionSpecObjectMother {
             setHana(new HanaParametersSpec()
             {{
                 setHost("localhost");
-//                setPort(testContainer.getMappedPort(HanaContainer.SYSTEM_PORT).toString());
-                setPort("39017");
+                setPort(testContainer.getMappedPort(HanaContainer.SYSTEM_PORT).toString());
                 setUser(HanaContainer.USERNAME);
                 setPassword(HanaContainer.PASSWORD);
             }});
