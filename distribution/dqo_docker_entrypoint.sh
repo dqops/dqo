a28c7a6142e93b186cf98b22cd571422e8d36398
Piotr Czarnas <piotr.czarnas@gmail.com>
2024-10-08 10:51:07 +0200

    Fix in classpath for starting in docker.

--- distribution/dqo_docker_entrypoint.sh
+++ distribution/dqo_docker_entrypoint.sh
@@ -27,7 +27,7 @@ _term() {
 trap _term SIGTERM
 trap _term SIGINT
 
-exec 3<&0 java $DQO_JAVA_OPTS --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/java.util.concurrent=ALL-UNNAMED -cp dqo/app:/dqo/app/lib/*:/dqo/home/jars/*:/dqo/userhome/jars/*:/dqo/userhome/jars/:/dqo/userhome/jars/paid/* com.dqops.cli.CliApplication $* <&3 &
+exec 3<&0 java $DQO_JAVA_OPTS --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/java.util.concurrent=ALL-UNNAMED -cp /dqo/app:/dqo/app/lib/*:/dqo/home/jars/*:/dqo/userhome/jars/*:/dqo/userhome/jars/:/dqo/userhome/jars/paid/* com.dqops.cli.CliApplication $* <&3 &
 
 child=$!
 wait $child
