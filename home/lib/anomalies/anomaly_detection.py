8c285201200caafcf13364d4889858c3e3486c98
Piotr Czarnas <piotr.czarnas@gmail.com>
2024-10-17 19:00:21 +0200

    anomaly detection function is optimized and has no performance bottlenecks under a sampling profiler

--- home/lib/anomalies/anomaly_detection.py
+++ home/lib/anomalies/anomaly_detection.py
@@ -18,20 +18,39 @@ import scipy.stats
 from .data_types import RuleExecutionRunParameters
 
 
+t_dist_dict = {}
+
+
+def get_t_distribution(degrees_of_freedom):
+    global t_dist_dict
+
+    if degrees_of_freedom in t_dist_dict:
+        return t_dist_dict[degrees_of_freedom]
+
+    t_dist = scipy.stats.t(df=degrees_of_freedom)
+    t_dist_dict[degrees_of_freedom] = t_dist
+
+    return t_dist
+
+
+def find_tail(degrees_of_freedom, values_median, values_std, tail):
+    t_dist = get_t_distribution(degrees_of_freedom)
+    return float(t_dist.ppf(tail)) * float(values_std) + float(values_median)
+
+
 def detect_upper_bound_anomaly(values: list[float], median: float, tail: float,
                                parameters: RuleExecutionRunParameters):
     values_above_median = [value for value in values if value >= median]
     values_array = np.array(values_above_median, dtype=float)
     values_median = np.median(values_array)
     values_std = scipy.stats.tstd(values_array)
+    df = float(parameters.configuration_parameters.degrees_of_freedom)
 
     if float(values_std) == 0:
         return float(values_median)
     else:
         # Assumption: the historical data follows t-student distribution
-        upper_readout_distribution = scipy.stats.t(df=float(parameters.configuration_parameters.degrees_of_freedom),
-                                                   loc=values_median, scale=values_std)
-        return float(upper_readout_distribution.ppf(1 - tail))
+        return find_tail(df, values_median, values_std, 1 - tail)
 
 
 def detect_lower_bound_anomaly(values: list[float], median: float, tail: float,
@@ -40,11 +59,10 @@ def detect_lower_bound_anomaly(values: list[float], median: float, tail: float,
     values_array = np.array(values_below_median, dtype=float)
     values_median = np.median(values_array)
     values_std = scipy.stats.tstd(values_array)
+    df = float(parameters.configuration_parameters.degrees_of_freedom)
 
     if float(values_std) == 0:
         return float(values_median)
     else:
         # Assumption: the historical data follows t-student distribution
-        lower_readout_distribution = scipy.stats.t(df=float(parameters.configuration_parameters.degrees_of_freedom),
-                                                   loc=values_median, scale=values_std)
-        return float(lower_readout_distribution.ppf(tail))
+        return find_tail(df, values_median, values_std, tail)
